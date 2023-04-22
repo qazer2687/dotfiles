39f5c7761cf4f5c6c3b5a4277f9f671b937f00ee
Alex (Qazer) <alexvasilkovski@outlook.com>
2023-04-22 20:59:27 +0100

    attempt to fix error

--- hosts/desktop/config/minecraft/default.nix
+++ hosts/desktop/config/minecraft/default.nix
@@ -1,50 +1,54 @@
 { config, pkgs, ... }:
 
 let
-  enigmatica6Dir = "/opt/minecraftserver";
-  enigmatica6ZipUrl = "https://mediafilez.forgecdn.net/files/4437/692/Enigmatica6Server-1.8.0.zip";
-  enigmatica6Zip = "$/opt/minecraftserver/Enigmatica6Server-1.8.0.zip";
-
-in {  
-
-  # Create and Accept EULA File
-  systemd.tmpfiles.rules = [
-  "w ${minecraftDir}/eula.txt 0644 minecraft minecraft - - eula=true"
-  
-  # Create Server Properties File
-  pkgs.writeText "/opt/minecraftserver/server.properties" ''
+  minecraftDir = "/opt/minecraftserver";
+  minecraftUrl = "https://mediafilez.forgecdn.net/files/4437/692/Enigmatica6Server-1.8.0.zip";
+  minecraftZip = "$/opt/minecraftserver/Enigmatica6Server-1.8.0.zip";
+  serverProperties = ''
   gamemode=survival
   difficulty=peaceful
   max-players=2
   server-port=25565
   '';
+
+in {
+  # Create & Accept EULA File
+  systemd.tmpfiles.rules = [
+    "w /opt/minecraftserver/eula.txt 0644 minecraft minecraft - - eula=true"
+  ];
   
-  builtins.writeTextFile "/opt/minecraftserver}/server.properties" serverProperties;
+  # Create & Write Server.Properties File
+  systemd.tmpfiles.rules = [
+    "w /opt/minecraftserver/server.properties 0644 minecraft minecraft - - ${serverProperties}"
+  ];
   
   # Install Java 11 & Unzip
   environment.systemPackages = with pkgs; [ jdk11 unzip ];
 
+  # Minecraft Service User
   users.users.minecraftserver = {
     isSystemUser = true;
     home = "/opt/minecraftserver";
     extraGroups = [ "networking" ];
   };
 
-  systemd.services.minecraftserver-prep = {
+  # Minecraft Installer Service
+  systemd.services.installminecraftserver = {
     script = ''
       set -euxo pipefail
       mkdir -p /opt/minecraftserver
       chown minecraftserve /opt/minecraftserver
       chmod 700 /opt/minecraftserver
-      curl -L -o /opt/minecraftserver ${enigmatica6ZipUrl}
-      unzip ${enigmatica6Zip} -d /opt/minecraftserver
+      curl -L -o /opt/minecraftserver ${minecraftUrl}
+      unzip ${minecraftZip} -d /opt/minecraftserver
       chown -R minecraftserver /opt/minecraftserver
       chmod -R 700 /opt/minecraftserver
-      rm ${enigmatica6Zip}
+      rm ${minecraftZip}
     '';
     wantedBy = [ "multi-user.target" ];
   };
 
+  # Minecraft Runner Service
   systemd.services.minecraftserver = {
     user = "minecraftserver";
     group = "users";
@@ -59,6 +63,6 @@ in {
       ExecStart = "${pkgs.jdk11}/bin/java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar forge-1.16.5-36.2.2.jar nogui";
       Type = "forking";
     };
-    after = [ "network.target" "enigmatica6-prep.service" ];
+    after = [ "network.target" "installminecraftserver.service" ];
   };
 }
