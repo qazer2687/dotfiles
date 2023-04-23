799d0093d50e1ccdc0b8830457b1e806c86d06d8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-04-23 18:55:00 +0100

    attempt to fix minecraft runner service

--- hosts/desktop/config/minecraft/default.nix
+++ hosts/desktop/config/minecraft/default.nix
@@ -46,7 +46,7 @@ in {
   systemd.services.minecraftserver = {
 
     serviceConfig = {
-      ExecStart = "${pkgs.jdk8}/bin/java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar /opt/minecraftserver/server.jar nogui";
+      ExecStart = "${pkgs.jdk8}/bin/java -jar /opt/minecraftserver/server.jar";
       Type = "simple";
     };
     after = [ "network.target" "installminecraftserver.service" ];
