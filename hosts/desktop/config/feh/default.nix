1b28cf3e5bec5c8ecc886d6ac5e1c77967c3ab70
Alex (Qazer) <alexvasilkovski@outlook.com>
2023-04-24 14:39:12 +0100

    fixed group issue

--- hosts/desktop/config/feh/default.nix
+++ hosts/desktop/config/feh/default.nix
@@ -2,6 +2,6 @@
 {
   # Tmpfiles rule to move wallpaper.
   systemd.tmpfiles.rules = [
-    "L+ /home/alex/.config/feh/wallpaper.png 0755 alex alex - ${./wallpaper.png}"
+    "L+ /home/alex/.config/feh/wallpaper.png 0755 alex users - ${./wallpaper.png}"
   ];
 }
