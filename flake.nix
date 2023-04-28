20cec8ea06889e38ea796eaf1d7f6bd3d659ff51
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-04-28 14:55:39 +0100

    typo

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       # Desktop Configuration
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit inputs; }
+        specialArgs = { inherit inputs; };
         modules = [
           (import ./hosts/desktop inputs)
           home-manager.nixosModules.home-manager {
