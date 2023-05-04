1658d2f9a432aebdf46eea6568d9e34337436193
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-05-04 22:48:27 +0100

    install winewowstagingfull

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -119,9 +119,9 @@
     ];
   })];
   
-  # environment.systemPackages = with pkgs; [
-  #    inputs.nixpkgs-stable.legacyPackages.${pkgs.system}.wineWowPackages.full
-  # ];
+  environment.systemPackages = with pkgs; [
+      inputs.nixpkgs-stable.legacyPackages.${pkgs.system}.wineWowPackages.stagingFull
+  ];
 
   # Home Manager
   home-manager.users.alex = {
