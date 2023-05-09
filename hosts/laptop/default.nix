acf881380cdd47bf08654228b17a88d4bc5dd0f4
Alex (Qazer) <alexvasilkovski@outlook.com>
2023-05-09 10:54:37 +0100

    configure pam

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -31,6 +31,17 @@
   services.open-fprintd.enable = true;
   services.python-validity.enable = true;
 
+  # Pam Authentication
+  security.pam.services.sudo.text = ''
+    account required pam_unix.so
+    auth sufficient ${nixos-06cb-009a-fingerprint-sensor.localPackages.fprintd-clients}/lib/security/pam_fprintd.so
+    auth sufficient pam_unix.so   likeauth try_first_pass nullok
+    auth required pam_deny.so
+    password sufficient pam_unix.so nullok sha512
+    session required pam_env.so conffile=/etc/pam/environment readenv=0
+    session required pam_unix.so
+  '';
+
   # Networking
   networking.hostName = "nixpad";
   networking.networkmanager.enable = true;
