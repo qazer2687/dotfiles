d49e8d5c7e386f0ef4918c88dae6ea774a52902f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-05-09 10:31:36 +0100

    input 06cb-009a flake

--- flake.nix
+++ flake.nix
@@ -9,9 +9,11 @@
     # Home Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
+    
+    # Fingerprint Drivers  
+    nixos-06cb-009a-fingerprint-sensor.url = "github:ahbnr/nixos-06cb-009a-fingerprint-sensor";
 
-  outputs = {self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: {
+  outputs = {self, nixpkgs, nixpkgs-stable, home-manager, nixos-06cb-009a-fingerprint-sensor, ... }@inputs: {
     
     # Hosts
     nixosConfigurations = {
@@ -35,6 +37,8 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/laptop
+          nixos-06cb-009a-fingerprint-sensor.nixosModules.open-fprintd
+          nixos-06cb-009a-fingerprint-sensor.nixosModules.python-validity
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
