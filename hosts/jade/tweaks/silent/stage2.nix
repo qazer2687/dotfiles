8d6d5f3336130cf4e1fb50e644ba76ec1da37b60
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-05-12 23:51:45 +0100

    install tweaks on jade

---
+++ hosts/jade/tweaks/silent/stage2.nix
@@ -0,0 +1,30 @@
+{ inputs, lib, config, pkgs, ... }:
+let
+useHostResolvConf = config.networking.resolvconf.enable && config.networking.useHostResolvConf;
+
+bootStage2 = pkgs.substituteAll {
+  src = pkgs.runCommand "stage-2-init.sh" {} ''
+    sed '2i exec 1<>/dev/null' ${inputs.nixpkgs}/nixos/modules/system/boot/stage-2-init.sh > $out
+  '';
+  shellDebug = "${pkgs.bashInteractive}/bin/bash";
+  shell = "${pkgs.bash}/bin/bash";
+  inherit (config.boot) readOnlyNixStore systemdExecutable extraSystemdUnitPaths;
+  inherit (config.system.nixos) distroName;
+  isExecutable = true;
+  inherit useHostResolvConf;
+  inherit (config.system.build) earlyMountScript;
+  path = lib.makeBinPath ([
+      pkgs.coreutils
+      pkgs.util-linux
+    ]
+    ++ lib.optional useHostResolvConf pkgs.openresolv);
+  postBootCommands =
+    pkgs.writeText "local-cmds"
+    ''
+      ${config.boot.postBootCommands}
+      ${config.powerManagement.powerUpCommands}
+    '';
+};
+in {
+  system.build.bootStage2 = lib.mkForce bootStage2;
+}
