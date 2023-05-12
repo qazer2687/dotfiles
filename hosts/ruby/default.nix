33da60a944ee1c07e7d94df462c8616e48afca6a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-05-13 00:52:23 +0100

    fix

--- hosts/ruby/default.nix
+++ hosts/ruby/default.nix
@@ -44,7 +44,7 @@
     enable = true;
     tod = {
       enable = true;
-      driver = nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
+      driver = inputs.nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
         calib-data-file = .tweaks/fingerprint/calib-data.bin;
       };
     };
