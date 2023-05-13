4734197082da45ac28dfcaf2ac79d9df97f93c24
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-05-13 14:52:00 +0100

    //

--- hosts/jade/configs/polybar/default.nix
+++ hosts/jade/configs/polybar/default.nix
@@ -1,6 +1,6 @@
 {config, pkgs, ...}:
 {
-  home-manager.users.alex.xdg.configFile."polybar/launch.sh".executable = true;
   home-manager.users.alex.xdg.configFile."polybar/launch.sh".text = builtins.readFile ./launch.sh;
+  home-manager.users.alex.xdg.configFile."polybar/launch.sh".executable = true;
   home-manager.users.alex.xdg.configFile."polybar/config.ini".text = builtins.readFile ./config.ini;
 }
