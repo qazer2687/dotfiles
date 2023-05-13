63e0617c25a5cdb9d7e18fea0a4c46d9246b8056
alexvasilkovski <alexvasilkovski@outlook.com>
2023-05-13 14:37:32 +0100

    update

---
+++ hosts/ruby/tweaks/fingerprint/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, inputs, ... }:
+{
+  # Fingerprint
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = inputs.nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
+        calib-data-file = ./tweaks/fingerprint/calib-data.bin;
+      };
+    };
+  };
+}
