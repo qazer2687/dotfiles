c328e60637f139597448f95a05717527bfe6cba5
qazer <alexvasilkovski@outlook.com>
2023-05-26 13:53:33 +0100

    fix

--- hosts/ruby/default.nix
+++ hosts/ruby/default.nix
@@ -32,7 +32,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.kernelPackages = linuxPackages_6_2;
+  boot.kernelPackages = pkgs.linuxPackages_6_2;
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl8812au
 ];
