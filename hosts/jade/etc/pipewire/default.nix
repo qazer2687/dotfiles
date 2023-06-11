75b2af133fd9a77673c836a5fd3f12b7cabb8ab1
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-06-11 16:52:19 +0100

    64 quant

--- hosts/jade/etc/pipewire/default.nix
+++ hosts/jade/etc/pipewire/default.nix
@@ -6,9 +6,9 @@
     "pipewire/pipewire.d/92-low-latency.conf".source = json.generate "92-low-latency.conf" {
       context.properties = {
         default.clock.rate = 48000;
-        default.clock.quantum = 32;
-        default.clock.min-quantum = 32;
-        default.clock.max-quantum = 32;
+        default.clock.quantum = 64;
+        default.clock.min-quantum = 64;
+        default.clock.max-quantum = 64;
       };
     };
   };
