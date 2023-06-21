46bbc6598c1d9c7fc980a6fa77c32a7e9211a9da
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-06-21 17:37:46 +0100

    fix

--- hosts/ruby/configs/polybar/default.nix
+++ hosts/ruby/configs/polybar/default.nix
@@ -6,6 +6,11 @@
 
   # Scripts
   home-manager.users.alex.xdg.configFile."polybar/scripts/battery-notification.sh".executable = true;
+  home-manager.users.alex.xdg.configFile."polybar/scripts/battery-notification.sh".text = builtins.readFile ./scripts/battery-notification.sh;
+
   home-manager.users.alex.xdg.configFile."polybar/scripts/temperature-notification.sh".executable = true;
+  home-manager.users.alex.xdg.configFile."polybar/scripts/temperature-notification.sh".text = builtins.readFile ./scripts/temperature-notification.sh;
+
   home-manager.users.alex.xdg.configFile."polybar/scripts/signal-notification.sh".executable = true;
+  home-manager.users.alex.xdg.configFile."polybar/scripts/signal-notification.sh".text = builtins.readFile ./scripts/signal-notification.sh;
 }
