be3a8013eca346632e0180386747b8748b810292
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-01 00:18:29 +0100

    overhaul debugging

--- modules/misc/colemak.nix
+++ modules/misc/colemak.nix
@@ -1,7 +1,7 @@
 { inputs, lib, config, pkgs, ... }:
 {
   options.modules.misc.colemak.enable = lib.mkEnableOption "";
-  config = lib.mkIf config.modules.misk.colemak.enable {
+  config = lib.mkIf config.modules.misc.colemak.enable {
     console.keyMap = "colemak";
     services.xserver = {
       layout = "gb";
