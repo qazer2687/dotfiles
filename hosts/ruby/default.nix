c08c0631a9174142bbed4efd904b67f081fde153
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-01 00:36:44 +0100

    overhaul release candidate 1

--- hosts/ruby/default.nix
+++ hosts/ruby/default.nix
@@ -3,25 +3,19 @@
   # Imports
   imports = [
     
-    # Hardware
-    ./hardware-configuration.nix
-
-    # Configs
+    # Configurations
     ./configs/polybar/default.nix
     ./configs/i3/default.nix
     ./configs/alacritty/default.nix
+    ./hardware-configuration.nix
 
-    # Tweaks
-    ./tweaks/silent/stage2.nix
-    ./tweaks/silent/default.nix
-    ./tweaks/zram/default.nix
-    ./tweaks/fingerprint/default.nix
-    ./tweaks/tlp/default.nix
+    # Modules
+    ../../modules/default.nix
 
   ];
 
   # State Version
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.05";
 
   # Unfree Software
   nixpkgs.config.allowUnfree = true;
@@ -29,75 +23,24 @@
   # Nix Experimental Commands
   nix.settings.experimental-features = [ "nix-command" ];
 
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-# boot.kernelPackages = pkgs.linuxPackages_6_2;
-# boot.extraModulePackages = with config.boot.kernelPackages; [
-#   rtl8812au
-# ];
-
-  # Networking
+  # Hostname
   networking.hostName = "ruby";
-  networking.networkmanager.enable = true;
-  
-  # XWayland Support
-  programs.xwayland.enable = true;
 
-  # Xorg
-  services.xserver = {
-    enable = true;
-    
-    # Login Manager
-    displayManager.gdm.enable = true;
-
-    # Window Manager
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-rounded;
-    };
-    
-    # Input Drivers
-    libinput.enable = true;
-  };
-
-  # Audio
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  
   # Users
   users.users.alex = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  # Keyboard
-  console.keyMap = "colemak";
-  services.xserver = {
-    layout = "gb";
-    xkbVariant = "colemak";
-  };
-
   # Locale
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
-
-  # Secrets
-  services.gnome.gnome-keyring.enable = true;
-
-  # Fonts
-  fonts.fonts = with pkgs; [( nerdfonts.override {
-    fonts = [
-      "FiraCode"
-    ];
-  })];
 
   # Home Manager
   home-manager.users.alex = {
 
     # State Version (HM)
-    home.stateVersion = "22.11";
+    home.stateVersion = "23.05";
 
     # Packages
     home.packages = with pkgs; [
