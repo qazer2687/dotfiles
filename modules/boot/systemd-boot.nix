86b48a0d9fda97d669ae3e79028d1d448ae238c1
qazer <alexvasilkovski@outlook.com>
2023-07-01 12:48:20 +0100

    1.5.1

--- modules/boot/systemd-boot.nix
+++ modules/boot/systemd-boot.nix
@@ -5,5 +5,6 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    boot.kernelModules = [ "nvidia" ];
   };
 }
