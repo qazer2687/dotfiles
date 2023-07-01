b55c0628285526c644772fe54a05eeb3c6fd8cbf
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-01 12:53:34 +0100

    1.5.3

--- modules/video/nvidia.nix
+++ modules/video/nvidia.nix
@@ -3,5 +3,6 @@
   options.modules.video.nvidia.enable = lib.mkEnableOption "";
   config = lib.mkIf config.modules.video.nvidia.enable {
     hardware.nvidia.package = [ "config.boot.kernelPackages.nvidiaPackages.stable" ];
+    boot.kernelModules = [ "nvidia" ];
   };
 }
