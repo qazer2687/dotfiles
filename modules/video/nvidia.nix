1c76a654e9a03d43a40f78994fe98cd7cd0fc93a
qazer <alexvasilkovski@outlook.com>
2023-07-02 19:00:32 +0100

    rc.1.5.9

--- modules/video/nvidia.nix
+++ modules/video/nvidia.nix
@@ -2,7 +2,6 @@
 {
   options.modules.video.nvidia.enable = lib.mkEnableOption "";
   config = lib.mkIf config.modules.video.nvidia.enable {
-    hardware.nvidia.package = [ "config.boot.kernelPackages.nvidiaPackages.stable" ];
     boot.kernelModules = [ "nvidia" ];
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.opengl = {
