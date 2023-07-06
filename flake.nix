aa99ecdf7679e62807665055b8a4b81c55f8e978
alexvasilkovski <alexvasilkovski@outlook.com>
2023-07-07 00:57:26 +0100

    1.6.3

--- flake.nix
+++ flake.nix
@@ -6,15 +6,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.11";
 
-    # Home Manager
+    # Home-Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Sops-Nix
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     
     # Fingerprint Drivers  
     nixos-06cb-009a-fingerprint-sensor.url = "github:ahbnr/nixos-06cb-009a-fingerprint-sensor";
   };
 
-  outputs = {self, nixpkgs, nixpkgs-stable, home-manager, nixos-06cb-009a-fingerprint-sensor, ... }@inputs: {
+  outputs = {self, nixpkgs, nixpkgs-stable, home-manager, sops-nix, nixos-06cb-009a-fingerprint-sensor, ... }@inputs: {
     
     # Development Environments
     devShells.x86_64-linux.r = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.mkShellNoCC {
@@ -32,6 +36,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/jade
+          sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -45,6 +50,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/ruby
+          sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -56,7 +62,10 @@
 #        opal = nixpkgs.lib.nixosSystem {
 #        system = "x86_64-linux";
 #        specialArgs = { inherit inputs; };
-#        modules = [ ./hosts/opal ];
+#        modules = [ 
+#           ./hosts/opal 
+#           sops-nix.nixosModules.sops
+#        ];
 #      };
 
     };
