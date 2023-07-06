a74a0ceb231924a24f8cee45c11f44278b6048bb
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-06 01:20:05 +0100

    kernel tweaks

--- modules/boot/silentboot.nix
+++ modules/boot/silentboot.nix
@@ -8,8 +8,10 @@
 
   boot = {
     blacklistedKernelModules = ["pcspkr"]; # stop beep on boot
-    kernelParams = ["fbcon=nodefer" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=3" "vt.global_cursor_default=0"];
+    kernelParams = ["fbcon=nodefer" "loglevel=2" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=1" "vt.global_cursor_default=0" "vsyscall=none" "acpi_call" "lockdown=confidentiality" "page_alloc.shuffle=1" "iommu=pt" "lsm=landlock,lockdown,yama,apparmor,bpf" "logo.nologo" "acpi_backlight=vendor"];
     consoleLogLevel = 3;
     initrd.verbose = false;
   };
+
+
 }
