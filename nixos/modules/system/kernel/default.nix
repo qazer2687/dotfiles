c644ce0415892746795212a1abcab32c667899e0
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-27 18:50:27 +0100

    fix

--- nixos/modules/system/kernel/default.nix
+++ nixos/modules/system/kernel/default.nix
@@ -1,40 +1,35 @@
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
-  options.modules.system.kernel.desktop.enable = lib.mkEnableOption "";
-  options.modules.system.kernel.laptop.enable = lib.mkEnableOption "";
+{ inputs, lib, config, pkgs, ... }: {
 
+  options.systemModules.system.kernel.desktop.enable = lib.mkEnableOption "";
+  options.systemModules.system.kernel.laptop.enable = lib.mkEnableOption "";
+  
   config = lib.mkMerge [
-    (lib.mkIf config.modules.system.kernelConfig.desktop.enable {
+    (lib.mkIf config.systemModules.system.kernel.desktop.enable {
       boot = {
-        # Disable MOBO Beep & Bluetooth
-        blacklistedKernelModules = ["pcspkr" "btusb" "bluetooth"];
+      # Disable MOBO Beep & Bluetooth
+      blacklistedKernelModules = ["pcspkr" "btusb" "bluetooth"]; 
 
-        # Silence Boot Stage
-        kernelParams = ["fbcon=nodefer" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=0" "vt.global_cursor_default=0"];
-        consoleLogLevel = 0;
-        initrd.verbose = false;
+      # Silence Boot Stage
+      kernelParams = ["fbcon=nodefer" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=0" "vt.global_cursor_default=0"];
+      consoleLogLevel = 0;
+      initrd.verbose = false;
 
-        # Zen Kernel
-        kernelPackages = pkgs.linuxPackages_zen;
+      # Zen Kernel
+      kernelPackages = pkgs.linuxPackages_zen;
       };
     })
-    (lib.mkIf config.modules.system.kernelConfig.laptop.enable {
+    (lib.mkIf config.systemModules.system.kernel.laptop.enable {
       boot = {
-        # Disable MOBO Beep & Bluetooth
-        blacklistedKernelModules = ["pcspkr" "btusb" "bluetooth"];
+      # Disable MOBO Beep & Bluetooth
+      blacklistedKernelModules = ["pcspkr" "btusb" "bluetooth"]; 
 
-        # Silence Boot Stage
-        kernelParams = ["fbcon=nodefer" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=0" "vt.global_cursor_default=0"];
-        consoleLogLevel = 0;
-        initrd.verbose = false;
+      # Silence Boot Stage
+      kernelParams = ["fbcon=nodefer" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=0" "vt.global_cursor_default=0"];
+      consoleLogLevel = 0;
+      initrd.verbose = false;
 
-        # Stable Kernel
-        kernelPackages = pkgs.linuxPackages_latest;
+      # Stable Kernel
+      kernelPackages = pkgs.linuxPackages_latest;
       };
     })
   ];
