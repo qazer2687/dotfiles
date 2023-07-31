540dd32522179a59fa64d5d8a3695730b633579f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-31 17:17:49 +0100

    fix

--- home/modules/programs/spicetify/default.nix
+++ home/modules/programs/spicetify/default.nix
@@ -6,17 +6,16 @@
   ...
 }: {
 
+  # Imports
   imports = [inputs.spicetify-nix.homeManagerModule];
-  
+
   options.homeModules.programs.spicetify.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.homeModules.programs.spicetify.enable {
     # Installation
-    home.packages = with pkgs; [spotify];
+   # home.packages = with pkgs; [spotify];
 
     # Configuration
-
-
     programs.spicetify = let spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default; in {
       enable = true;
       theme = spicePkgs.themes.catppuccin-mocha;
