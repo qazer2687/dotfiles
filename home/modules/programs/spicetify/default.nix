9a1ef6c1fba530d8833e4473d73c82f4d88065d9
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-31 17:57:54 +0100

    fix

--- home/modules/programs/spicetify/default.nix
+++ home/modules/programs/spicetify/default.nix
@@ -23,28 +23,27 @@
         fullAppDisplay
         shuffle
         hidePodcasts
-        autoSkipVideo
-        songStats
       ];
-      colorScheme = "custom";
-      customColorScheme = {
-        text = "ffffff";
-        subtext = "F0F0F0";
-        sidebar-text = "e0def4";
-        main = "191724";
-        sidebar = "0a0a0a";
-        player = "0f0f0f";
-        card = "191724";
-        shadow = "1f1d2e";
-        selected-row = "050505";
-        button = "31748f";
-        button-active = "31748f";
-        button-disabled = "555169";
-        tab-active = "ebbcba";
-        notification = "1db954";
-        notification-error = "eb6f92";
-        misc = "6e6a86";
-      };
+    #  colorScheme = "custom";
+      
+     # customColorScheme = {
+     #   text = "ffffff";
+     #   subtext = "F0F0F0";
+     #   sidebar-text = "e0def4";1
+     #   main = "191724";
+     #   sidebar = "0a0a0a";
+     #   player = "0f0f0f";
+     #   card = "191724";
+      #  shadow = "1f1d2e";
+     #   selected-row = "050505";
+     #   button = "31748f";
+     #   button-active = "31748f";
+     #   button-disabled = "555169";
+     #   tab-active = "ebbcba";
+     #   notification = "1db954";
+     #   notification-error = "eb6f92";
+     #   misc = "6e6a86";
+     # };
     };
   };
 }
