b1fdfcfe670cd4ee8ea35b28b6eb7a7dadd05f24
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-31 15:34:45 +0100

    fix

--- home/modules/services/spotifyd/default.nix
+++ home/modules/services/spotifyd/default.nix
@@ -9,10 +9,34 @@
     # Installation
     services.spotifyd = {
       enable = true;
+      settings = {
+        global = {
+          username = "3fl3wvehzpuudbzovifsyndbp";
+          password_cmd = "$HOME/.config/spotifyd/read_password.sh";
+          use_keyring = false;
+          use_mpris = true;
+          dbus_type = "session";
+          backend = "pipewire";
+          device = "default";
+          audio_format = "S16";
+          mixer = "PCM";
+          volume_controller = "alsa";
+          device_name = "SpotifyD";
+          bitrate = 320;
+          cache_path = "/home/alex/.cache/spotifyd";
+          max_cache_size = 5000000000;
+          no_audio_cache = false;
+          initial_volume = "60";
+          volume_normalisation = false;
+          normalisation_pregain = -10;
+          autoplay = true;
+          device_type = "speaker";
+        };
+      };
     };
 
     # Configuration
-    xdg.configFile."spotifyd/spotifyd.conf".text = builtins.readFile ./spotifyd.conf;
+    #xdg.configFile."spotifyd/spotifyd.conf".text = builtins.readFile ./spotifyd.conf;
     xdg.configFile."spotifyd/read_password.sh".text = builtins.readFile ./read_password.sh;
     xdg.configFile."spotifyd/read_password.sh".executable = true;
   };
