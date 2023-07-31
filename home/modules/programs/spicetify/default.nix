643437b11e226586003236116a1a22e28a35a30d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-07-31 17:42:32 +0100

    spicetify colourscheme

--- home/modules/programs/spicetify/default.nix
+++ home/modules/programs/spicetify/default.nix
@@ -18,13 +18,33 @@
     # Configuration
     programs.spicetify = let spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default; in {
       enable = true;
-      theme = spicePkgs.themes.catppuccin-mocha;
-      colorScheme = "flamingo";
+      theme = spicePkgs.themes.dribbblish;
       enabledExtensions = with spicePkgs.extensions; [
         fullAppDisplay
-        shuffle # shuffle+ (special characters are sanitized out of ext names)
+        shuffle
         hidePodcasts
+        autoSkipVideo
+        songStats
       ];
+      colorScheme = "custom";
+      customColorScheme = {
+        text = "ffffff";
+        subtext = "F0F0F0";
+        sidebar-text = "e0def4";
+        main = "191724";
+        sidebar = "0a0a0a";
+        player = "0f0f0f";
+        card = "191724";
+        shadow = "1f1d2e";
+        selected-row = "050505";
+        button = "31748f";
+        button-active = "31748f";
+        button-disabled = "555169";
+        tab-active = "ebbcba";
+        notification = "1db954";
+        notification-error = "eb6f92";
+        misc = "6e6a86";
+      };
     };
   };
 }
