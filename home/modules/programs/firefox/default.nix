bac6bf31552936f90ebe5a3e9c5660fda5a9cd48
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-01 02:27:37 +0100

    settings

--- home/modules/programs/firefox/default.nix
+++ home/modules/programs/firefox/default.nix
@@ -8,9 +8,14 @@
   config = lib.mkIf config.homeModules.programs.firefox.enable {
     programs.firefox = {
       enable = true;
-      profiles.custom.isDefault = true;
-      profiles.custom.name = "custom";
-      profiles.custom.userChrome = builtins.readFile ./userChrome.css;
+      profiles.custom = {
+        isDefault = true;
+        name = "custom";
+        userChrome = builtins.readFile ./userChrome.css;
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        };
+      };
     };
   };
 }
