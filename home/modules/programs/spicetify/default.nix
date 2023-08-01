cd7b80a680b6239a68846a62fbcdf2325603f747
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-01 02:20:39 +0100

    firefox module

--- home/modules/programs/spicetify/default.nix
+++ home/modules/programs/spicetify/default.nix
@@ -12,39 +12,17 @@
   options.homeModules.programs.spicetify.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.homeModules.programs.spicetify.enable {
-    # Installation
-   # home.packages = with pkgs; [spotify];
-
     # Configuration
     programs.spicetify = let spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default; in {
       enable = true;
-      theme = spicePkgs.themes.Sleek;
+      theme = spicePkgs.themes.catppuccin-mocha;
+      colorScheme = "rosewater";
       enabledExtensions = with spicePkgs.extensions; [
         fullAppDisplay
         shuffle
         hidePodcasts
         songStats
       ];
-      colorScheme = "custom";
-      
-      customColorScheme = {
-        text = "ffffff";
-        subtext = "F0F0F0";
-        sidebar-text = "e0def4";
-        main = "191724";
-        sidebar = "0a0a0a";
-        player = "0f0f0f";
-        card = "191724";
-        shadow = "1f1d2e";
-        selected-row = "050505";
-        button = "31748f";
-        button-active = "31748f";
-        button-disabled = "555169";
-        tab-active = "ebbcba";
-        notification = "1db954";
-        notification-error = "eb6f92";
-        misc = "6e6a86";
-      };
     };
   };
 }
