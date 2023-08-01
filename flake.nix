3c20c7380660af67c271e462b600763a6a5d7c42
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-01 03:11:15 +0100

    attempting something

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     # Unstable Packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # Custom NUR
+    nur.url = "https://github.com/***REMOVED***/nur-combined";
+
     # Home-Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -21,6 +24,7 @@
     nixpkgs,
     home-manager,
     sops-nix,
+    nur,
     ...
   } @ inputs: {
     # Hosts
