79e8726991f0e2e2931996fcc9467ce0e24f235a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-01 14:09:36 +0100

    theming

---
+++ home/modules/programs/discord/default.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options.homeModules.programs.discord.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.homeModules.programs.discord.enable {
+    # Installation
+    home.packages = with pkgs; [webcord-vencord];
+
+    # Configuration
+    xdg.configFile."WebCord/Themes/catpuccin-mocha-rosewater.theme.css".text = builtins.readFile ./catpuccin-mocha-rosewater.theme.css;
+  };
+}
