97cd0bc701cfe936425d9168603895cabf56541c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-10 19:57:59 +0100

    discord

--- home/modules/programs/discord/default.nix
+++ home/modules/programs/discord/default.nix
@@ -4,23 +4,6 @@
   pkgs,
   ...
 }: {
-  options.homeModules.programs.discord.enable = lib.mkEnableOption "";
-
-  config = lib.mkIf config.homeModules.programs.discord.enable {
-    # Installation
-    home.packages = with pkgs; [webcord-vencord];
-
-    # Configuration
-    xdg.configFile."WebCord/Themes/catpuccin-mocha-rosewater.theme.css".text = builtins.readFile ./config/desktop;
-  };
-}
-
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
   options.homeModules.programs.discord.desktopConfig.enable = lib.mkEnableOption "";
   options.homeModules.programs.discord.laptopConfig.enable = lib.mkEnableOption "";
 
