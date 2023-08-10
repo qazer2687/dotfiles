0c803baa6fe5741871bc4a95fa2e5d65925d0be5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-10 17:14:57 +0100

    fix

--- home/modules/programs/spicetify/default.nix
+++ home/modules/programs/spicetify/default.nix
@@ -5,17 +5,22 @@
   config,
   ...
 }: {
+  
 
   # Imports
   imports = [inputs.spicetify-nix.homeManagerModule];
 
+  
+
   options.homeModules.programs.spicetify.enable = lib.mkEnableOption "";
+
+  
 
   config = lib.mkIf config.homeModules.programs.spicetify.enable {
     # Configuration
     programs.spicetify = let dynamicOptions = lib.attrsets.mapAttrs (name: _pkg: lib.mkOption { default = null; }) inputs.spicetify-nix.packages.${pkgs.system}.default; in { 
       enable = true;
-      theme = dynamicOptions.default.themes.catppuccin-macchiato;
+      theme = default.dynamicOptions.default.themes.catppuccin-macchiato;
       colorScheme = "flamingo";
       enabledExtensions = with dynamicOptions.extensions; [
         hidePodcasts
