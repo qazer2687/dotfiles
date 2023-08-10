2c1f13137eeb735bed9d36810563c80c7ec53061
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-10 19:57:14 +0100

    fix

--- home/modules/programs/discord/default.nix
+++ home/modules/programs/discord/default.nix
@@ -14,3 +14,31 @@
     xdg.configFile."WebCord/Themes/catpuccin-mocha-rosewater.theme.css".text = builtins.readFile ./config/desktop;
   };
 }
+
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.homeModules.programs.discord.desktopConfig.enable = lib.mkEnableOption "";
+  options.homeModules.programs.discord.laptopConfig.enable = lib.mkEnableOption "";
+
+  config = lib.mkMerge [
+    (lib.mkIf config.homeModules.programs.discord.desktopConfig.enable {
+    # Installation
+    home.packages = with pkgs; [webcord-vencord];
+
+    # Configuration
+    xdg.configFile."WebCord/Themes/catpuccin-mocha-rosewater.theme.css".text = builtins.readFile ./config/desktop;
+    })
+
+    (lib.mkIf config.homeModules.programs.discord.laptopConfig.enable {
+    # Installation
+    home.packages = with pkgs; [webcord-vencord];
+
+    # Configuration
+    xdg.configFile."WebCord/Themes/catpuccin-macchiato.theme.css".text = builtins.readFile ./config/laptop;
+    })
+  ];
+}
