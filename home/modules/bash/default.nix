e63633a7b26ca000a78ade22006885a12c37ad15
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-24 20:51:03 +0100

    fix bash

--- home/modules/bash/default.nix
+++ home/modules/bash/default.nix
@@ -3,21 +3,21 @@
   config,
   ...
 }: {
-  options.homeModules.bash.enable = lib.mkEnableOption "";
+  options.homeModules.bash.jade.enable = lib.mkEnableOption "";
+  options.homeModules.bash.ruby.enable = lib.mkEnableOption "";
 
   config = lib.mkMerge [
-    (lib.mkIf config.homeModules.bash.enable {
+    (lib.mkIf config.homeModules.bash.jade.enable {
       programs.bash = {
         enable = true;
         enableCompletion = true;
       };
     })
 
-    (lib.mkIf config.homeModules.bash.swayProfile.enable {
+    (lib.mkIf config.homeModules.bash.ruby.enable {
        programs.bash = {
-        profileExtra = ''
-          
-        '';
+        enable = true;
+        enableCompletion = true;
       };
     })
   ];
