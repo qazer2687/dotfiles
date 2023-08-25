20a56e939773c8a5b56d05d2ed44529466fbfc90
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-25 12:08:44 +0100

    convert auto-cpufreq settings type

--- nixos/modules/auto-cpufreq/default.nix
+++ nixos/modules/auto-cpufreq/default.nix
@@ -8,7 +8,20 @@
   config = lib.mkIf config.systemModules.auto-cpufreq.enable {
     services.auto-cpufreq = {
       enable = true;
-      #settings = builtins.readFile ./config/laptop;
+      settings = {
+        charger = {
+          governor = "performance";
+          scaling_min_freq = 1700000;
+          scaling_max_freq = 2000000;
+          turbo = "auto";
+        };
+        battery = {
+          governor = "powersave";
+          scaling_min_freq = 800000;
+          scaling_max_freq = 1000000;
+          turbo = "never";
+        };
+      };
     };
   };
 }
