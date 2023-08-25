908780319196cf71203110312646717c26748923
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-25 21:19:37 +0100

    fix

--- home/modules/waybar/default.nix
+++ home/modules/waybar/default.nix
@@ -37,8 +37,11 @@
           "tray"
           "custom/seperator-blank"
         ];
+        "sway/workspaces" = {
+          format = " {name} ";
+        };
         battery = {
-          format = "{icon}";
+          format = " {icon} ";
           format-icons = [ "󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
           format-plugged = "󰂄";
           states = {
@@ -48,26 +51,26 @@
           interval = 10;
         };
         clock = {
-          format = "󰥔";
+          format = " 󰥔 ";
           tooltip-format = "{:%Y-%m-%d | %H:%M}";
         };
         cpu = {
-          format = "󰘚";
+          format = " 󰘚 ";
           tooltip = true;
           interval = 2;
         };
         memory = {
           interval = 2;
-          format = "󰍛";
+          format = " 󰍛 ";
         };
         network = {
           interval = 1;
-          format = "󰤨";
+          format = " 󰤨 ";
           format-disconnected = "󰤭";
           format-ethernet = "󰈀";
         };
         pulseaudio = {
-          format = "{icon}";
+          format = " {icon} ";
           format-icons = {
             default = [ "󰕿" "󰖀" "󰕾" ];
             headphones = "󰋋";
@@ -79,7 +82,7 @@
         };
         temperature = {
           critical-threshold = 50;
-          format = "{icon}";
+          format = " {icon} ";
           tooltip-format = "{temperatureC}°C";
           format-icons = [ "󱃃" "󰔏" "󱃂" ];
         };
@@ -87,7 +90,7 @@
           format = " ";
         };
         "custom/seperator-beam" = {
-          format = " | ";
+          format = "|";
         };
       }];
       style = ''
@@ -97,7 +100,7 @@
         }
         #waybar * {
           font-family: 'FiraCode Nerd Font', monospace;
-          font-size: 12px;
+          font-size: 15px;
           color: white;
         }
       '';
