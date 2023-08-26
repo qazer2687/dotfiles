30b32387c8580414f3f310eae54ebd0e954764dd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-26 11:57:47 +0100

    fix

--- home/modules/gammastep/default.nix
+++ home/modules/gammastep/default.nix
@@ -8,7 +8,6 @@
   config = lib.mkIf config.homeModules.gammastep.enable {
     services.gammastep = {
       enable = true;
-      provider = "manual";
       latitude = 51.5;
       longitude = 0.1;
       temperature.day = 4000;
