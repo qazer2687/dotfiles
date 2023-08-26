4e688a87a43b578adee8ca14c6b7d4584a9f36ef
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-26 19:59:34 +0100

    install firamono nerd font

--- home/modules/gammastep/default.nix
+++ home/modules/gammastep/default.nix
@@ -8,6 +8,7 @@
   config = lib.mkIf config.homeModules.gammastep.enable {
     services.gammastep = {
       enable = true;
+      provider = "manual";
       latitude = 51.5;
       longitude = 0.1;
       temperature.day = 4000;
