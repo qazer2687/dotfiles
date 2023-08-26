adf0e7a8b6c4210b95ac9ce833034e521428582d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-26 11:04:22 +0100

    gammastep attempted fix

--- home/modules/gammastep/default.nix
+++ home/modules/gammastep/default.nix
@@ -9,10 +9,8 @@
     services.gammastep = {
       enable = true;
       provider = "manual";
-      dawnTime = "6:00-7:45";
-      duskTime = "18:35-20:15";
-      latitude = 51.5072;
-      longitude = 0.1276;
+      latitude = 51.5;
+      longitude = 0.1;
       temperature.day = 4000;
       temperature.night = 4000;
     };
