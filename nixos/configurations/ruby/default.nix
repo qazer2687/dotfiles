62af70100e839e7afafd8cf78793c4071ddd0a60
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-08-27 20:58:31 +0100

    fix

--- nixos/configurations/ruby/default.nix
+++ nixos/configurations/ruby/default.nix
@@ -1,48 +1,4 @@
-{config, ...}: {
-  # Imports
-  imports = [
-    ./hardware-configuration.nix
-    ../../modules
-  ];
-
-  systemModules = {
-    pipewire.enable = true;
-    systemd-boot.enable = true;
-    colemak.enable = true;
-    fonts.enable = true;
-    libinput.enable = true;
-    gnome-keyring.enable = true;
-    networkmanager.enable = true;
-    kernel.ruby.enable = true;
-    tlp.enable = true;
-    fstrim.enable = true;
-    polkit.enable = true;
-    opendrop.enable = true;
-    opengl.enable = true;
-    sway.ruby.enable = true;
-  };
-
-  # Hostname
-  networking.hostName = "ruby";
-
-  # Users
-  users.users.alex = {
-    isNormalUser = true;
-    extraGroups = ["networkmanager" "wheel" "video"];
-  };
-
-  # System State Version
-  system.stateVersion = "23.05";
-
-  # Issue/MOTD
-  environment.etc = {
-    issue = {
-      text = ''\e[31mWelcome to Ruby!\e[0m'';
-    };
-  };
-
-  # Allow Unfree Software
-  nixpkgs.config.allowUnfree = true;
+ 
 
   # Nix Experimental Features
   nix.settings.experimental-features = ["nix-command" "flakes"];
