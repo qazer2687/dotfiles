a6e3bce234d5208dd981ee35764d1499166b1afc
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-09-12 09:58:57 +0100

    logind extraConfig for disabling power button

---
+++ nixos/modules/logind/default.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options.systemModules.logind.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.systemModules.logind.enable {
+    services.logind = {
+      extraConfig = ''
+      HandlePowerKey=ignore
+    '';
+    }
+  };
+}
