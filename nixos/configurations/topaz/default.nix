b25f6f2672676e9f898cb17792a7f07af27c468f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-09-14 12:32:18 +0100

    waybar frequency

---
+++ nixos/configurations/topaz/default.nix
@@ -0,0 +1,34 @@
+{...}: {
+  # Imports
+  imports = [
+    ./hardware-configuration.nix
+    ./modules.nix
+  ];
+
+  # Hostname
+  networking.hostName = "ruby";
+
+  # Disable XWayland & Xorg
+  programs.xwayland.enable = false;
+  services.xserver.enable = false;
+
+  # Disable XDG Portal
+  xdg.portal.enable = false;
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1"; # Wayland Electron Support
+    MOZ_ENABLE_WAYLAND = "1";
+    GTK_USE_PORTAL = "0"; # Waybar Startup Delay Fix
+  };
+  # Issue/MOTD
+  environment.etc = {
+    issue = {
+      text = ''\e[31mWelcome to Topaz!\e[0m'';
+    };
+  };
+
+  # No Login Manager
+  environment.loginShellInit = ''
+    [[ "$(tty)" == /dev/tty1 ]] && sway
+  '';
+}
