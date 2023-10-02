ff03f9abe4ce904a9fa6c177be3b4ed03d345f5f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-10-02 19:44:38 +0100

    switch jade to wayland experimentally

--- home/configurations/ruby/modules.nix
+++ home/configurations/ruby/modules.nix
@@ -4,14 +4,12 @@
   ];
 
   homeModules = {
-    bash.ruby.enable = true;
     direnv.enable = true;
     git.enable = true;
     neovim.enable = true;
     gammastep.enable = true;
     firefox.enable = true;
     obsidian.enable = true;
-    armcord.enable = true;
     vscode.ruby.enable = true;
     foot.ruby.enable = true;
     sway.ruby.enable = true;
--- nixos/configurations/jade/modules.nix
+++ nixos/configurations/jade/modules.nix
@@ -8,20 +8,17 @@
     pipewire.enable = true;
     easyeffects.enable = true;
     systemd-boot.enable = true;
-    gdm.xorg.enable = true;
     i3.enable = true;
-    steam.enable = true;
     colemak.enable = true;
     fonts.enable = true;
     libinput.enable = true;
     zram.enable = true;
-    keepassxc.enable = true;
     gnome-keyring.enable = true;
     networkmanager.enable = true;
     udev.via.enable = true;
     kernel.jade.enable = true;
     fstrim.enable = true;
     nvidia.enable = true;
-    opendrop.enable = true;
+    sway.jade.enable = true;
   };
 }
