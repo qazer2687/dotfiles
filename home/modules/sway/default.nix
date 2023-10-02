a5b1a5767fce2449e352e17341784c712d6fc5d5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-10-02 19:46:01 +0100

    fix

--- home/modules/sway/default.nix
+++ home/modules/sway/default.nix
@@ -3,18 +3,6 @@
   config,
   ...
 }: {
-  options.homeModules.sway.ruby.enable = lib.mkEnableOption "";
-
-  config = lib.mkIf config.homeModules.sway.ruby.enable {
-    xdg.configFile."sway/config".text = builtins.readFile ./config/ruby;
-  };
-}
-
-{
-  lib,
-  config,
-  ...
-}: {
   options.homeModules.sway.jade.enable = lib.mkEnableOption "";
   options.homeModules.sway.ruby.enable = lib.mkEnableOption "";
 
