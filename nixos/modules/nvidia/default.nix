ed4c97115ba349b0fafb01cf7263299878937d5b
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-10-02 21:45:24 +0100

    nouveau

--- nixos/modules/nvidia/default.nix
+++ nixos/modules/nvidia/default.nix
@@ -8,7 +8,7 @@
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia = {
       modesetting.enable = false;
-      open = false;
+      open = true;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
