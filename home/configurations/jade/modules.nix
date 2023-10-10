e9b7b9a36525af0da14ede9dd34551701a3a1b75
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-10-10 20:27:31 +0100

    enable spicetify

--- home/configurations/jade/modules.nix
+++ home/configurations/jade/modules.nix
@@ -8,6 +8,7 @@
     git.enable = true;
     neovim.enable = true;
     firefox.enable = true;
+    spicetify.enable = true;
     obs.enable = true;
     vscode.jade.enable = true;
     spotifyd.enable = true;
