401c4f6cd838f0446659c1f6c1111324e2a12463
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-10-18 18:14:46 +0100

    fix

--- home/configurations/jade/default.nix
+++ home/configurations/jade/default.nix
@@ -3,18 +3,5 @@
     ./modules.nix
   ];
 
-  home.packages = with pkgs; [
-    # General
-    #obs-studio
-    #vlc
-
-    # Gaming
-    #osu-lazer-bin
-    #prismlauncher
-    #lunar-client
-    #lutris
-    #protonup-qt
-  ];
-
   sops.secrets.spotify_password.path = "/home/alext/.config/spotifyd/password";
 }
