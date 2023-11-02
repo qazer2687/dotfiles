fd3e853130fc271a4d96b16f3376248d92f0d862
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-11-02 15:14:28 +0000

    checks

--- nixos/modules/virt-manager/default.nix
+++ nixos/modules/virt-manager/default.nix
@@ -8,6 +8,6 @@
   config = lib.mkIf config.systemModules.virt-manager.enable {
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
-    users.users.alex.extraGroups = [ "libvirtd" ];
+    users.users.alex.extraGroups = ["libvirtd"];
   };
 }
