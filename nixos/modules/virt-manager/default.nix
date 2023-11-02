aadd7364c4184f953145d4bef8956525e725306a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-11-02 16:13:22 +0000

    configure virt-manager

--- nixos/modules/virt-manager/default.nix
+++ nixos/modules/virt-manager/default.nix
@@ -7,9 +7,38 @@
   options.systemModules.virt-manager.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.systemModules.virt-manager.enable {
-    virtualisation.libvirtd.enable = true;
+    
+    # Virtualisation Service
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        qemu = {
+          swtpm.enable = true;
+          ovmf.enable = true;
+          ovmf.packages = with pkgs; [OVMFFull.fd];
+        };
+      };
+      spiceUSBRedirection.enable = true;
+    };
+    services.spice-vdagentd.enable = true;
+    
+    # Dconf
     programs.dconf.enable = true;
-    environment.systemPackages = with pkgs; [ virt-manager ];
+    
+    # Required Packages
+    environment.systemPackages = with pkgs; [
+      virt-manager
+      virt-viewer
+      spice
+      spice-gtk
+      spice-protocol
+      win-virtio
+      win-spice
+      gnome.adwaita-icon-theme
+    ];
+
+    # Add Group
     users.users.alex.extraGroups = ["libvirtd"];
+
   };
 }
