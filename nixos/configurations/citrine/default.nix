fcdb1f107fc3bd8d5826fa34ff2505610c69c42d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-11-02 19:07:50 +0000

    citrine host config

---
+++ nixos/configurations/citrine/default.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../../hardware/citrine
+    ../../modules
+  ];
+
+  # NETWORKING
+  networking.hostName = "citrine";
+
+  # MODULES
+  systemModules = {
+    user.alex.enable = true;
+    grub.enable = true;
+    colemak.enable = true;
+    networkmanager.enable = true;
+    kernel = {
+      enable = true;
+      type = "latest";
+    };
+  };
+}
+
