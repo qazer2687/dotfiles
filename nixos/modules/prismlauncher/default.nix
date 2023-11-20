d5254001bf22a676ac70d96e7ce232ba8e6e75af
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-11-20 16:52:16 +0000

    fix

--- nixos/modules/prismlauncher/default.nix
+++ nixos/modules/prismlauncher/default.nix
@@ -11,7 +11,7 @@
       systemPackages = with pkgs; [
         prismlauncher
         temurin-bin-8
-        graalvmCEPackages.graalvm17-ce-full
+        graalvm-ce
       ];
       etc = {
         "jdks/17".source = lib.getBin pkgs.openjdk17;
