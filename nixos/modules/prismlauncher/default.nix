5a12204951604287e3c3b514a5259ec9bb59278f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-11-21 13:47:53 +0000

    enable steam

--- nixos/modules/prismlauncher/default.nix
+++ nixos/modules/prismlauncher/default.nix
@@ -10,10 +10,9 @@
     environment = {
       systemPackages = with pkgs; [
         prismlauncher
-        temurin-bin-8
-        graalvm-ce
       ];
       etc = {
+        "jdks/8-temurin".source = lib.getBin pkgs.temurin-bin-8;
         "jdks/17".source = lib.getBin pkgs.openjdk17;
         "jdks/11".source = lib.getBin pkgs.openjdk11;
         "jdks/8".source = lib.getBin pkgs.openjdk8;
