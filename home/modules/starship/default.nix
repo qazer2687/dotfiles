b11e74678b27ad256adeefa61596d05cc9e492f3
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-12-03 03:48:54 +0000

    refactor: modify config

--- home/modules/starship/default.nix
+++ home/modules/starship/default.nix
@@ -18,7 +18,7 @@
           impure_msg = "[impure shell](bold red)";
           pure_msg = "[pure shell](bold green)";
           unknown_msg = "[unknown shell](bold yellow)";
-          format = "via [󱄅 $state( \($name\))](bold blue) ";
+          format = "on [$state( \($name\))](bold blue) ";
         };
       };
     };
