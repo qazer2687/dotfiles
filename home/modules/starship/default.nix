e5b7395b36cc60193f2863c5e1a8080d56ea277a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-12-03 03:46:54 +0000

    style: add semicolon

--- home/modules/starship/default.nix
+++ home/modules/starship/default.nix
@@ -18,7 +18,7 @@
           impure_msg = "[impure shell](bold red)";
           pure_msg = "[pure shell](bold green)";
           unknown_msg = "[unknown shell](bold yellow)";
-          format = "via [󱄅 $state( \($name\))](bold blue) "
+          format = "via [󱄅 $state( \($name\))](bold blue) ";
         };
       };
     };
