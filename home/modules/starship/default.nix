e68ffff33570717726f329817543c58926eb1a94
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-12-03 03:56:24 +0000

    refactor: attempt to fix nix-shell starship integration

--- home/modules/starship/default.nix
+++ home/modules/starship/default.nix
@@ -15,11 +15,7 @@
           format = "via [󰒓 $version](red bold) ";
         };
         nix-shell = {
-          impure_msg = "[impure shell](bold red)";
-          pure_msg = "[pure shell](bold green)";
-          unknown_msg = "[unknown shell](bold yellow)";
           symbol = "󱄅";
-          format = "via [$state( \($name\))](bold blue) ";
         };
       };
     };
