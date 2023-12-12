708fab45595c16f5826e9c4f7469cd09f4da2fa5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-12-12 01:59:00 +0000

    style: add semicolon

--- nixos/configurations/jade/default.nix
+++ nixos/configurations/jade/default.nix
@@ -16,7 +16,7 @@
   # ENVIRONMENT
   environment.sessionVariables = {
     NIXPKGS_ALLOW_UNFREE = "1"; # Allow Unfree Packages
-    NIXPKGS_ALLOW_INSECURE= "1" # Allow Insecure Packages
+    NIXPKGS_ALLOW_INSECURE= "1"; # Allow Insecure Packages
   };
 
   # MODULES
