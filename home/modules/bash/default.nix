8509309fe0f3041715789e526c73ff1d8a8b5faa
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2023-12-17 06:36:53 +0000

    screenshot fix

--- home/modules/bash/default.nix
+++ home/modules/bash/default.nix
@@ -6,7 +6,7 @@
   bashAliases = {
     "check" = "alejandra -q **/* && deadnix -e && statix fix";
     "rebuild" = "sudo nixos-rebuild switch --flake github:***REMOVED***/dotfiles#$(hostname) --refresh --option eval-cache false";
-    "rebuild -local" = "sudo nixos-rebuild switch --flake .#$(hostname)";
+    "rebuild-local" = "sudo nixos-rebuild switch --flake .#$(hostname)";
   };
 in {
   options.homeModules.bash.enable = lib.mkEnableOption "";
