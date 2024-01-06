711b781f2661a6c0987dbd5646b551ed00236d64
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-06 09:43:17 +0000

    jade to wayland

--- home/configurations/jade/default.nix
+++ home/configurations/jade/default.nix
@@ -8,20 +8,17 @@
     git.enable = true;
     neovim.enable = true;
     vscode.enable = true;
-    rider.enable = false;
     firefox.enable = true;
-    armcord.enable = false;
     obsidian.enable = true;
-    spicetify.enable = false;
     obs.enable = true;
     vlc.enable = true;
-    alacritty.enable = true;
-    polybar.enable = true;
     bash.enable = true;
+    foot.enable = true;
+    starship.enable = true;
     spotifyd.enable = true;
     mangohud.enable = true;
-    i3.jade.enable = true;
     dunst.enable = true;
+    waybar.enable = true;
     gtk.enable = true;
   };
 }
