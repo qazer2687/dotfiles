3ba3f0085800ad16a00413abb5d6fa426de8d4c1
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-19 18:09:34 +0000

    feat: create opal configuration

---
+++ nixos/configurations/opal/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../../hardware/opal
+    ../../modules
+  ];
+
+  networking.hostName = "opal";
+
+  users.users.alex = {
+    isNormalUser = true;
+    extraGroups = ["networkmanager" "wheel" "video"];
+  };
+
+  services.homepage-dashboard = {
+    enable = true;
+    openFirewall = true;
+    listenPort = 80;
+  };
+
+  services.cockpit = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.jellyfin = {
+    enable = true;
+    user = "alex";
+    openFirewall = true;
+  };
+
+  
+  
+  modules = {
+    bash.enable = true;
+    kernel.enable = true;
+    networkmanager.enable = true;
+    systemd-boot.enable = true;
+    zram.enable = true;
+  };
+}
