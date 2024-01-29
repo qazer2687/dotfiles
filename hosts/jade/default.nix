01257d326962eafcd8456b71fd2bef8b651d3c47
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 17:23:19 +0000

    feat: enable i3 systemwide

--- hosts/jade/default.nix
+++ hosts/jade/default.nix
@@ -13,6 +13,7 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.windowManager.i3.enable = true;
 
   users.users.alex = {
     isNormalUser = true;
