cae70e31fa55a77353bd5343a94cd3845f7052cb
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 16:03:17 +0000

    fix: syntax error

--- homes/alex-jade/default.nix
+++ homes/alex-jade/default.nix
@@ -24,8 +24,8 @@
     prismlauncher.enable = true;
   };
 
-  home.stateVersion = mkDefault "23.05";
-  home.homeDirectory = mkDefault "/home/alex";
+  home.stateVersion = "23.05";
+  home.homeDirectory = "/home/alex";
 
   sops.defaultSopsFile = ../../../secrets/default.yaml;
   sops.age.sshKeyPaths = ["/home/alex/.ssh/id_ed25519"];
