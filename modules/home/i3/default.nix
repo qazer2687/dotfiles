e70f153061b790784a4280f070430d37aba160c6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 16:07:47 +0000

    fix: broken i3 module

--- modules/home/i3/default.nix
+++ modules/home/i3/default.nix
@@ -5,7 +5,7 @@
 }: {
   options.modules.i3.enable = lib.mkEnableOption "";
 
-  config = lib.mkIf config.modules.i3.jade.enable {
+  config = lib.mkIf config.modules.i3.enable {
     xdg.configFile."i3/config".text = builtins.readFile ./config/default;
   };
 }
