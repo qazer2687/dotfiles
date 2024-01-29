374ce0f161ab844fbbcb9b7b6b03db6045188855
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 16:07:18 +0000

    fix: broken i3 module

--- modules/home/i3/default.nix
+++ modules/home/i3/default.nix
@@ -3,18 +3,9 @@
   config,
   ...
 }: {
-  options.modules.i3.jade.enable = lib.mkEnableOption "";
-  options.modules.i3.ruby.enable = lib.mkEnableOption "";
+  options.modules.i3.enable = lib.mkEnableOption "";
 
-  config = lib.mkMerge [
-    (lib.mkIf config.modules.i3.jade.enable {
-      # Configuration
-      xdg.configFile."i3/config".text = builtins.readFile ./config/jade;
-    })
-
-    (lib.mkIf config.modules.i3.ruby.enable {
-      # Configuration
-      xdg.configFile."i3/config".text = builtins.readFile ./config/ruby;
-    })
-  ];
+  config = lib.mkIf config.modules.i3.jade.enable {
+    xdg.configFile."i3/config".text = builtins.readFile ./config/default;
+  };
 }
