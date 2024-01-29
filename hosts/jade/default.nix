fad0395971d1486872955e42197491e6fe0d1d0a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 18:50:51 +0000

    fix: attempt to fix i3

--- hosts/jade/default.nix
+++ hosts/jade/default.nix
@@ -13,11 +13,7 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.windowManager.i3 ={
-    enable = true;
-    package = pkgs.i3-rounded;
-  };
-
+  
   users.users.alex = {
     isNormalUser = true;
     extraGroups = ["networkmanager" "wheel" "video"];
