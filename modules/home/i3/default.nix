d09867222404a8208107a09005877d3e28ffb255
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 16:18:04 +0000

    feat: extra packages for i3

--- modules/home/i3/default.nix
+++ modules/home/i3/default.nix
@@ -6,6 +6,14 @@
   options.modules.i3.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.i3.enable {
+
+    home.packages = [
+      dmenu
+      scrot
+      feh
+      gnome.nautilus
+      redshift
+    ];
     xdg.configFile."i3/config".text = builtins.readFile ./config/default;
   };
 }
