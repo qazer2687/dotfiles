b3780552a5fc0934c2087435938c1134e08eedc3
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-01-29 20:46:57 +0000

    feat: issue and autorun startx

--- hosts/ruby/default.nix
+++ hosts/ruby/default.nix
@@ -11,6 +11,12 @@
   networking.hostName = "ruby";
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
+  environment.etc = {
+    issue = {
+      text = ''\e[31mWelcome to Ruby!\e[0m'';
+    };
+  };
+
   environment.loginShellInit = ''
     [[ "$(tty)" == /dev/tty1 ]] && sway
   '';
