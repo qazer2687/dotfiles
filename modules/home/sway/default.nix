2f3b6b8a010f7bb05600d909ef4172653aad13c6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-02-10 13:46:55 +0000

    feat: sway improvements

--- modules/home/sway/default.nix
+++ modules/home/sway/default.nix
@@ -133,6 +133,22 @@ in {
           swaybar_command waybar
         }
       '';
+
+      extraSessionCommands = ''
+        export XDG_SESSION_TYPE=wayland
+        export XDG_CURRENT_DESKTOP=sway
+        export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+        export QT_AUTO_SCREEN_SCALE_FACTOR=0
+        export QT_SCALE_FACTOR=1
+        export GDK_SCALE=1
+        export GDK_DPI_SCALE=1
+        export MOZ_ENABLE_WAYLAND=1
+      '';
+
+      wrapperFeatures = {
+          base = true;
+          gtk = true;
+        };
     };
   };
 }
