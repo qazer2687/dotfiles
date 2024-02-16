a82a24ae1f2936d3f731a9296df4d4d4c7c98d8c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-02-16 22:20:38 +0000

    feat: nixos module for mailserver

--- containers/mailserver/default.nix
+++ containers/mailserver/default.nix
@@ -3,14 +3,6 @@
   config,
   ...
 }: {
-
-  imports = [
-    (builtins.fetchTarball {
-      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-23.05/nixos-mailserver-nixos-23.05.tar.gz";
-      sha256 = "1ngil2shzkf61qxiqw11awyl81cr7ks2kv3r3k243zz7v2xakm5c";
-    })
-  ];
-
   options.modules.containers.mailserver.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.containers.mailserver.enable {
