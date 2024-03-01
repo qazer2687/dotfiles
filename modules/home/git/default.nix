5bb93ac0a92240e0680042f90d6ffcfb86be47de
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-03-01 11:41:43 +0000

    fix: sops

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -8,8 +8,8 @@
   config = lib.mkIf config.modules.git.enable {
     programs.git = {
       enable = true;
-      userName = config.sops.secrets.github.user-name.path;
-      userEmail = config.sops.secrets.github.user-email.path;
+      userName = config.sops.secrets.github-user-name.path;
+      userEmail = config.sops.secrets.github-user-email.path;
     };
   };
 }
