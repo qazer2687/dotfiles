99a072843de845fa1b717a3b152f0ea80b6c32f4
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-03-11 22:15:18 +0000

    feat: bluetooth support

---
+++ modules/nixos/bluetooth/default.nix
@@ -0,0 +1,16 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.modules.bluetooth.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.modules.bluetooth.enable {
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    services.blueman.enable = true;
+  };
+}
