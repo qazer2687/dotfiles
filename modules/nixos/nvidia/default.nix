0ff8f51e51af37fcfc3c9de5d1ceaceb6b681258
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-03-20 11:53:59 +0000

    fix

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -14,5 +14,24 @@
         nvidiaSettings = true;
       };
     };
+    programs.gamemode = {
+      enable = true;
+      enableRenice = false;
+      settings = {
+        general = {
+          desiredgov = "performance";
+          defaultgov = "powersave";
+        };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 0;
+          nv_powermizer_mode = 1; # "Prefer Maximum Performance"
+        };
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode Enabled'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode Disabled'";
+        };
+      };
+    };
   };
 }
