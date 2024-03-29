fba77b14f4f3acbb6c6986e92aa336d784e22259
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-03-29 14:05:13 +0000

    fix

--- modules/home/wofi/default.nix
+++ modules/home/wofi/default.nix
@@ -11,7 +11,7 @@
       enable = true;
       settings = {
         mode = "drun";
-        prompt = "";
+        prompt = "\b";
         allow_images = "false";
         insensitive = "true";
         no_actions = "true";
