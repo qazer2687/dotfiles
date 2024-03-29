45888fe4db8aa765ef4a77891e01fe3badf9a47c
alexvasilkovski <alexvasilkovski@outlook.com>
2024-03-29 13:41:42 +0000

    fix

--- modules/home/wofi/default.nix
+++ modules/home/wofi/default.nix
@@ -38,7 +38,10 @@
         }
 
         #entry {
-          margin: 8px;
+          margin-left: 8px;
+          margin-right: 8px;
+          margin-top: 4px;
+          margin-bottom: 4px;
           padding: 8px;
           color: #9699b7;
           border-radius: 5px;
