e5ec4d4528a05af6bcf40804f5f194202ed32ab3
alexvasilkovski <alexvasilkovski@outlook.com>
2024-03-29 11:54:28 +0000

    fix

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -24,7 +24,7 @@
       settings = [
         {
           layer = "top";
-          margin = "8 8 0 8"
+          margin = "8 8 0 8";
           modules-left = [ "clock" "sway/workspaces" "mpris"];
           modules-center = [];
           modules-right = [ "memory" "pulseaudio" "disk" "backlight" "network" "battery"];
