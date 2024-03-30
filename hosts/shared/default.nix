56117643cdf5db9d6abc16c745c2aadd62a9b860
alexvasilkovski <alexvasilkovski@outlook.com>
2024-03-30 23:58:27 +0000

    fix

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -93,6 +93,7 @@ in {
   environment = {
     defaultPackages = lib.mkForce [];
     sessionVariables = {
+      GTK_THEME = "Adwaita-dark";
       NIXPKGS_ALLOW_UNFREE = "1";
       NIXPKGS_ALLOW_INSECURE = "1";
     };
