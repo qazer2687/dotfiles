c00cb96939433c53a978a6d3fd20af8bfd85a788
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-04-06 15:28:20 +0100

    temp fix

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -8,9 +8,9 @@
   config = lib.mkIf config.modules.git.enable {
     programs.git = {
       enable = true;
-      userName = "q";
+      userName = "qazer2687";
       #userName = config.sops.secrets."github-user-name".path;
-      userEmail = "114782572+qazer2687@users.noreply.github.com";
+      userEmail = "***REMOVED***@gmail.com";
       #userEmail = config.sops.secrets."github-user-email".path;
     };
   };
