f6b2b235cd8b4b38e138805df1b7d4ebbf0ddf08
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-04-20 23:38:45 +0100

    half fix

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -12,6 +12,14 @@
       gamescopeSession.enable = true;
       package = pkgs.steam.override {
         extraPkgs = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
           libkrb5
           keyutils
         ];
