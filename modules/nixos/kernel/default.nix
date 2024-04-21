3216392fd688b924d5c032330a729c9cacceddb2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-04-21 18:14:13 +0100

    slim down some modules

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -9,11 +9,22 @@
   config = lib.mkIf config.modules.kernel.enable {
     boot = {
       blacklistedKernelModules = ["pcspkr"];
-      kernelParams = ["nouveau" "fbcon=nodefer" "bgrt_disable" "quiet" "systemd.show_status=false" "rd.udev.log_level=0" "vt.global_cursor_default=0" "i915.enable_psr=0"];
+      kernelParams = [
+        "fbcon=nodefer"
+        "bgrt_disable"
+        "quiet"
+        "systemd.show_status=false"
+        "rd.udev.log_level=0"
+        "vt.global_cursor_default=0"
+        "i915.enable_psr=0"
+        "ipv6.disable=1"
+        "mitigations=off"
+        "kernel.nmi_watchdog=0"
+      ];
       consoleLogLevel = 0;
       initrd.verbose = false;
       supportedFilesystems = ["ntfs"];
-      kernelPackages = pkgs.linuxPackages_zen;
+      kernelPackages = pkgs.linuxPackages_latest;
     };
   };
 }
--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -7,26 +7,20 @@
   options.modules.nvidia.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.nvidia.enable {
-
-    # gamemode
+    services.xserver.videoDrivers = ["nvidia"];
+    hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+    };
     programs.gamemode = {
       enable = true;
       enableRenice = true;
-      settings = {
-        general = {
-          desiredgov = "performance";
-          defaultgov = "powersave";
-        };
-        gpu = {
-          apply_gpu_optimisations = "accept-responsibility";
-          gpu_device = 0;
-          nv_powermizer_mode = 1; # "Prefer Maximum Performance"
-        };
-        custom = {
-          start = "${pkgs.libnotify}/bin/notify-send 'GameMode Enabled'";
-          end = "${pkgs.libnotify}/bin/notify-send 'GameMode Disabled'";
-        };
-      };
     };
   };
 }
