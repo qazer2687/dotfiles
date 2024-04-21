06436370ed3777947596ad3a1965a76e9521c096
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-04-21 18:14:13 +0100

    slim down some modules

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -9,27 +9,10 @@
   config = lib.mkIf config.modules.steam.enable {
     programs.steam = {
       enable = true;
-      gamescopeSession = {
-        enable = true;
-        env = {
-          WLR_RENDERER = "vulkan";
-          SDL_VIDEODRIVER = "x11";
-        };
-      };
       package = pkgs.steam.override {
         extraPkgs = pkgs: with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
           libkrb5
           keyutils
-          gamescope
-          mangohud
         ];
       };
     };
