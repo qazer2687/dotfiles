4c78ff1791f80ebfb4fcd002efdb6d22f6b43497
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-04-21 21:21:58 +0100

    disable open kernel module

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -10,7 +10,7 @@
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia = {
       modesetting.enable = true;
-      open = true;
+      open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
