48cd2b683d12fa2298e4d9643ddc4e66372ce2cc
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-04-21 20:48:38 +0100

    deps

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -11,8 +11,21 @@
       enable = true;
       package = pkgs.steam.override {
         extraPkgs = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
           libkrb5
           keyutils
+          gamemode
+          mangohud
+          openssl
+          openssl_1_1
+          openssl_3_1
         ];
       };
     };
