c814cc7f7e5dc142661e601dd3b57c43f3b6093b
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-04-21 21:29:09 +0100

    fixes

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -16,15 +16,17 @@
         "systemd.show_status=false"
         "rd.udev.log_level=0"
         "vt.global_cursor_default=0"
-        "nvidia_drm.fbdev=1"
+        #"nvidia_drm.fbdev=1"
         "mitigations=off"
         "kernel.nmi_watchdog=0"
+        "vsyscall=emulate"
         "clearcpuid=304" # disable avx-512
+        "clearcpuid=514" # disable umip (sgdt)
       ];
       consoleLogLevel = 0;
       initrd.verbose = false;
       supportedFilesystems = ["ntfs"];
-      kernelPackages = pkgs.linuxPackages_latest;
+      kernelPackages = pkgs.linuxPackages_zen;
     };
   };
 }
