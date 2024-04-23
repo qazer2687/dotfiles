6bce0cd2703816c9e9dd88f1427c21a842bf90bb
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-04-23 18:27:30 +0100

    things

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -13,16 +13,17 @@
         "fbcon=nodefer"
         "bgrt_disable"
         "quiet"
-        "systemd.show_status=false"
+        "rd.systemd.show_status=false"
         "rd.udev.log_level=0"
+        "udev.log_priority=3"
         "vt.global_cursor_default=0"
-        "nvidia_drm.fbdev=1"
-        "nvidia-drm.modeset=1" # required by gamescope
+        #"nvidia_drm.fbdev=1"
+        #"nvidia-drm.modeset=1" # required by gamescope
         "mitigations=off"
         "kernel.nmi_watchdog=0"
-        "vsyscall=emulate"
-        "clearcpuid=304" # disable avx-512
-        "clearcpuid=514" # disable umip (sgdt)
+        #"vsyscall=emulate"
+        #"clearcpuid=304" # disable avx-512
+        #"clearcpuid=514" # disable umip (sgdt)
       ];
       consoleLogLevel = 0;
       initrd.verbose = false;
