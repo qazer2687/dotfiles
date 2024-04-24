28b6be105fad19192a498431007915ecfb8b0a6b
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-04-24 10:51:02 +0100

    fix

--- modules/nixos/tlp/default.nix
+++ modules/nixos/tlp/default.nix
@@ -5,7 +5,7 @@
 }: {
   options.modules.tlp.enable = lib.mkEnableOption "";
 
-  config = lib.mkIf config.systemModules.tlp.enable {
+  config = lib.mkIf config.modules.tlp.enable {
     services.tlp = {
       enable = true;
       settings = {
