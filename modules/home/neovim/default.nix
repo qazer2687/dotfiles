d1368c6b9db250d00625c5e25dec7d8f9a84dac4
Alex Vasilkovski <alexvasilkosvki@outlook.com>
2024-04-24 13:16:42 +0100

    feat: nixvim config

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -8,6 +8,40 @@
   config = lib.mkIf config.modules.neovim.enable {
     programs.nixvim = {
       enable = true;
+
+      opts = {
+        shiftwidth = 2;
+        relativenumber = false;
+        number = true;
+        updatetime = 100;
+      };
+
+      plugins = {
+        lightline = {
+          enable = true;
+        };
+
+        nvim-tree = {
+          enable = true;
+          autoReloadOnWrite = true;
+        };
+
+        auto-save = {
+          enable = true;
+          enableAutoSave = true;
+        };
+
+        lsp = {
+          enable = true;
+          servers = {
+            nixd.enable = true;
+          };
+        };
+      };
+
+      colorschemes = {
+        catppuccin.enable = true;
+      };
     };
   };
 }
