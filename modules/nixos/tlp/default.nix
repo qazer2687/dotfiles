95bdce1505924280f2a25da22675d8c36b6d59c7
alexvasilkovski <alexvasilkovski@outlook.com>
2024-04-24 10:50:16 +0100

    tlp

---
+++ modules/nixos/tlp/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options.modules.tlp.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.systemModules.tlp.enable {
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        TLP_DEFAULT_MODE = "BAT";
+        USB_AUTOSUSPEND = 1;
+        TLP_PERSISTENT_DEFAULT = 0; # more power when plugged in
+        RUNTIME_PM_ALL = 1;
+        SOUND_POWER_SAVE_ON_AC = 1;
+        SOUND_POWER_SAVE_ON_BAT = 1;
+        MEM_SLEEP_ON_AC = "deep";
+        MEM_SLEEP_ON_BAT = "deep";
+        WIFI_PWR_ON_AC = "on";
+        WIFI_PWR_ON_BAT = "on";
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 100;
+      };
+    };
+  };
+}
