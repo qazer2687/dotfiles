3e9d490bdc9929b0482e7e1846363ddef27c36d6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-05-27 00:59:00 +0100

    fix syntax

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -2,26 +2,26 @@
   lib,
   config,
   ...
-}: {
-  let aliases = {
+}: let
+  aliases = {
     "check" = "alejandra -q **/* && deadnix -e && statix fix";
     "rebuild" = "sudo nixos-rebuild switch --flake github:qazer2687/dotfiles#$(hostname) --refresh --option eval-cache false";
 
     "ls" = "eza --colour=always --icons=always --all";
   };
-  in {
-    options.modules.fish.enable = lib.mkEnableOption "";
+in {
+  options.modules.fish.enable = lib.mkEnableOption "";
 
-    config = lib.mkIf config.modules.fish.enable {
-      programs.fish = {
-        enable = true;
-        interactiveShellInit = ''
-          set fish_greeting # Disable greeting
-        '';
-        shellAliases = aliases;
-      };
-
-      programs.eza.enable = true;
+  config = lib.mkIf config.modules.fish.enable {
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      shellAliases = aliases;
     };
-  }
+
+    programs.eza.enable = true;
+  };
 }
+
