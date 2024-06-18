511336e9d6c969c48e32f031d011abbf9cb3d7a6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-18 11:19:16 +0100

    fix

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -6,6 +6,9 @@
   options.modules.emacs.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.emacs.enable {
+    programs.emacs = {
+      enable = true;
+    };
     services.emacs = {
       enable = true;
       defaultEditor = true;
