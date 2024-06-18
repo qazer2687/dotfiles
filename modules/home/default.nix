7b259f423e03ad5189dcb62fa7249963475707c9
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-18 11:07:45 +0100

    enable emacs

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -17,5 +17,6 @@
     ./keyring
     ./polybar
     ./dunst
+    ./emacs
   ];
 }
---
+++ modules/home/emacs/default.nix
@@ -0,0 +1,14 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options.modules.emacs.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.modules.emacs.enable {
+    programs.emacs = {
+      enable = true;
+      defaultEditor = true;
+    };
+  };
+}
