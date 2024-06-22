4bb39ce1064d4e8d9a0170462a80a5bb1e2736a6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-23 00:55:15 +0100

    waybar

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -142,13 +142,12 @@
 
       style = ''
         * {
-          font-family: LiterationMono Nerd Font;
+          font-family: FiraCode Mono Nerd Font;
           font-size: 12px;
           background-color: transparent;
           border-radius: 4px;
           margin-left: 2px;
           margin-right: 2px;
-          align-items: center;
         }
 
         window#waybar {
