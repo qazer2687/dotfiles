3d9308ae0c5ed82a34546c13c66c2a4b6448e03f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-23 22:52:40 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -98,7 +98,7 @@
   };
 
   darwinConfigurations = {
-    onyx = nix-darwin.lib.darwinSystem {
+    onyx = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       pkgs = import inputs.nixpkgs { system = "aarch64-darwin"; };
       specialArgs = { inherit inputs; };
