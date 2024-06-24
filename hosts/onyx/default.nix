83a2d2ba5be68cfb0d705e32fc0e8a036fe78eb2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-24 16:58:00 +0100

    fix

--- hosts/onyx/default.nix
+++ hosts/onyx/default.nix
@@ -59,8 +59,8 @@
   modules = {
     skhd.enable = true;
     yabai.enable = true;
-    #spacebar.enable = true;
-    sketchybar.enable = true;
+    spacebar.enable = true;
+    #sketchybar.enable = true;
   };
 
   system.stateVersion = 4;
