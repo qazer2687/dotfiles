0fa55ee35c89f5f42ce322cc936b2775d787829c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-24 13:43:46 +0100

    fix

--- hosts/onyx/default.nix
+++ hosts/onyx/default.nix
@@ -39,8 +39,6 @@
 
   ## requires manual chsh
   programs.fish.enable = true;
-  ## add fish to /etc/shells
-  environment.shells = with pkgs; [ fish ];
 
   security.pam.enableSudoTouchIdAuth = true;
 
