0aeb77538c3b9cabc46b514585aaad6e2a8b5754
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-06-24 17:21:08 +0100

    fix

--- modules/nixos-darwin/spacebar/default.nix
+++ modules/nixos-darwin/spacebar/default.nix
@@ -30,7 +30,7 @@
           battery_icon_color = "0xffd75f5f";
           dnd_icon_color = "0xffa8a8a8";
           clock_icon_color = "0xffa8a8a8";
-          clock_icon = "";
+          clock_icon = "🕰";
           power_icon_strip = "";
           space_icon = "";
           space_icon_color = "0xffffffff";
