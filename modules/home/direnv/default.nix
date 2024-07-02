f43a7c7d02cff5e0d9f6920baa0de94f97004006
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-02 21:35:28 +0100

    fix

--- modules/home/direnv/default.nix
+++ modules/home/direnv/default.nix
@@ -9,7 +9,7 @@
   config = lib.mkIf config.modules.direnv.enable {
     programs.direnv = {
       enable = true;
-      enableFishIntegration = true;
+      #enableFishIntegration = true; # broken for some reason
       nix-direnv = {
         enable = true;
       };
