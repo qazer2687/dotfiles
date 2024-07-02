422858a9351776c96ba9f26d3e01b9aac47b6e3a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-02 21:52:52 +0100

    typo fix

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -6,7 +6,7 @@
   aliases = {
     "check" = ''nix-shell -p alejandra -p deadnix -p statix --command "alejandra -q **/* && deadnix -e && statix fix"'';
     "nrebuild" = "sudo nixos-rebuild switch --flake github:qazer2687/dotfiles#$(hostname) --refresh --option eval-cache false";
-    "debuild" = "darwin-rebuild switch --flake github:qazer2687/dotfiles#$(hostname) --refresh --option eval-cache false";
+    "drebuild" = "darwin-rebuild switch --flake github:qazer2687/dotfiles#$(hostname) --refresh --option eval-cache false";
     "ls" = "eza --colour=always --icons=always --all";
   };
 in {
