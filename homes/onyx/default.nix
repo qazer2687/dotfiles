15c5300d42c4390e5429cee5ce853e0314c0ed39
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-02 20:02:50 +0100

    update alacritty config

--- homes/onyx/default.nix
+++ homes/onyx/default.nix
@@ -23,7 +23,6 @@
   };
 
   home.stateVersion = "24.05";
-  home.homeDirectory = "/home/alex";
 
   sops.defaultSopsFile = ../../../secrets/default.yaml;
   sops.age.sshKeyPaths = ["/home/alex/.ssh/id_ed25519"];
