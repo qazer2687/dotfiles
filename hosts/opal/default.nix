0915d1ba92f5a611c893bcb91908d0fd17d17601
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-11 22:58:19 +0100

    network changes

--- hosts/opal/default.nix
+++ hosts/opal/default.nix
@@ -18,42 +18,23 @@
     desktopManager.gnome.enable = true;
   };
 
-  networking = {
-    networkmanager.enable = true;
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [
-        22 # SSH
-        3000 # Gitea
-        8096 # Jellyfin
-        9090 # Cockpit
-        25565 # Minecraft
-      ];
-    };
-  };
-  systemd = {
-    services.NetworkManager-wait-online.enable = false;
-    network.wait-online.enable = false;
-  };
+  networking.firewall.allowedTCPPorts = [
+    22 # SSH
+    3000 # Gitea
+    8096 # Jellyfin
+    9090 # Cockpit
+    25565 # Minecraft
+  ];
 
   services.cockpit = {
     enable = true;
   };
 
-  services.jellyfin = {
-    enable = true;
-    user = "alex";
-  };
-
-  environment.systemPackages = with pkgs; [
-    qbittorrent-nox
-    flood
-  ];
-
   modules = {
     kernel.enable = true;
     systemd-boot.enable = true;
     pipewire.enable = true;
+    networkmanager.enable = true;
 
     containers = {
       homepage.enable = true;
