c61e2f6ab09b70b57dbcdffcc7c142eac85db6fc
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-16 17:39:36 +0100

    enable nixos_ozone_wl

--- modules/home/sway/default.nix
+++ modules/home/sway/default.nix
@@ -154,13 +154,8 @@ in {
         export XDG_SESSION_TYPE=wayland
         export XDG_CURRENT_DESKTOP=sway
         export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-        #export QT_AUTO_SCREEN_SCALE_FACTOR=0
-        #export QT_SCALE_FACTOR=1
-        #export GDK_SCALE=1
-        #export GDK_DPI_SCALE=1
         export MOZ_ENABLE_WAYLAND=1
-        #export OZONE_PLATFORM=wayland
-        #export OBSIDIAN_USE_WAYLAND=1
+        export NIXOS_OZONE_WL=1
       '';
 
       wrapperFeatures = {
