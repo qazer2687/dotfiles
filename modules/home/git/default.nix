38b744a14e2673afd8f02979aa5bd81a8d45eaa2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-17 18:58:48 +0100

    git module

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -11,6 +11,16 @@
       userName = "qazer2687";
       userEmail = "114782572+qazer2687@users.noreply.github.com";
       # TODO: use sops to retrieve details
+      extraConfig = {
+        credential.helper = "
+          ${
+            pkgs.git.override {
+              withLibsecret = true;
+            }
+          }
+          /bin/git-credential-libsecret
+        ";
+      };
     };
   };
 }
