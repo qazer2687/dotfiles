52a5860fa104b667cf88916150f4629e9e3e4f48
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-22 19:13:13 +0100

    starship

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -13,32 +13,32 @@
       enableTransience = true;
       settings = {
         format = ''
-          [](#9A348E)\
-          $os\
-          $username\
-          [](bg:#DA627D fg:#9A348E)\
-          $directory\
-          [](fg:#DA627D bg:#FCA17D)\
-          $git_branch\
-          $git_status\
-          [](fg:#FCA17D bg:#86BBD8)\
-          $c\
-          $elixir\
-          $elm\
-          $golang\
-          $gradle\
-          $haskell\
-          $java\
-          $julia\
-          $nodejs\
-          $nim\
-          $rust\
-          $scala\
-          [](fg:#86BBD8 bg:#06969A)\
-          $docker_context\
-          [](fg:#06969A bg:#33658A)\
-          $time\
-          [ ](fg:#33658A)\
+          [](#9A348E)
+          $os
+          $username
+          [](bg:#DA627D fg:#9A348E)
+          $directory
+          [](fg:#DA627D bg:#FCA17D)
+          $git_branch
+          $git_status
+          [](fg:#FCA17D bg:#86BBD8)
+          $c
+          $elixir
+          $elm
+          $golang
+          $gradle
+          $haskell
+          $java
+          $julia
+          $nodejs
+          $nim
+          $rust
+          $scala
+          [](fg:#86BBD8 bg:#06969A)
+          $docker_context
+          [](fg:#06969A bg:#33658A)
+          $time
+          [ ](fg:#33658A)
         '';
 
         username = {
