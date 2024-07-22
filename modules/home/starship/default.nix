922f7a670a3f31b296db27342ee417608e5152e3
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-22 19:35:37 +0100

    starship

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -12,122 +12,302 @@
       enableFishIntegration = true;
       enableTransience = true;
       settings = {
-        format = "[█](#9A348E)$os$username[](bg:#DA627D fg:#9A348E)$directory[](fg:#DA627D bg:#FCA17D)$git_branch$git_status[](fg:#FCA17D bg:#86BBD8)$c$elixir$elm$golang$gradle$haskell$java$julia$nodejs$nim$rust$scala[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$time[ ](fg:#33658A)";
-
-        username = {
-          show_always = true;
-          style_user = "bg:#9A348E";
-          style_root = "bg:#9A348E";
-          format = "[$user ]($style)";
-          disabled = false;
+        character = {
+          success_symbol = "[>](bold green)";
+          error_symbol = "[x](bold red)";
+          vimcmd_symbol = "[<](bold green)";
         };
 
-        directory = {
-          style = "bg:#DA627D";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-          substitutions = {
-            Documents = "󰈙 ";
-            Downloads = " ";
-            Music = " ";
-            Pictures = " ";
-          };
-        };
-
-        c = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
-        };
-
-        docker_context = {
-          symbol = " ";
-          style = "bg:#06969A";
-          format = "[ $symbol $context ]($style)";
-        };
-
-        elixir = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
-        };
-
-        elm = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
-        };
-
-        git_branch = {
-          symbol = "";
-          style = "bg:#FCA17D";
-          format = "[ $symbol $branch ]($style)";
+        git_commit = {
+          tag_symbol = " tag ";
         };
 
         git_status = {
-          style = "bg:#FCA17D";
-          format = "[$all_status$ahead_behind ]($style)";
+          ahead = ">";
+          behind = "<";
+          diverged = "<>";
+          renamed = "r";
+          deleted = "x";
+        };
+
+        aws = {
+          symbol = "aws ";
+        };
+
+        azure = {
+          symbol = "az ";
+        };
+
+        bun = {
+          symbol = "bun ";
+        };
+
+        c = {
+          symbol = "C ";
+        };
+
+        cobol = {
+          symbol = "cobol ";
+        };
+
+        conda = {
+          symbol = "conda ";
+        };
+
+        crystal = {
+          symbol = "cr ";
+        };
+
+        cmake = {
+          symbol = "cmake ";
+        };
+
+        daml = {
+          symbol = "daml ";
+        };
+
+        dart = {
+          symbol = "dart ";
+        };
+
+        deno = {
+          symbol = "deno ";
+        };
+
+        dotnet = {
+          symbol = ".NET ";
+        };
+
+        directory = {
+          read_only = " ro";
+        };
+
+        docker_context = {
+          symbol = "docker ";
+        };
+
+        elixir = {
+          symbol = "exs ";
+        };
+
+        elm = {
+          symbol = "elm ";
+        };
+
+        fennel = {
+          symbol = "fnl ";
+        };
+
+        fossil_branch = {
+          symbol = "fossil ";
+        };
+
+        gcloud = {
+          symbol = "gcp ";
+        };
+
+        git_branch = {
+          symbol = "git ";
+        };
+
+        gleam = {
+          symbol = "gleam ";
         };
 
         golang = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "go ";
         };
 
         gradle = {
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "gradle ";
         };
 
-        haskell = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+        guix_shell = {
+          symbol = "guix ";
+        };
+
+        hg_branch = {
+          symbol = "hg ";
         };
 
         java = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "java ";
         };
 
         julia = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "jl ";
+        };
+
+        kotlin = {
+          symbol = "kt ";
+        };
+
+        lua = {
+          symbol = "lua ";
         };
 
         nodejs = {
-          symbol = "";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "nodejs ";
+        };
+
+        memory_usage = {
+          symbol = "memory ";
+        };
+
+        meson = {
+          symbol = "meson ";
+        };
+
+        nats = {
+          symbol = "nats ";
         };
 
         nim = {
-          symbol = "󰆥 ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "nim ";
+        };
+
+        nix_shell = {
+          symbol = "nix ";
+        };
+
+        ocaml = {
+          symbol = "ml ";
+        };
+
+        opa = {
+          symbol = "opa ";
+        };
+
+        os = {
+          symbols = {
+            AIX = "aix ";
+            Alpaquita = "alq ";
+            AlmaLinux = "alma ";
+            Alpine = "alp ";
+            Amazon = "amz ";
+            Android = "andr ";
+            Arch = "rch ";
+            Artix = "atx ";
+            CentOS = "cent ";
+            Debian = "deb ";
+            DragonFly = "dfbsd ";
+            Emscripten = "emsc ";
+            EndeavourOS = "ndev ";
+            Fedora = "fed ";
+            FreeBSD = "fbsd ";
+            Garuda = "garu ";
+            Gentoo = "gent ";
+            HardenedBSD = "hbsd ";
+            Illumos = "lum ";
+            Kali = "kali ";
+            Linux = "lnx ";
+            Mabox = "mbox ";
+            Macos = "mac ";
+            Manjaro = "mjo ";
+            Mariner = "mrn ";
+            MidnightBSD = "mid ";
+            Mint = "mint ";
+            NetBSD = "nbsd ";
+            NixOS = "nix ";
+            OpenBSD = "obsd ";
+            OpenCloudOS = "ocos ";
+            openEuler = "oeul ";
+            openSUSE = "osuse ";
+            OracleLinux = "orac ";
+            Pop = "pop ";
+            Raspbian = "rasp ";
+            Redhat = "rhl ";
+            RedHatEnterprise = "rhel ";
+            RockyLinux = "rky ";
+            Redox = "redox ";
+            Solus = "sol ";
+            SUSE = "suse ";
+            Ubuntu = "ubnt ";
+            Ultramarine = "ultm ";
+            Unknown = "unk ";
+            Void = "void ";
+            Windows = "win ";
+          };
+        };
+
+        package = {
+          symbol = "pkg ";
+        };
+
+        perl = {
+          symbol = "pl ";
+        };
+
+        php = {
+          symbol = "php ";
+        };
+
+        pijul_channel = {
+          symbol = "pijul ";
+        };
+
+        pulumi = {
+          symbol = "pulumi ";
+        };
+
+        purescript = {
+          symbol = "purs ";
+        };
+
+        python = {
+          symbol = "py ";
+        };
+
+        quarto = {
+          symbol = "quarto ";
+        };
+
+        raku = {
+          symbol = "raku ";
+        };
+
+        ruby = {
+          symbol = "rb ";
         };
 
         rust = {
-          symbol = "";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "rs ";
         };
 
         scala = {
-          symbol = " ";
-          style = "bg:#86BBD8";
-          format = "[ $symbol ($version) ]($style)";
+          symbol = "scala ";
         };
 
-        time = {
-          disabled = false;
-          time_format = "%R";
-          style = "bg:#33658A";
-          format = "[ $time ]($style)";
+        spack = {
+          symbol = "spack ";
+        };
+
+        solidity = {
+          symbol = "solidity ";
+        };
+
+        status = {
+          symbol = "[x](bold red) ";
+        };
+
+        sudo = {
+          symbol = "sudo ";
+        };
+
+        swift = {
+          symbol = "swift ";
+        };
+
+        typst = {
+          symbol = "typst ";
+        };
+
+        terraform = {
+          symbol = "terraform ";
+        };
+
+        zig = {
+          symbol = "zig ";
         };
       };
     };
