b3ba029ea31b17efdfe7ba76da8294f4bf366403
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-24 21:24:38 +0100

    test

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.modules.firefox.enable = lib.mkEnableOption "";
@@ -24,6 +25,29 @@
           statshunters
         ];
         */
+
+        lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 {
+          programs.firefox.profiles.default.settings = {
+            "media.gmp-widevinecdm.version" = pkgs.widevinecdm-aarch64.version;
+            "media.gmp-widevinecdm.visible" = true;
+            "media.gmp-widevinecdm.enabled" = true;
+            "media.gmp-widevinecdm.autoupdate" = false;
+            "media.eme.enabled" = true;
+            "media.eme.encrypted-media-encryption-scheme.enabled" = true;
+          };
+
+          home.file."firefox-widevinecdm" = {
+            enable = true;
+            target = ".mozilla/firefox/default/gmp-widevinecdm";
+            source = pkgs.runCommandLocal "firefox-widevinecdm" { } ''
+              out=$out/${pkgs.widevinecdm-aarch64.version}
+              mkdir -p $out
+              ln -s ${pkgs.widevinecdm-aarch64}/manifest.json $out/manifest.json
+              ln -s ${pkgs.widevinecdm-aarch64}/libwidevinecdm.so $out/libwidevinecdm.so
+            '';
+            recursive = true;
+          };
+        };
 
         userChrome = ''
           @-moz-document url(chrome://browser/content/browser.xhtml){
---
+++ packages/widevinecdm-aarch64/default.nix
@@ -0,0 +1,43 @@
+{ stdenv
+, fetchFromGitHub
+, fetchurl
+, python3
+, squashfsTools
+, nspr
+}:
+let
+  widevine-installer = fetchFromGitHub {
+    owner = "AsahiLinux";
+    repo = "widevine-installer";
+    rev = "7a3928fe1342fb07d96f61c2b094e3287588958b";
+    sha256 = "sha256-XI1y4pVNpXS+jqFs0KyVMrxcULOJ5rADsgvwfLF6e0Y=";
+  };
+  lacros-image = fetchurl {
+    url =
+      let
+        distfiles_base = "https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles";
+        lacros_name = "chromeos-lacros-arm64-squash-zstd";
+        lacrosVersion = "120.0.6098.0";
+      in
+      "${distfiles_base}/${lacros_name}-${lacrosVersion}";
+    hash = "sha256-OKV8w5da9oZ1oSGbADVPCIkP9Y0MVLaQ3PXS3ZBLFXY=";
+  };
+in
+stdenv.mkDerivation {
+  name = "widevine";
+  version = "4.10.2662.3";
+
+  dontUnpack = true;
+  dontBuild = true;
+
+  buildInputs = [ python3 squashfsTools ];
+
+  installPhase = ''
+    mkdir $out
+    unsquashfs -q ${lacros-image} 'WidevineCdm/*'
+    python3 ${widevine-installer}/widevine_fixup.py squashfs-root/WidevineCdm/_platform_specific/cros_arm64/libwidevinecdm.so $out/libwidevinecdm.so
+    mv squashfs-root/WidevineCdm/manifest.json $out/
+    mv squashfs-root/WidevineCdm/LICENSE $out/
+    patchelf --add-rpath ${nspr}/lib $out/libwidevinecdm.so 
+  '';
+}
