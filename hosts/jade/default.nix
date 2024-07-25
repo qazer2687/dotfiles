672b4b8313b2ed73763c144e0975b3c9e6ccdc20
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-25 16:46:34 +0100

    update

--- hosts/jade/default.nix
+++ hosts/jade/default.nix
@@ -1,6 +1,9 @@
 {
-  pkgs,
+  inputs,
+  outputs,
   lib,
+  config,
+  pkgs,
   ...
 }: {
   imports = [
@@ -8,31 +11,106 @@
     ../../modules/nixos
   ];
 
-  # require for everything to not shit itself when i rebuild
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
 
-  # required for i3 to start properly
-  services.xserver = {
-    enable = true;
-    displayManager.startx.enable = true;
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = "nix-command flakes";
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
+      keep-derivations = true;
+      keep-outputs = true;
+      auto-optimise-store = true;
+      sandbox = true;
+      require-sigs = false;
+    };
+    channel.enable = false;
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   # Hostname
   networking.hostName = "jade";
 
+  # User
+  users.users = {
+    alex = {
+      initialPassword = "xela";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here...
+      ];
+      extraGroups = ["networkmanager" "wheel" "video" "audio"];
+      shell = pkgs.fish;
+    };
+  };
+
+  # Shell
+  programs.fish.enable = true;
+
+  # Hardware
+  hardware = {
+    graphics = {
+      enable = true;
+    };
+    boot = {
+      loader.efi.efiSysMountPoint = "/boot/efi"; #! Required for everything to not shit itself when I rebuild...
+      kernelParams = [
+        # TODO: Move kernelParams from module to here...
+      ];
+    };
+  };
+
   # Autologin
+  services.getty.autologinUser = "alex";
   environment.loginShellInit = ''
     [[ "$(tty)" == /dev/tty1 ]] && startx
   '';
-  services.getty.autologinUser = "alex";
+
+  # Swap
+  swapDevices = [{
+    device = "/swapfile";
+    size = 16 * 1024;
+  }];
+
+  # Environment
+  environment = {
+    sessionVariables = {
+      # Theme
+      GTK_THEME = "Adwaita-dark";
+
+      # Nixpkgs
+      NIXPKGS_ALLOW_UNFREE = "1";
+      NIXPKGS_ALLOW_INSECURE = "1";
+    };
+    defaultPackages = lib.mkForce [];
+  };
+
+  # X Server
+  services.xserver = {
+    enable = true;
+    displayManager.startx.enable = true;
+  };
 
   # Custom Kernel
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_xanmod ;
-
+  
   # Modules
   modules = {
-    kernel.enable = true;
+    # Network
     networkmanager.enable = true;
+
+    kernel.enable = true;
     nvidia.enable = true;
     pipewire.enable = true;
     systemd-boot.enable = true;
@@ -45,6 +123,13 @@
     # Utilities
     nh.enable = true;
   };
+
+  # SOPS
+  sops.defaultSopsFile = ./secrets/default.yaml;
+
+  # Locale
+  time.timeZone = mkDefault "Europe/London";
+  i18n.defaultLocale = mkDefault "en_GB.UTF-8";
 
   # Did you read the comment?
   system.stateVersion = "23.05";
