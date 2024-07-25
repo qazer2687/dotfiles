7080ca7c1263bd13fe01e16a6e72ba87c7ea05dd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-25 14:34:19 +0100

    test

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    packages = forAllSystems (system: import ./packages nixpkgs.legacyPackages.${system});
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
     overlays = import ./overlays {inherit inputs;};
 
     nixosConfigurations = {
