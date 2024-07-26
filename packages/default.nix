35d7feebff6abd5b9cb6eeb426992468c45840ad
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-26 17:54:34 +0100

    try

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,5 @@
-{
-  self,
-  ...
-}: {
-  widevinecdm-aarch64 = self.packages.callPackage ./widevinecdm-aarch64 {};
+# Custom packages, that can be defined similarly to ones from nixpkgs
+# You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
+{pkgs ? (import ../nixpkgs.nix) {}}: {
+  widevinecdm-aarch64 = pkgs.callPackage ./widevinecdm-aarch64 {};
 }
