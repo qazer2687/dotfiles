b1e92339471ba5d0af49cad8d3258646c5f42799
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-26 20:53:36 +0100

    test

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
   in rec {
     packages = forAllSystems (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = nixpkgs.legacyPackages.${system};
       in
         pkgs
     );
