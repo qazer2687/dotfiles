bcfdfc420bb154358be28ae6b9a1e3686ebf49d4
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-27 16:46:36 +0100

    replace

--- hardware/jet/apple-silicon-support/modules/default.nix
+++ hardware/jet/apple-silicon-support/modules/default.nix
@@ -1,10 +1,5 @@
+{ config, pkgs, lib, ... }:
 {
-  config,
-  pkgs,
-  self,
-  lib,
-  ...
-}: {
   imports = [
     ./kernel
     ./mesa
@@ -14,18 +9,17 @@
   ];
 
   config = let
-    cfg = config.hardware.asahi;
-  in
-    lib.mkIf cfg.enable {
-      nixpkgs.overlays = lib.mkBefore [cfg.overlay];
+      cfg = config.hardware.asahi;
+    in lib.mkIf cfg.enable {
+      nixpkgs.overlays = lib.mkBefore [ cfg.overlay ];
 
       hardware.asahi.pkgs =
         if cfg.pkgsSystem != "aarch64-linux"
         then
-          import pkgs.path {
+          import (pkgs.path) {
             crossSystem.system = "aarch64-linux";
             localSystem.system = cfg.pkgsSystem;
-            overlays = [cfg.overlay];
+            overlays = [ cfg.overlay ];
           }
         else pkgs;
     };
