f93461a9ff4c921e2e08f4a3eb049c29389d1f6e
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-29 00:04:18 +0100

    test

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     packages = forAllSystems (
-      system: import ./packages nixpkgs.legacyPackages.${system}
+      system: import ./packages nixpkgs.legacyPackages.${system} // nixpkgs.legacyPackages.${system}
     );
 
     # Jade
