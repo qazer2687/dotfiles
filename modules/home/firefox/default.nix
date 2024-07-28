fa66afc78087cc0651506539f233ab9135e3ef78
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-07-29 00:19:47 +0100

    fix

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -29,7 +29,7 @@
 
         
         # Asahi Widevine Support
-        settings = lib.mkIf self.packages.stdenv.hostPlatform.isAarch64 {
+        settings = lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 {
           "media.gmp-widevinecdm.version" = self.packages.widevinecdm-aarch64.version;
           "media.gmp-widevinecdm.visible" = true;
           "media.gmp-widevinecdm.enabled" = true;
@@ -97,14 +97,14 @@
     };
 
     # Asahi Widevine Support
-    home.file."firefox-widevinecdm" = lib.mkIf self.packages.stdenv.hostPlatform.isAarch64 {
+    home.file."firefox-widevinecdm" = lib.mkIf pkgs.stdenv.hostPlatform.isAarch64 {
       enable = true;
       target = ".mozilla/firefox/default/gmp-widevinecdm";
-      source = self.packages.runCommandLocal "firefox-widevinecdm" {} ''
+      source = pkgs.runCommandLocal "firefox-widevinecdm" {} ''
         out=$out/${pkgs.widevinecdm-aarch64.version}
         mkdir -p $out
-        ln -s ${pkgs.widevinecdm-aarch64}/manifest.json $out/manifest.json
-        ln -s ${pkgs.widevinecdm-aarch64}/libwidevinecdm.so $out/libwidevinecdm.so
+        ln -s ${self.packages.widevinecdm-aarch64}/manifest.json $out/manifest.json
+        ln -s ${self.packages.widevinecdm-aarch64}/libwidevinecdm.so $out/libwidevinecdm.so
       '';
       recursive = true;
     };
