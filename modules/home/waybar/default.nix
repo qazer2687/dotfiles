021d363fc175270156f579466ba0a434b2f4d6db
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-08-20 23:53:10 +0100

    waybar

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -142,7 +142,7 @@
           box-shadow: none;
         }
         #pulseaudio-slider trough {
-          min-width: 0;
+          min-width: 100px;
           min-height: 10px;
           border-radius: 2px;
         }
