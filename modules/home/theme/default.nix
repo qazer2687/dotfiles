e4faaece5c3fa1834b6bc894772c5ebff36b407c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-08-26 23:03:10 +0100

    few changes

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -7,15 +7,20 @@
   options.modules.theme.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.theme.enable {
-    dconf = {
-      enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    # Dconf
+    dconf.enable = true;
+
+    # HM
+    home = {
+      pointerCursor = {
+        gtk.enable = true;
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+        size = 24;
+      };
     };
-    home.sessionVariables = {
-      GTK_THEME = "Adwaita-dark";
-      XCURSOR_SIZE = 24;
-      GDK_FONT_SCALE = 2;
-    };
+
+    # GTK
     gtk = {
       enable = true;
       iconTheme = {
@@ -26,19 +31,19 @@
         name = "Adwaita";
         package = pkgs.gnome-themes-extra;
       };
-      gtk3.extraConfig = {gtk-application-prefer-dark-theme = 1;};
-      gtk4.extraConfig = {gtk-application-prefer-dark-theme = 1;};
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
     };
+
+    # QT
     qt = {
       enable = true;
       platformTheme.name = "adwaita";
       style.name = "adwaita-dark";
-    };
-    home.pointerCursor = {
-      gtk.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 24;
     };
   };
 }
