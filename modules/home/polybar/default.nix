bfc3012bd7261a358c84f47765d3a497d385b6cd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-02 21:18:19 +0100

    polybar

--- modules/home/polybar/default.nix
+++ modules/home/polybar/default.nix
@@ -7,10 +7,11 @@
   options.modules.polybar.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.polybar.enable {
-    services.polybar = {
-      enable = true;
-      config = builtins.readFile ./config/default;
-      script = builtins.readFile ./config/launch.sh;
-    };
+    home.packages = with pkgs; [polybarFull];
+
+    xdg.configFile."polybar/config.ini".text = builtins.readFile ./config/default;
+
+    xdg.configFile."polybar/launch.sh".text = builtins.readFile ./config/launch.sh;
+    xdg.configFile."polybar/launch.sh".executable = true;
   };
 }
