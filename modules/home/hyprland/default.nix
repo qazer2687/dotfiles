d0b502f029e5ca971f179795c8496a50e0376148
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-08 00:00:55 +0100

    hyprland

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -16,47 +16,30 @@
         general { 
           gaps_in = 6
           gaps_out = 0
-
           border_size = 0
-
-
-          # Set to true enable resizing windows by clicking and dragging on borders and gaps
           resize_on_border = true 
-
-          # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
           allow_tearing = false
-
         }
 
         decoration {
           rounding = 6
-
-          # Change transparency of focused and unfocused windows
           active_opacity = 1.0
           inactive_opacity = 0.9
-
           drop_shadow = true
           shadow_range = 4
           shadow_render_power = 3
           col.shadow = rgba(1a1a1aee)
-
-          # https://wiki.hyprland.org/Configuring/Variables/#blur
           blur {
             enabled = true
             size = 3
             passes = 1
-            
             vibrancy = 0.1696
           }
         }
 
         animations {
           enabled = true
-
-          # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-
           bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
           animation = windows, 1, 7, myBezier
           animation = windowsOut, 1, 7, default, popin 80%
           animation = border, 1, 10, default
@@ -66,7 +49,7 @@
         }
 
         # HiDPI
-        monitor = highres, auto, 2
+        #monitor = highres, auto, 2
 
         # Input
         input {
@@ -106,28 +89,28 @@
         bind = $mod, down, movewindow, d
 
         # Workspace Navigation
-        bind = $mod, 1, workspace 1
-        bind = $mod, 2, workspace 2
-        bind = $mod, 3, workspace 3
-        bind = $mod, 4, workspace 4
-        bind = $mod, 5, workspace 5
-        bind = $mod, 6, workspace 6
-        bind = $mod, 7, workspace 7
-        bind = $mod, 8, workspace 8
-        bind = $mod, 9, workspace 9
-        bind = $mod, 0, workspace 10
+        bind = $mod, 1, workspace, 1
+        bind = $mod, 2, workspace, 2
+        bind = $mod, 3, workspace, 3
+        bind = $mod, 4, workspace, 4
+        bind = $mod, 5, workspace, 5
+        bind = $mod, 6, workspace, 6
+        bind = $mod, 7, workspace, 7
+        bind = $mod, 8, workspace, 8
+        bind = $mod, 9, workspace, 9
+        bind = $mod, 0, workspace, 10
 
         # Workspace Manipulation
-        bind = $mod SHIFT, 1, movecontainer to workspace 1
-        bind = $mod SHIFT, 2, movecontainer to workspace 2
-        bind = $mod SHIFT, 3, movecontainer to workspace 3
-        bind = $mod SHIFT, 4, movecontainer to workspace 4
-        bind = $mod SHIFT, 5, movecontainer to workspace 5
-        bind = $mod SHIFT, 6, movecontainer to workspace 6
-        bind = $mod SHIFT, 7, movecontainer to workspace 7
-        bind = $mod SHIFT, 8, movecontainer to workspace 8
-        bind = $mod SHIFT, 9, movecontainer to workspace 9
-        bind = $mod SHIFT, 0, movecontainer to workspace 10
+        bind = $mod SHIFT, 1, movetoworkspace, 1
+        bind = $mod SHIFT, 2, movetoworkspace, 2
+        bind = $mod SHIFT, 3, movetoworkspace, 3
+        bind = $mod SHIFT, 4, movetoworkspace, 4
+        bind = $mod SHIFT, 5, movetoworkspace, 5
+        bind = $mod SHIFT, 6, movetoworkspace, 6
+        bind = $mod SHIFT, 7, movetoworkspace, 7
+        bind = $mod SHIFT, 8, movetoworkspace, 8
+        bind = $mod SHIFT, 9, movetoworkspace, 9
+        bind = $mod SHIFT, 0, movetoworkspace, 10
 
         # Resize
         binde = [
