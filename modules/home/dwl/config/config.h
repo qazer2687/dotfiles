10d398c486a6657d9dce0ac23f74907210766fad
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-09 00:26:55 +0100

    fix

--- modules/home/dwl/config/config.h
+++ modules/home/dwl/config/config.h
@@ -4,7 +4,9 @@
                         ((hex >> 8) & 0xFF) / 255.0f, \
                         (hex & 0xFF) / 255.0f }
 /* appearance */
-static const int sloppyfocus               = 1;  /* focus follows mouse */
+static const int sloppyfocus = 1; /* focus follows mouse */
+static const int smartgaps                 = 0;  /* 1 means no outer gap when there is only one window */
+static const int monoclegaps               = 0;  /* 1 means outer gaps in monocle layout */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const unsigned int borderpx         = 0;  /* border pixel of windows */
 static const float rootcolor[]             = COLOR(0x222222ff);
