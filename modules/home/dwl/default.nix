6cb3fb06da84431eb791abc7634442ff0ef02218
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-08 22:05:23 +0100

    dwl

--- modules/home/dwl/default.nix
+++ modules/home/dwl/default.nix
@@ -7,7 +7,7 @@
   options.modules.dwl.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.dwl.enable {
-    home.systemPackages = with pkgs; [
+    home.packages = with pkgs; [
       #? The configuration is set in an overlay and defined in the config folder.
       dwl
     ];
