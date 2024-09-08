0f21b6d6ab9ad3e940060c30fff1e6e87855fc43
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-08 23:27:36 +0100

    dwl

--- modules/home/dwl/config/config.h
+++ modules/home/dwl/config/config.h
@@ -121,7 +121,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wofi --show drun", NULL };
+static const char *menucmd[] = { "wofi", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
