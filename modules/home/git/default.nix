04e9ff8aa467d3d5867844e627a6a1ea25bc93a7
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-19 13:30:21 +0100

    github config for college

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -11,5 +11,11 @@
       userName = "qazer2687";
       userEmail = "114782572+qazer2687@users.noreply.github.com";
     };
+
+    home.file.".ssh/config".text = ''
+      Host github.com
+        Hostname ssh.github.com
+        Port 443
+  '';
   };
 }
