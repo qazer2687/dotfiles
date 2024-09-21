31599f3b68d194ec47ed38513d8ce1afba94b916
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-21 16:08:57 +0100

    prismlauncher jet

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -12,6 +12,8 @@
       userEmail = "114782572+qazer2687@users.noreply.github.com";
     };
 
+    #? This is a fix from stackoverflow that allows connecting to github over SSH when port 22 is blocked.
+    #? https://stackoverflow.com/questions/7953806/github-ssh-via-public-wifi-port-22-blocked
     home.file.".ssh/config".text = ''
       Host github.com
         Hostname ssh.github.com
--- modules/home/prismlauncher/default.nix
+++ modules/home/prismlauncher/default.nix
@@ -13,15 +13,19 @@
     ];
 
     home.file = {
+      # 8
       ".jdks/8".source = lib.getBin pkgs.openjdk8;
       ".jdks/8-temurin".source = lib.getBin pkgs.temurin-bin-8;
 
+      # 11
       ".jdks/11".source = lib.getBin pkgs.openjdk11;
 
+      # 17
       ".jdks/17".source = lib.getBin pkgs.openjdk17;
       ".jdks/17-temurin".source = lib.getBin pkgs.temurin-bin-17;
       ".jdks/17-zulu".source = lib.getBin pkgs.zulu17;
 
+      # 21
       ".jdks/21-temurin".source = lib.getBin pkgs.temurin-bin;
     };
   };
