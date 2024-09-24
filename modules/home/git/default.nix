c316a80cc0a4e05e1321cc938867e633e6898e95
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-24 10:09:21 +0100

    fix ssh

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -12,12 +12,20 @@
       userEmail = "114782572+qazer2687@users.noreply.github.com";
     };
 
-    #? This is a fix from stackoverflow that allows connecting to github over SSH when port 22 is blocked.
-    #? https://stackoverflow.com/questions/7953806/github-ssh-via-public-wifi-port-22-blocked
-    home.file.".ssh/config".text = ''
-      Host github.com
-        Hostname ssh.github.com
-        Port 443
-  '';
+    home.file.".ssh/config" = {
+      #? This is a fix from stackoverflow that allows connecting to github over SSH when port 22 is blocked.
+      #? https://stackoverflow.com/questions/7953806/github-ssh-via-public-wifi-port-22-blocked
+      text = ''
+        Host github.com
+          Hostname ssh.github.com
+          Port 443
+      '';
+      #? This is a fix from github that handles the 'bad owner or permissions on ~/.ssh/config' error.
+      #? https://github.com/nix-community/home-manager/issues/322#issuecomment-1856128020
+      onChange = ''
+        echo "${config.home.file.".ssh/config".text}" > ~/.ssh/config
+        chmod 400 ~/.ssh/config
+      '';
+    };
   };
 }
