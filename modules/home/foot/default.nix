f70677df7b8491ae89c0c20ed35beeb4ffc0b48a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-24 14:30:26 +0100

    foot revert

--- modules/home/foot/default.nix
+++ modules/home/foot/default.nix
@@ -15,8 +15,8 @@
       settings = {
         main = {
           #! Managed by Stylix.
-          #font = "Agave:size=11, FiraCode Nerd Font:size=11, Noto Color Emoji:size=8";
-          #font-bold = "Agave:size=11:style=Bold, FiraCode Nerd Font:size=11, Noto Color Emoji:size=8";
+          font = "Agave:size=11, FiraCode Nerd Font:size=11, Noto Color Emoji:size=8";
+          font-bold = "Agave:size=11:style=Bold, FiraCode Nerd Font:size=11, Noto Color Emoji:size=8";
           line-height = "16px";
           pad = "16x16";
         };
