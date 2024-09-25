ecc200af4d825a5aa7133afddcc28d8150ba60d8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-09-25 12:14:02 +0100

    sync

--- modules/nixos/filesystem/default.nix
+++ modules/nixos/filesystem/default.nix
@@ -7,19 +7,17 @@
   options.modules.filesystem.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.filesystem.enable {
-    # Automount External Drives
     services.devmon.enable = true;
     services.gvfs.enable = true;
     services.udisks2.enable = true;
 
-    # iOS Support
+    #? For iOS support.
     services.usbmuxd.enable = true;
     environment.systemPackages = with pkgs; [
       ifuse
       libimobiledevice
     ];
 
-    # Additional Supported Filesystems
     boot = {
       supportedFilesystems = ["ntfs"];
     };
