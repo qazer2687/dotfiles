d0a83a6cc163b0935e96c9d9e72f5eff80d7837c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-10-02 10:03:43 +0100

    firefox css

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -85,6 +85,7 @@
           /* Remove padding, margins, and close buttons from non-selected/non-pinned tabs */
           .tabbrowser-tab {
             padding: 0 !important;
+            height: 20px; /* fix */
           }
           .tabbrowser-tab:not([pinned]) {
             margin-inline-start: 0 !important;
