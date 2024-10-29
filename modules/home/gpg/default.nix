02863be358f0ad2c51191cb4c0f4666d51218ea3
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-10-29 00:24:36 +0000

    add ili public key

--- modules/home/gpg/default.nix
+++ modules/home/gpg/default.nix
@@ -9,13 +9,25 @@
   config = lib.mkIf config.modules.gpg.enable {
     programs.gpg = {
       enable = true;
-
       homedir = "${config.home.homeDirectory}/.gnupg";
-
       mutableKeys = true;
       mutableTrust = false;
-
       package = pkgs.gnupg;
+
+      # Trust Levels
+      # 1 = Unknown
+      # 2 = Never
+      # 3 = Marginal
+      # 4 = Full
+      # 5 = Ultimate
+
+      publicKeys."michaili" = {
+        source = pkgs.fetchurl {
+          url = "https://github.com/MichailiK.gpg";
+          sha256 = "b97af827ff77670cb174f5a6ed040e651b89ab90df02ecb1655209ac6b9efea7";
+        };
+        trust = 4;
+      };
     };
 
     services.gpg-agent = {
