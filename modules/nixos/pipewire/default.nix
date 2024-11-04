fa465040cacf8b5f35bec50c0c8ecb347049940d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-04 19:51:07 +0000

    fix buggy audio

--- modules/nixos/pipewire/default.nix
+++ modules/nixos/pipewire/default.nix
@@ -30,9 +30,9 @@
       extraConfig.pipewire."92-low-latency" = {
         context.properties = {
           default.clock.rate = 48000;
-          default.clock.quantum = 128;
-          default.clock.min-quantum = 128;
-          default.clock.max-quantum = 128;
+          default.clock.quantum = 1024;
+          default.clock.min-quantum = 32;
+          default.clock.max-quantum = 2048;
         };
       };
     };
