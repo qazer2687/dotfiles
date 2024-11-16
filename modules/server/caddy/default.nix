583cbbf089aaf1cbe0839d93b889066cbbbb99bd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-16 22:18:36 +0000

    test

--- modules/server/caddy/default.nix
+++ modules/server/caddy/default.nix
@@ -8,23 +8,35 @@
   config = lib.mkIf config.modules.server.caddy.enable {
     services.caddy = {
       enable = true;
+      extraConfig = ''
+        tls {
+          dns cloudflare {
+            api_token "${sops.secrets."cloudflare-api-key"}"
+        }
+      '';
       virtualHosts."grafana.qazer.org".extraConfig = ''
         reverse_proxy http://opal:3000
+        import cloudflare
       '';
       virtualHosts."pihole.example.org".extraConfig = ''
         reverse_proxy http://opal:3001
+        import cloudflare
       '';
       virtualHosts."prometheus.qazer.org".extraConfig = ''
         reverse_proxy http://opal:9090
+        import cloudflare
       '';
       virtualHosts."node-exporter.qazer.org".extraConfig = ''
         reverse_proxy http://opal:9100
+        import cloudflare
       '';
       virtualHosts."cockpit.qazer.org".extraConfig = ''
         reverse_proxy http://opal:10000
+        import cloudflare
       '';
       virtualHosts."minecraft.qazer.org".extraConfig = ''
         reverse_proxy http://opal:25565
+        import cloudflare
       '';
     };
   };
