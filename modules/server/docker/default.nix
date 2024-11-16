22b4b3d985c911459b133bf5c0f831fc3c6698ac
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-16 21:39:57 +0000

    test

--- modules/server/docker/default.nix
+++ modules/server/docker/default.nix
@@ -8,11 +8,11 @@
   config = lib.mkIf config.modules.server.docker.enable {
     virtualisation.docker = {
       enable = true;
+      extraOptions = [
+        "--host=tcp://0.0.0.0:2376"
+      ];
       daemon.settings = {
         data-root = "/home/alex/.docker"; # sudo chown -R alex ~/.docker
-        extraOptions = [
-          "--host=tcp://0.0.0.0:2376"
-        ];
       };
     };
     users.users.alex.extraGroups = ["docker"];
