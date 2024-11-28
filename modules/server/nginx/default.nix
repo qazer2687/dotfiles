16f88d404b1a08fd215ab3ecf2256f3e55a2e353
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-28 23:36:15 +0000

    test

--- modules/server/nginx/default.nix
+++ modules/server/nginx/default.nix
@@ -22,10 +22,10 @@ in {
   config = lib.mkIf config.modules.server.nginx.enable {
     services.nginx = {
       enable = true;
-      recommendedZstdSettings = true;
-      recommendedTlsSettings = true;
-      recommendedProxySettings = true;
-      recommendedOptimisation = true;
+      #recommendedZstdSettings = true;
+      #recommendedTlsSettings = true;
+      #recommendedProxySettings = true;
+      #recommendedOptimisation = true;
       virtualHosts = lib.mkMerge [
         (mkRP "grafana" "3000")
         (mkRP "pihole" "3001")
