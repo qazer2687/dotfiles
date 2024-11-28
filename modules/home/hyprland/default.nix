2820d806d1a7645a99dd0ab03beb6b19e32d9371
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-28 22:05:13 +0000

    test

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -74,14 +74,6 @@
         input {
             kb_layout = gb
             kb_variant = colemak
-            kb_model =
-            kb_options =
-            kb_rules =
-
-            follow_mouse = 1
-
-            sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-
             touchpad {
               natural_scroll = true
               middle_button_emulation = true
