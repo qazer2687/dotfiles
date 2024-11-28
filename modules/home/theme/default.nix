d2e8e16b4d3c1417fcdeca243ea7d5a7eb21b551
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-28 23:18:58 +0000

    test

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -7,14 +7,14 @@
   options.modules.theme.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.theme.enable {
-    home = {
+    /*home = {
       #pointerCursor = {
       #  gtk.enable = true;
        # package = pkgs.bibata-cursors;
        # name = "Bibata-Modern-Ice";
         #size = 24;
       };
-    };
+    };*/
 
     gtk = {
       enable = true;
