8e99d146ee4ed913838ef1f0a602cfe269bb985d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-29 01:15:38 +0000

    fix

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -101,7 +101,7 @@
         bindel = ,XF86AudioMicMute, exec, ${pkgs.pamixer}/bin/pamixer --default-source -t
 
         # Brightness Controls
-        bind = ,XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 1%
+        bind = ,XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 1%+
         bind = ,XF86MonBrightnessDown, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 1%-
 
         # Floating
