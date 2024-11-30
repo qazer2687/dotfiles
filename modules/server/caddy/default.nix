aebfdc1c69e65bd6f27bba6eada4bd4f9fcfe400
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-11-30 21:25:31 +0000

    test

--- modules/server/caddy/default.nix
+++ modules/server/caddy/default.nix
@@ -3,43 +3,40 @@
   config,
   inputs,
   ...
-}: let
-
-  cloudflare-api-token = builtins.readFile config.sops.secrets.cloudflare-api-token.path;
-  cloudflare-email = builtins.readFile config.sops.secrets.cloudflare-email.path;
-
-  # Function to create virtualHosts for Caddy
-  mkRP = sub: port: let
-    dom = if sub == "" then "qazer.org" else "${sub}.qazer.org";
-  in {
-    "${dom}" = {
-      extraConfig = ''
-        reverse_proxy http://100.100.101.66:${port}
-      '';
-    };
-  };
-
-in {
+}: {
 
   options.modules.server.caddy.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.server.caddy.enable {
-    sops.secrets.cloudflare-api-token = {};
     services.caddy = {
       enable = true;
       extraConfig = ''
         qazer.org
       '';
-      virtualHosts = lib.mkMerge [
-        (mkRP "grafana" "3000")
-        (mkRP "pihole" "3001")
-        (mkRP "dashboard" "8082")
-        (mkRP "prometheus" "9090")
-        (mkRP "portainer" "9443")
-        (mkRP "node-exporter" "9100")
-        (mkRP "cockpit" "10000")
-        (mkRP "nextcloud" "11000")
-      ];
+      virtualHosts."grafana.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:3000
+      '';
+      virtualHosts."pihole.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:3001
+      '';
+      virtualHosts."dashboard.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:8082
+      '';
+      virtualHosts."prometheus.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:9090
+      '';
+      virtualHosts."portainer.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:9443
+      '';
+      virtualHosts."node-exporter.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:9100
+      '';
+      virtualHosts."cockpit.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:10000
+      '';
+      virtualHosts."nextcloud.qazer.org".extraConfig = ''
+        reverse_proxy http://100.100.101.66:11000
+      '';
     };
   };
 }
