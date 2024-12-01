e91a99a2cfef4c0081c0adf497ae3f554280fff2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-12-01 18:49:49 +0000

    test

--- modules/server/homepage-dashboard/default.nix
+++ modules/server/homepage-dashboard/default.nix
@@ -16,7 +16,7 @@
         background = "https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
         color = "slate";
         hideVersion = "true";
-        showStats = "true";
+        #showStats = "true";
         statusStyle = "dot";
       };
       widgets = [
