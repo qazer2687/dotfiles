225b1832f0173a9030085e9e1112111f3199d169
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-12-01 18:50:42 +0000

    test

--- modules/server/homepage-dashboard/default.nix
+++ modules/server/homepage-dashboard/default.nix
@@ -13,7 +13,7 @@
         title = "dashboard.qazer.org";
         startURL = "https://dashboard.qazer.org";
         theme = "dark";
-        background = "https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+        background = "https://imgur.com/GgtJC5a";
         color = "slate";
         hideVersion = "true";
         #showStats = "true";
