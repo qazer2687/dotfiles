8264856ebf32558edc6ba47c09ffb2034b57dc9f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-12-03 23:52:37 +0000

    remove stupide stuff

--- modules/server/docker/default.nix
+++ modules/server/docker/default.nix
@@ -8,33 +8,10 @@
   config = lib.mkIf config.modules.server.docker.enable {
     virtualisation.docker = {
       enable = true;
+      liveRestore = false;
       daemon.settings = {
         data-root = "/var/lib/docker";
         "hosts" = [ "tcp://0.0.0.0:2376" "unix:///var/run/docker.sock" ];
-      };
-    };
-    users.users.alex.extraGroups = ["docker"];
-    # Allow the system to boot independantly of docker.
-    systemd = {
-      timers.docker = {
-        unitConfig.OnBootSec = "10sec";
-        wantedBy = [ "timers.target" ];
-      };
-      services.docker = {
-        before = [];
-        after = [];
-        wants = [];
-        wantedBy = [];
-        requires = [];
-        requiredBy = [];
-      };
-      sockets.docker = {
-        before = [];
-        after = [];
-        wants = [];
-        wantedBy = [];
-        requires = [];
-        requiredBy = [];
       };
     };
   };
