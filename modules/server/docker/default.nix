622b6e1d75f71a5d7ff8a10a5feee40b41076749
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-12-03 23:59:29 +0000

    revert

--- modules/server/docker/default.nix
+++ modules/server/docker/default.nix
@@ -9,7 +9,8 @@
     virtualisation.docker = {
       enable = true;
       liveRestore = false;
-      enableOnBoot = false;
+      # Adds 20s to multi-user.target but it doesn't really matter.
+      enableOnBoot = true;
       daemon.settings = {
         data-root = "/var/lib/docker";
         "hosts" = [ "tcp://0.0.0.0:2376" "unix:///var/run/docker.sock" ];
