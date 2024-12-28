fe2f095485d2b8f2d8d51faba1f6b9f3b89a881e
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2024-12-28 02:01:01 +0000

    fix

--- hosts/jet/default.nix
+++ hosts/jet/default.nix
@@ -35,7 +35,7 @@
   # opened across applications.
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    #wlr.enable = true;
     xdgOpenUsePortal = true;
     extraPortals = [
       #pkgs.xdg-desktop-portal-hyprland
