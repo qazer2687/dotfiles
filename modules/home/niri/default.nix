60b9598c1070e6a5a1f218987d7c02f278351d1f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-07 09:50:50 +0000

    test

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -113,8 +113,8 @@
           "Mod+down".action = move-window-down;
 
           # Window Resizing
-          "Mod+Shift+right".action = set-column-width "+10%";
-          "Mod+Shift+left".action = set-column-width "-10%";
+          "Mod+Shift+right".action = set-window-width "+10%";
+          "Mod+Shift+left".action = set-window-width "-10%";
           
           # Workspace switching
           "Mod+1".action = focus-workspace 1;
