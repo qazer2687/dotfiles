28fce704f7746fac0a01e4cbd0981dd8aed5dd50
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-08 08:50:00 +0000

    fix

--- hosts/jet/default.nix
+++ hosts/jet/default.nix
@@ -100,7 +100,7 @@
     ];
   };
   environment.loginShellInit = ''
-    [[ "$(tty)" == /dev/tty1 ]] && niri-session
+    [[ "$(tty)" == /dev/tty1 ]] && dbus-run-session niri
   '';
 
   swapDevices = [
