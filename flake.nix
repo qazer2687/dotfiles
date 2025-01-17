5151ddf34b7073ccfa14f54705eb25a6ff0fd5cb
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-17 09:13:40 +0000

    test

--- flake.nix
+++ flake.nix
@@ -55,7 +55,30 @@
     overlays = import ./overlays {inherit (inputs) self;};
 
     nixosConfigurations = {
-      jet = import ./flake/jet { inherit (inputs) self nixpkgs; };
+      jet = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs inputs.self; };
+        modules = [
+          ./hosts/jet
+          nur.modules.nixos.default
+          sops-nix.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          asahi.nixosModules.apple-silicon-support
+          {
+            home-manager = {
+              users.alex = ./homes/jet;
+              extraSpecialArgs = { inherit inputs inputs.self; };
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              sharedModules = [
+                inputs.niri.homeModules.niri
+                inputs.nur.modules.homeManager.default
+                inputs.sops-nix.homeManagerModules.sops
+                inputs.nixvim.homeManagerModules.nixvim
+              ];
+            };
+          }
+        ];
+      };
     };
     
     nixosConfigurations = {
