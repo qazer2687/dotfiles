604f0876e1b6b377ea552debae7f9c7cb57b1097
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-26 23:31:15 +0000

    test

--- modules/home/mako/default.nix
+++ modules/home/mako/default.nix
@@ -1,24 +1,26 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.modules.mako.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.mako.enable {
+    home.packages = [ pkgs.libnotify ];
     services.mako = {
       enable = true;
-      backgroundColor = "#000000";
-      textColor = "#ffffff";
-      borderColor = "#ffffff";
-      borderRadius = 6;
+      backgroundColor = "#222222";
+      textColor = "#dddddd";
+      borderColor = "#dddddd";
+      borderRadius = 2;
       borderSize = 1;
-      progressColor = "source #ff0000";
-      font = "Departure Mono 12";
+      progressColor = "source #ffaaaa";
+      font = "Departure Mono 8";
       width = 300;
       height = 80;
       margin = "10";
-      padding = "8";
+      padding = "4";
       defaultTimeout = 5000;
       layer = "top";
       anchor = "top-right";
