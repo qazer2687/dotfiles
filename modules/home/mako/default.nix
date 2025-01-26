c60eb8d214ae67e9a4a099c777784204ae2a697a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-26 21:10:27 +0000

    test

--- modules/home/mako/default.nix
+++ modules/home/mako/default.nix
@@ -8,14 +8,45 @@
   config = lib.mkIf config.modules.mako.enable {
     services.mako = {
       enable = true;
-      defaultTimeout = 5000;
-      borderRadius = 6;
-      margin = "8";
-      anchor = "top-right";
-      width = 600;
-      backgroundColor = "#000000";
-      borderColor = "#000000";
-      padding = "8,8,8,8";
+    backgroundColor = "#FAFAFA";
+    textColor = "#333333";
+    borderColor = "#CCCCCC";
+    borderRadius = 15;
+    borderSize = 1;
+    progressColor = "source #007AFF";
+    font = "SF Pro Text 12";
+    width = 300;
+    height = 80;
+    margin = "10";
+    padding = "8";
+    #iconPath = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
+    defaultTimeout = 5000;
+    layer = "top";
+    anchor = "top-right";
+
+    extraConfig = ''
+      # These settings aren't exposed in the module
+      text-alignment=left
+      icons=left
+      max-icon-size=64
+      group-by=app-name
+      history=yes
+      ignore-timeout=yes
+      markup=yes
+      actions=inline
+      inline-replies=yes
+      
+      # Only override urgency levels
+      [urgency=low]
+      background-color=#E8F2FC
+      border-color=#B3D7FF
+      
+      [urgency=critical]
+      background-color=#FFD1D1
+      text-color=#CC0000
+      border-color=#CC0000
+    '';
+  };
     };
   };
 }
