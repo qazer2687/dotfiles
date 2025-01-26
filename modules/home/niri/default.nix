94b06310dddf0c6b1d64d7408852d217f694c55b
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-26 23:40:57 +0000

    test

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -4,13 +4,28 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+
+  screenshot = pkgs.writeShellApplication {
+    name = "screenshot";
+    runtimeInputs = with pkgs; [
+      grim
+      slurp
+      wl-clipboard
+    ];
+    text = ''
+      grim -g "$(slurp -b 00000055 -c ffffffff)" - | wl-copy -t image/png
+    '';
+  };
+
+in {
   options.modules.niri.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.niri.enable {
     home.packages = with pkgs; [
       #inputs.swww.packages.${pkgs.system}.swww
       swaybg
+      screenshot
     ];
 
     programs.niri = {
