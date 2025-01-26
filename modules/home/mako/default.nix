f9865983756ed9212f01daedf033c73e5b7bcdfd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-26 22:33:23 +0000

    hyprland ai slop

--- modules/home/mako/default.nix
+++ modules/home/mako/default.nix
@@ -8,38 +8,20 @@
   config = lib.mkIf config.modules.mako.enable {
     services.mako = {
       enable = true;
-      backgroundColor = "#FAFAFA";
-      textColor = "#333333";
-      borderColor = "#CCCCCC";
-      borderRadius = 15;
+      backgroundColor = "#000000";
+      textColor = "#ffffff";
+      borderColor = "#ffffff";
+      borderRadius = 6;
       borderSize = 1;
-      progressColor = "source #007AFF";
-      font = "SF Pro Text 12";
+      progressColor = "source #ff0000";
+      font = "Departure Mono 12";
       width = 300;
       height = 80;
       margin = "10";
       padding = "8";
-      #iconPath = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
       defaultTimeout = 5000;
       layer = "top";
       anchor = "top-right";
-
-      extraConfig = ''
-        # These settings aren't exposed in the module
-        text-alignment=left
-        max-icon-size=64
-        group-by=app-name
-
-        # Only override urgency levels
-        [urgency=low]
-        background-color=#E8F2FC
-        border-color=#B3D7FF
-        
-        [urgency=critical]
-        background-color=#FFD1D1
-        text-color=#CC0000
-        border-color=#CC0000
-      '';
     };
   };
 }
