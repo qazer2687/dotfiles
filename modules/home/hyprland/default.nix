ff5f00993868f020a4aa3a104ddf0e5392df930a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-01-28 16:02:25 +0000

    test

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -83,59 +83,57 @@
           disable_hyprland_logo = true;
         };
 
-        keybindings = {
+        bind = [
           # Basic controls
-          "SUPER + Return" = "exec, foot";
-          "SUPER + E" = "exec, wofi --show drun";
-          "SUPER + Q" = "killactive";
-          "SUPER + SPACE" = "togglefloating";
+          "SUPER, Return, exec, foot"
+          "SUPER, E, exec, wofi --show drun"
+          "SUPER, Q, killactive"
+          "SUPER, SPACE, togglefloating"
 
           # Window movement
-          "SUPER + left" = "movewindow, l";
-          "SUPER + right" = "movewindow, r";
-          "SUPER + up" = "movewindow, u";
-          "SUPER + down" = "movewindow, d";
+          "SUPER, left, movewindow, l"
+          "SUPER, right, movewindow, r"
+          "SUPER, up, movewindow, u"
+          "SUPER, down, movewindow, d"
 
           # Workspaces
-          "SUPER + 1" = "workspace, 1";
-          "SUPER + 2" = "workspace, 2";
-          "SUPER + 3" = "workspace, 3";
-          "SUPER + 4" = "workspace, 4";
-          "SUPER + 5" = "workspace, 5";
-          "SUPER + 6" = "workspace, 6";
-          "SUPER + 7" = "workspace, 7";
-          "SUPER + 8" = "workspace, 8";
-          "SUPER + 9" = "workspace, 9";
-          "SUPER + 0" = "workspace, 10";
+          "SUPER, 1, workspace, 1"
+          "SUPER, 2, workspace, 2"
+          "SUPER, 3, workspace, 3"
+          "SUPER, 4, workspace, 4"
+          "SUPER, 5, workspace, 5"
+          "SUPER, 6, workspace, 6"
+          "SUPER, 7, workspace, 7"
+          "SUPER, 8, workspace, 8"
+          "SUPER, 9, workspace, 9"
+          "SUPER, 0, workspace, 10"
 
           # Window to workspace
-          "SUPER + SHIFT + 1" = "movetoworkspace, 1";
-          "SUPER + SHIFT + 2" = "movetoworkspace, 2";
-          "SUPER + SHIFT + 3" = "movetoworkspace, 3";
-          "SUPER + SHIFT + 4" = "movetoworkspace, 4";
-          "SUPER + SHIFT + 5" = "movetoworkspace, 5";
-          "SUPER + SHIFT + 6" = "movetoworkspace, 6";
-          "SUPER + SHIFT + 7" = "movetoworkspace, 7";
-          "SUPER + SHIFT + 8" = "movetoworkspace, 8";
-          "SUPER + SHIFT + 9" = "movetoworkspace, 9";
-          "SUPER + SHIFT + 0" = "movetoworkspace, 10";
-        };
+          "SUPER SHIFT, 1, movetoworkspace, 1"
+          "SUPER SHIFT, 2, movetoworkspace, 2"
+          "SUPER SHIFT, 3, movetoworkspace, 3"
+          "SUPER SHIFT, 4, movetoworkspace, 4"
+          "SUPER SHIFT, 5, movetoworkspace, 5"
+          "SUPER SHIFT, 6, movetoworkspace, 6"
+          "SUPER SHIFT, 7, movetoworkspace, 7"
+          "SUPER SHIFT, 8, movetoworkspace, 8"
+          "SUPER SHIFT, 9, movetoworkspace, 9"
+          "SUPER SHIFT, 0, movetoworkspace, 10"
 
-        keycodebindings = {
-          # Volume controls
-          ",XF86AudioRaiseVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -i 5";
-          ",XF86AudioLowerVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -d 5";
-          ",XF86AudioMute" = "exec, ${pkgs.pamixer}/bin/pamixer -t";
-          ",XF86AudioMicMute" = "exec, ${pkgs.pamixer}/bin/pamixer --default-source -t";
+          # Media controls
+          ",XF86AudioRaiseVolume, exec, pamixer -i 5"
+          ",XF86AudioLowerVolume, exec, pamixer -d 5"
+          ",XF86AudioMute, exec, pamixer -t"
+          ",XF86AudioMicMute, exec, pamixer --default-source -t"
 
           # Brightness controls
-          ",XF86MonBrightnessUp" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl set 1%+";
-          ",XF86MonBrightnessDown" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl set 1%-";
+          ",XF86MonBrightnessUp, exec, brightnessctl set 1%+"
+          ",XF86MonBrightnessDown, exec, brightnessctl set 1%-"
 
           # Keyboard backlight
-          "SUPER + ,XF86MonBrightnessUp" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl --class leds --device kbd_backlight set 10%+";
-          "SUPER + ,XF86MonBrightnessDown" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl --class leds --device kbd_backlight set 10%-";
-        };
+          "SUPER, XF86MonBrightnessUp, exec, brightnessctl --class leds --device kbd_backlight set 10%+"
+          "SUPER, XF86MonBrightnessDown, exec, brightnessctl --class leds --device kbd_backlight set 10%-"
+        ];
 
         exec-once = [
           "waybar"
@@ -144,14 +142,7 @@
       };
 
       extraConfig = ''
-        # Modifier definition must stay in extraConfig
-        $mod = MOD4
-
-        # Resize window bindings (commented out in original)
-        # bind = $mod, Shift, left, resizeactive -5 0
-        # bind = $mod, Shift, up, resizeactive 0 -5
-        # bind = $mod, Shift, down, resizeactive 0 5
-        # bind = $mod, Shift, right, resizeactive 5 0
+        # Add extra config here...
       '';
     };
   };
