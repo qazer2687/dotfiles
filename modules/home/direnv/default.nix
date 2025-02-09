89ab2b392d1eaf6a0563f5ef4c6971c35e1ebd7a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-09 22:48:36 +0000

    test

--- modules/home/direnv/default.nix
+++ modules/home/direnv/default.nix
@@ -13,8 +13,9 @@
       };
       config = {
         global = {
-          disable_stdin = "true";
-          warn_timeout = "0";
+          disable_stdin = true;
+          warn_timeout = 0;
+					hide_env_diff = true;
         };
       };
     };
