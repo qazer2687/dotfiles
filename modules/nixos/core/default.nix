b1ee721e2cd76f0b94ea4e251119a2f94f7444ec
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-18 22:35:12 +0000

    cachix

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -24,6 +24,10 @@
       flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
     in {
       settings = {
+        # Enable cache for the Hyprland flake.
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+
         experimental-features = [
           "nix-command"
           "flakes"
