50d747886182ba53f91478f16b82f818dbb4ae2d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-18 23:32:08 +0000

    test fork

--- modules/nixos/zram/default.nix
+++ modules/nixos/zram/default.nix
@@ -7,7 +7,8 @@
 
   config = lib.mkIf config.modules.zram.enable {
     zramSwap = {
-      enable = true;
+      # Marcan said something about it being ineffective on apple silicon.
+      enable = false;
       algorithm = "zstd";
       memoryPercent = 50;
     };
