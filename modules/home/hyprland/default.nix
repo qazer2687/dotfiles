2e8c9b56f99006281297e48798866b9667f85508
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-19 00:45:24 +0000

    test

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -45,7 +45,7 @@ in {
     wayland.windowManager.hyprland = {
       enable = true;
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      #portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       plugins = [ 
         pkgs.hyprlandPlugins.hyprscroller
         inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
