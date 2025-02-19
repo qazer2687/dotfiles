984e8be9a84266ac0ef6a9b09ba3926079ebbf26
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-19 02:36:28 +0000

    test

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -73,14 +73,12 @@ in {
 					};
 
 					borders-plus-plus = {
-						add_borders = 2;
-						col.border_1 = "#000000";
-						col.border_2 = "#ffffff";
-						col.border_3 = "#000000";
+						add_borders = 1;
+						col.border_1 = "#ffffff";
+						col.border_2 = "#000000";
 
-						border_size_1 = 8;
+						border_size_1 = 16;
 						border_size_2 = 8;
-        		border_size_3 = 8;
     			};
 				};
 
