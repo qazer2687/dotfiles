ea9237f4bd9746d900b694b075a52109005a7697
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-19 21:29:35 +0000

    test

--- modules/home/neofetch/default.nix
+++ modules/home/neofetch/default.nix
@@ -62,7 +62,7 @@
       bar_color_elapsed="distro"
       bar_color_total="distro"
       image_backend="kitty"
-      image_source="/etc/nixos/home/images/misc/neofetch.png"
+      image_source="${config.home.homeDirectory}/.config/assets/neofetch.png"
       image_size="250px"
       image_loop="on"
       ascii_distro="nixos"
