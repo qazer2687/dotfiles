431841386200b6b8c1f91e9e81e6937197ed96bf
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-21 16:45:01 +0000

    test

--- packages/zen-browser/default.nix
+++ packages/zen-browser/default.nix
@@ -8,20 +8,21 @@ let
     sha256 = "K53Kb/EGzG3dxPb1UUvkP6xpK9rni0rNDqM3xE55KHo=";
   };
 
-  appimageContents = appimageTools.extractType1 { name = pname; src = src; };
+  appimageContents = appimageTools.extractType1 { inherit pname src; };
 in
 appimageTools.wrapType2 rec {
   inherit pname version src;
 
   extraInstallCommands = ''
     substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
+      --replace 'Exec=AppRun' 'Exec=${meta.mainProgram}'
   '';
 
-  meta = {
+  meta = with lib; {
     description = "Zen Browser launcher using AppImage.";
     homepage = "https://github.com/zen-browser/desktop";
-    license = lib.licenses.unfree;
+    license = licenses.unfree;
     platforms = [ "aarch64-linux" ];
+    mainProgram = "zen-browser";
   };
 }
