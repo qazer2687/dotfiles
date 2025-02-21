66a3df03b8a9b0c5365159645693b69d742ad094
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-21 17:22:01 +0000

    desktop entry for zen

--- packages/zen-browser/default.nix
+++ packages/zen-browser/default.nix
@@ -8,13 +8,26 @@ let
     sha256 = "K53Kb/EGzG3dxPb1UUvkP6xpK9rni0rNDqM3xE55KHo=";
   };
 
+  desktopItem = makeDesktopItem {
+    name = pname;
+    exec = "${pname}";
+    icon = pname;
+    desktopName = "Zen Browser";
+    comment = "A privacy-focused browser";
+    categories = [ "Network" "WebBrowser" ];
+  };
+
   appimageContents = appimageTools.extractType1 { inherit pname src; };
 in
 appimageTools.wrapType2 rec {
   inherit pname version src;
 
+  extraInstallCommands = ''
+    install -Dm644 ${desktopItem}/share/applications/*.desktop $out/share/applications/${pname}.desktop
+  '';
+
   meta = with lib; {
-    description = "Zen Browser launcher using AppImage.";
+    description = "A privacy-focused browser";
     homepage = "https://github.com/zen-browser/desktop";
     license = licenses.unfree;
     platforms = [ "aarch64-linux" ];
