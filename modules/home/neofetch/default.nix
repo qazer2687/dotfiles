667070795d008bcbcfe38484e090aa69d33deda8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-22 23:16:19 +0000

    test

--- modules/home/neofetch/default.nix
+++ modules/home/neofetch/default.nix
@@ -11,48 +11,43 @@
 
     home.packages = with pkgs; [
       neofetch
+      chafa
     ];
 
-    home.file.".config/assets/neofetch.png" = {
-      source = ../../../assets/neofetch.png;
+    home.file.".config/assets/konata.png" = {
+      source = ../../../assets/konata.png;
     };
 
     # This doesn't work properly for displaying images so 
     # I will make this a fastfetch config at some point. 
     home.file.".config/neofetch/config.conf".text = ''
       print_info() {
-          prin ""
-          info title
-          info "\e[34m  " os
-          info "\e[31m " kernel
-          info "\e[33m " uptime
-          info "\e[32m " shell
-          info "\e[35m " wm
-          prin "$(color 1)▂▂ $(color 2)▂▂ $(color 3)▂▂ $(color 4)▂▂ $(color 5)▂▂ $(color 6)▂▂ "
+        prin ""
+        info title
+        info "\e[34m  " os
+        info "\e[31m " kernel
+        info "\e[33m " uptime
+        info "\e[32m " shell
+        info "\e[35m " wm
+        prin "$(color 1)▂▂ $(color 2)▂▂ $(color 3)▂▂ $(color 4)▂▂ $(color 5)▂▂ $(color 6)▂▂ "
       }
 
       title_fqdn="off"
       kernel_shorthand="on"
       distro_shorthand="off"
       uptime_shorthand="tiny"
-      memory_percent="off"
-      memory_unit="mib"
-      package_managers="off"
-      shell_path="off"
-      shell_version="on"
-      speed_type="bios_limit"
-      speed_shorthand="off"
-      cpu_brand="on"
-      cpu_cores="logical"
-      refresh_rate="off"
-      gtk_shorthand="off"
-      gtk2="on"
-      gtk3="on"
-      de_version="on"
-      bold="off"
-      underline_enabled="on"
-      underline_char="_"
-      separator=" • "
+
+      image_backend="chafa"
+      image_source="${config.home.homeDirectory}/.config/assets/konata.png"
+      image_size="250px"
+      image_loop="on"
+      thumbnail_dir="${config.home.homeDirectory}/.cache/thumbnails/neofetch"
+      crop_mode="fit"
+      crop_offset="center"
+      xoffset=0
+      yoffset=0
+      stdout="off"
+
       color_blocks="on"
       block_width=3
       block_height=1
@@ -63,20 +58,6 @@
       bar_length=15
       bar_color_elapsed="distro"
       bar_color_total="distro"
-      image_backend="sixel"
-      image_source="${config.home.homeDirectory}/.config/assets/neofetch.png"
-      image_size="250px"
-      image_loop="on"
-      ascii_distro="nixos"
-      ascii_colors=(distro)
-      ascii_bold="on"
-      thumbnail_dir="${config.home.homeDirectory}/.cache/thumbnails/neofetch"
-      crop_mode="fit"
-      crop_offset="center"
-      yoffset=0
-      xoffset=0
-      stdout="off"
-
     '';
   };
 }
