2ee66ff674ee977359124d50d8ecf60f66f138ed
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-22 23:47:56 +0000

    test

--- modules/home/neofetch/default.nix
+++ modules/home/neofetch/default.nix
@@ -10,7 +10,7 @@
   config = lib.mkIf config.modules.neofetch.enable {
 
     home.packages = with pkgs; [
-      neofetch
+      fastfetch
       chafa
       # Required dependancy for displaying images.
       imagemagick
@@ -20,46 +20,46 @@
       source = ../../../assets/konata.png;
     };
 
-    # This doesn't work properly for displaying images so 
-    # I will make this a fastfetch config at some point. 
-    home.file.".config/neofetch/config.conf".text = ''
-      print_info() {
-        prin ""
-        info title
-        info "\e[34m  " os
-        info "\e[31m " kernel
-        info "\e[33m " uptime
-        info "\e[32m " shell
-        info "\e[35m " wm
-        prin "$(color 1)▂▂ $(color 2)▂▂ $(color 3)▂▂ $(color 4)▂▂ $(color 5)▂▂ $(color 6)▂▂ "
-      }
+    home.file.".config/fastfetch/config.conf".text = ''
+      title_fqdn = false
+      kernel_shorthand = true
+      distro_shorthand = false
+      uptime_shorthand = "tiny"
 
-      title_fqdn="off"
-      kernel_shorthand="on"
-      distro_shorthand="off"
-      uptime_shorthand="tiny"
+      image_backend = "chafa"
+      image_source = "$HOME/.config/assets/konata.png"
+      image_size = "250px"
+      image_loop = true
+      thumbnail_dir = "$HOME/.cache/thumbnails/fastfetch"
+      crop_mode = "fit"
+      crop_offset = "center"
+      xoffset = 0
+      yoffset = 0
+      stdout = false
 
-      image_backend="chafa"
-      image_source="${config.home.homeDirectory}/.config/assets/konata.png"
-      image_size="250px"
-      image_loop="on"
-      thumbnail_dir="${config.home.homeDirectory}/.cache/thumbnails/neofetch"
-      crop_mode="fit"
-      crop_offset="center"
-      xoffset=0
-      yoffset=0
-      stdout="off"
+      color_blocks = true
+      block_width = 3
+      block_height = 1
+      col_offset = "auto"
+      bar_char_elapsed = "-"
+      bar_char_total = "="
+      bar_border = true
+      bar_length = 15
+      bar_color_elapsed = "distro"
+      bar_color_total = "distro"
 
-      color_blocks="on"
-      block_width=3
-      block_height=1
-      col_offset="auto"
-      bar_char_elapsed="-"
-      bar_char_total="="
-      bar_border="on"
-      bar_length=15
-      bar_color_elapsed="distro"
-      bar_color_total="distro"
+      # Fastfetch lets you define the order (and custom strings) of the info to be printed.
+      # In this example, we mimic your custom print_info() function:
+      info_order = [
+          "",
+          "title",
+          "\u001b[34m  os",      # Blue colored OS label
+          "\u001b[31m  kernel",  # Red colored Kernel label
+          "\u001b[33m  uptime",  # Yellow colored Uptime label
+          "\u001b[32m  shell",   # Green colored Shell label
+          "\u001b[35m  wm",      # Magenta colored WM label
+          "$(color 1)▂▂ $(color 2)▂▂ $(color 3)▂▂ $(color 4)▂▂ $(color 5)▂▂ $(color 6)▂▂ "
+      ]
     '';
   };
 }
