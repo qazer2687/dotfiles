8dfdd1c4d9667b8a930acb064c80be766486e079
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-24 15:44:40 +0000

    test

--- modules/server/docker/default.nix
+++ modules/server/docker/default.nix
@@ -6,9 +6,12 @@
   options.modules.server.docker.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.server.docker.enable {
+
+    hardware.nvidia-container-toolkit.enable = true;
     virtualisation.docker = {
       enable = true;
       daemon.settings = {
+        features.cdi = true;
         data-root = "/var/lib/docker";
         "hosts" = ["tcp://0.0.0.0:2376" "unix:///var/run/docker.sock"];
       };
