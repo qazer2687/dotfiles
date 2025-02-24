775ccf730d1eed8163bd3b348957539595385f6f
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-02-24 21:25:25 +0000

    test

--- modules/server/matrix/default.nix
+++ modules/server/matrix/default.nix
@@ -15,8 +15,8 @@
       settings.global = {
         allow_registration = true;
         registration_token = "f3d0dd560091aa99d8e97e48b36e73924e14b8b50a39280b5df751c5394b9a15";
-        server_name = "opal";
-        address = "::1";
+        server_name = "matrix.opal";
+        address = "100.100.101.66";
         database_backend = "rocksdb";
       };
     };
