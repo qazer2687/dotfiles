3c1dfdab3c781bc2cf5dfc2b4841ec35e2724584
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-03-02 17:30:22 +0000

    test

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
 
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
