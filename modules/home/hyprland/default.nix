f85e83d5dd04de25146aa5fe862cb60f9770e694
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-03-02 18:35:18 +0000

    test

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -148,14 +148,14 @@ in {
 
         # Smart Gaps
         workspace = [
-          "w[tv1], gapsout:0, gapsin:0"
-          "f[1], gapsout:0, gapsin:0"
+          "w[tv1], gapsout:4, gapsin:0"
+          "f[1], gapsout:4, gapsin:0"
         ];
         windowrulev2 = [
-          "bordersize 0, floating:0, onworkspace:w[tv1]"
-          "rounding 0, floating:0, onworkspace:w[tv1]"
-          "bordersize 0, floating:0, onworkspace:f[1]"
-          "rounding 0, floating:0, onworkspace:f[1]"
+          "bordersize 1, floating:0, onworkspace:w[tv1]"
+          "rounding 4, floating:0, onworkspace:w[tv1]"
+          "bordersize 1, floating:0, onworkspace:f[1]"
+          "rounding 4, floating:0, onworkspace:f[1]"
         ];
   
         bind = [
