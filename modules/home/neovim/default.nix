8bbd48088d134f92f7b820d4b094549de672581b
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-03-05 01:24:18 +0000

    nixvim lsp

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -7,84 +7,30 @@
   options.modules.neovim.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.neovim.enable {
-    home.packages = [
+    home.packages = with pkgs; [
       # CMP Dependency
-      pkgs.ripgrep
+      gnumake
+      unzip
+      gcc
+      ripgrep
     ];
 
     programs.nixvim = {
       enable = true;
 
-      opts = {
-        relativenumber = false;
-        number = false;
-        updatetime = 100;
-
-        tabstop = 2;
-        shiftwidth = 2;
-        expandtab = true;
-        softtabstop = 2;
-      };
-
       plugins = {
-        treesitter = {
-          enable = true;
-          grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-            bash
-            json
-            lua
-            make
-            markdown
-            nix
-            regex
-            toml
-            vim
-            vimdoc
-            xml
-            yaml
-          ];
-        };
-
-        lightline = {
-          enable = true;
-          settings = {
-            colorscheme = "ayu_dark";
-          };
-        };
-
-        telescope = {
-          enable = true;
-        };
-
-        nvim-tree = {
-          enable = true;
-          autoReloadOnWrite = true;
-        };
-
-        auto-save = {
-          enable = true;
-          settings = {
-            enabled = true;
-          };
-        };
-
         lsp = {
           enable = true;
           servers = {
             nixd.enable = true;
-            csharp-ls.enable = true;
+            csharp_ls.enable = true;
+            lua_ls = true;
           };
         };
 
         cmp.enable = true;
 
       };
-
-      colorschemes = {
-        ayu.enable = true;
-      };
-
-      extraConfigLua = builtins.readFile ./config/init.lua;
     };
   };
 }
