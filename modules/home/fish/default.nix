71b6bacbb4e6ab2dd230806b86b819151be20a9a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-03-06 18:25:49 +0000

    nvim alias

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -16,6 +16,8 @@
     home.shellAliases = {
       "check" = ''nix-shell -p alejandra -p deadnix -p statix --command "alejandra -q . && deadnix -e && statix fix"'';
       "rebuild" = "sudo nixos-rebuild switch --flake github:qazer2687/dotfiles#$(hostname) --refresh --option eval-cache false";
+
+      "nvim" = "nix run $HOME/.config/nvim#neovim";
     };
   };
 }
