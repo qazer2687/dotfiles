f613933cce67b91c21a7d77dfc868a3907d8d70d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-03-20 14:02:16 +0000

    fix hang on shutdown and modify some kernel flags

--- modules/nixos/docker/default.nix
+++ modules/nixos/docker/default.nix
@@ -8,9 +8,11 @@
   config = lib.mkIf config.modules.docker.enable {
     
     users.users.alex.extraGroups = ["docker"];
-
     virtualisation.docker = {
       enable = true;
+      # A fix for s6-svscan hanging on shutdown.
+      # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
+      liveRestore = false;
       daemon.settings = {
         #features.cdi = true;
         data-root = "/var/lib/docker";
