9fc40540bcea1b28c12ee4251dedd1f00a9f3140
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-03-24 11:25:04 +0000

    test

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -20,15 +20,5 @@
 
       "nvim" = "nix run $HOME/.config/nvim#neovim";
     };
-    # Do not use fish as the login shell as it is not posix compliant.
-    programs.bash = {
-      interactiveShellInit = ''
-        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        fi
-      '';
-    };
   };
 }
