5c722ad190431fa856c4fa4113893cbbc8766d4d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-04-10 17:03:31 +0100

    fix

--- modules/nixos/samba/default.nix
+++ modules/nixos/samba/default.nix
@@ -8,8 +8,7 @@
   config = lib.mkIf config.modules.samba.enable {
     services.samba = {
       enable = true;
-      # Might be required I'm not sure.
-      #openFirewall = true;
+      openFirewall = true;
       settings = {
         global = {
           # Allow local network and tailnet.
@@ -22,10 +21,31 @@
           "path" = "/mnt/external/media";
           "read only" = "yes";
           "guest ok" = "yes";
-          # Hide the downloads folder. (no longer exists on this drive but this is just in case there's anything else to hide)
-          # "veto files" = "/downloads/";
+          # Hide the downloads folder.
+          #"veto files" = "/downloads/";
         };
       };
     };
+
+    # Allow network discovery.
+    services.samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+      interface = "tailscale0";
+    };
+
+    # No idea what this does.
+    services.avahi = {
+      publish.enable = true;
+      publish.userServices = true;
+      # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+      nssmdns4 = true;
+      # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
+      enable = true;
+      openFirewall = true;
+    };
+
+    networking.firewall.enable = true;
+    networking.firewall.allowPing = true;
   };
 }
