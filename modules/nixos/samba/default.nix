1d5454e46a600d32b4cf519f082673a1e976ef04
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-04-10 17:05:20 +0100

    fix

--- modules/nixos/samba/default.nix
+++ modules/nixos/samba/default.nix
@@ -8,13 +8,13 @@
   config = lib.mkIf config.modules.samba.enable {
     services.samba = {
       enable = true;
-      openFirewall = true;
+      #openFirewall = true;
       settings = {
         global = {
           # Allow local network and tailnet.
           "hosts allow" = "192.168.0. localhost 100.64.0.0/10";
           "hosts deny" = "0.0.0.0/0";
-          "guest account" = "guest";
+          "guest account" = "alex";
           "map to guest" = "bad user";
         };
         samba = {
@@ -26,26 +26,5 @@
         };
       };
     };
-
-    # Allow network discovery.
-    services.samba-wsdd = {
-      enable = true;
-      openFirewall = true;
-      interface = "tailscale0";
-    };
-
-    # No idea what this does.
-    services.avahi = {
-      publish.enable = true;
-      publish.userServices = true;
-      # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
-      nssmdns4 = true;
-      # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
-      enable = true;
-      openFirewall = true;
-    };
-
-    networking.firewall.enable = true;
-    networking.firewall.allowPing = true;
   };
 }
