ab4c97481da295903f08af8d23d3a7bc52f67887
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-03 22:18:59 +0100

    test

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -86,12 +86,12 @@ in {
         decoration = {
           rounding = 4;
 
-          active_opacity = 1;
-          inactive_opacity = 1;
+          active_opacity = 0.98;
+          inactive_opacity = 0.9;
 
           # Disabled for performance.
           blur = {
-            enabled = false;
+            enabled = true;
           };
           shadow = {
             enabled = false;
