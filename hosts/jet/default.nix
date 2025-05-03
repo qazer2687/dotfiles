0c9c7fbae573692766c91dd9fdd3dac2451677e2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-03 20:12:15 +0100

    fix

--- hosts/jet/default.nix
+++ hosts/jet/default.nix
@@ -106,7 +106,9 @@
     ];
   };
   programs.hyprland.enable = true;
+  # Add correct sudo path to PATH so that it actually works and start hyprland.
   environment.extraInit = ''
+    export PATH="/run/wrappers/bin:$PATH"
     [[ "$(tty)" == /dev/tty1 ]] && dbus-run-session Hyprland
   '';
   
