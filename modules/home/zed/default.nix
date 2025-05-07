34bc3b5b1419e21ff9fbac1e3ddc15eed21acee5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-07 09:30:25 +0100

    zed module

---
+++ modules/home/zed/default.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  options.modules.zed.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.modules.zed.enable {
+    programs.zed-editor = {
+      enable = true;
+    }
+    home.shellAliases = {
+      "zed" = "zeditor";
+    };
+  };
+}
