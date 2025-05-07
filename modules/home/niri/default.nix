564168ad8d548b66caf9284d383b24b4a47651f0
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-07 19:33:40 +0100

    niri

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -28,7 +28,7 @@ in {
 
     programs.niri = {
       enable = true;
-      package = inputs.niri.packages.${pkgs.system}.niri-unstable;
+      package = inputs.niri.packages.${pkgs.system}.niri-stable;
       settings = {
         hotkey-overlay.skip-at-startup = true;
         prefer-no-csd = true;
