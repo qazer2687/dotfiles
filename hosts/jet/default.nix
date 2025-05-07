ab4a3c87ba9f3177794fd21cf6e7c459130b80f1
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-07 15:22:49 +0100

    niri

--- hosts/jet/default.nix
+++ hosts/jet/default.nix
@@ -103,9 +103,8 @@
       "--noclear"
     ];
   };
-  programs.hyprland.enable = true;
   environment.loginShellInit = ''
-    [[ "$(tty)" == /dev/tty1 ]] && dbus-run-session Hyprland
+    [[ "$(tty)" == /dev/tty1 ]] && niri-session
   '';
 
   swapDevices = [
