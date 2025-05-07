9bd27ff34c2967744e6a7f97ecc91dcdc5459fc2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-07 11:14:44 +0100

    fix

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -57,8 +57,6 @@ in {
             clip-to-geometry = true;
             
             open-maximized = true;
-            
-            baba-is-float = true;
 
             # A ratio of screen space used against the total, out of
             # 1280 which is half of my display width and I use 2x scaling.
