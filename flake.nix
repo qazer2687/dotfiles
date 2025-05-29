321c05719e2fb1bbd713ac499495c9e1158fa7fd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-05-29 22:27:28 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager";
     sops-nix.url = "github:Mic92/sops-nix";
     asahi.url = "github:tpwrules/nixos-apple-silicon";
