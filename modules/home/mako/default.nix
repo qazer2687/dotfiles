5762cd0663c6cbba2dc61946708d5c3ec912abfd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-04 20:40:56 +0100

    fix

--- modules/home/mako/default.nix
+++ modules/home/mako/default.nix
@@ -5,25 +5,26 @@
   ...
 }: {
   options.modules.mako.enable = lib.mkEnableOption "";
-
   config = lib.mkIf config.modules.mako.enable {
     home.packages = [pkgs.libnotify];
     services.mako = {
       enable = true;
-      backgroundColor = "#000000";
-      textColor = "#ffffff";
-      borderColor = "#ffffff";
-      borderRadius = 2;
-      borderSize = 1;
-      progressColor = "source #ff0000";
-      font = "Departure Mono 8";
-      width = 500;
-      height = 120;
-      margin = "4";
-      padding = "10";
-      defaultTimeout = 5000;
-      layer = "top";
-      anchor = "top-right";
+      settings = {
+        background-color = "#000000";
+        text-color = "#ffffff";
+        border-color = "#ffffff";
+        border-radius = 2;
+        border-size = 1;
+        progress-color = "source #ff0000";
+        font = "Departure Mono 8";
+        width = 500;
+        height = 120;
+        margin = "4";
+        padding = "10";
+        default-timeout = 5000;
+        layer = "top";
+        anchor = "top-right";
+      };
     };
   };
 }
