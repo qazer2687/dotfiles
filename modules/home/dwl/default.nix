d87236597b672f3607cc04d279eb754fe4e82b71
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-06 17:50:08 +0100

    enable dwl module

--- modules/home/dwl/default.nix
+++ modules/home/dwl/default.nix
@@ -9,6 +9,8 @@
   config = lib.mkIf config.modules.dwl.enable {
     home.packages = with pkgs; [
       # The configuration is set in an overlay and defined in the config folder.
+      # Enabling this is really just the same as adding dwl to packages but I guess I 
+      # can add dependencies here.
       dwl
       swaybg
     ];
