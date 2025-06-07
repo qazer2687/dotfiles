9436d5547549e968c613d18e0674c6bbe905ff20
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-07 21:59:01 +0100

    fix

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -9,22 +9,6 @@
   config = lib.mkIf config.modules.steam.enable {
     programs.steam = {
       enable = true;
-      package = pkgs.steam.override {
-        extraPkgs = _pkgs:
-          with pkgs; [
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            libkrb5
-            keyutils
-            openssl
-          ];
-      };
     };
 
     programs.gamescope = {
