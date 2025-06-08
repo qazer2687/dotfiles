c960e85db717536c100ad9067ffb6cef4b45d698
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-08 15:20:34 +0100

    new waybar layout

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   pkgs,
+  osConfig,
   ...
 }: {
   options.modules.waybar.enable = lib.mkEnableOption "";
@@ -21,183 +22,13 @@
     programs.waybar = {
       enable = true;
       systemd.enable = false;
-      settings = [
-        {
-          layer = "top";
-          height = 28;
-          margin = "0 0 0 0";
-          modules-left = ["clock" "dwl/tags"];
-          modules-center = [];
-          modules-right = ["network" "pulseaudio" "battery"];
-
-          pulseaudio = {
-            format = "{icon} {volume}%";
-            tooltip = false;
-            format-muted = "  MUTED";
-            format-icons = {
-              default = [" " " " " "];
-            };
-          };
-          clock = {
-            format = "{:%A %d, %H:%M}";
-            #format-alt = "{:%Y/%m/%d | %H:%M:%S}";
-            tooltip = false;
-          };
-
-          battery = {
-            tooltip = false;
-            format = "{icon} {capacity}%";
-            format-icons = [" " " " " " " " " "];
-            format-charging = "󱐋 {capacity}%";
-            interval = 10;
-          };
-          
-          "dwl/tags" = {
-            num-tags = 9;
-            tag-labels = {
-              "1" = "1";
-              "2" = "2";
-              "3" = "3";
-              "4" = "4";
-              "5" = "5";
-              "6" = "6";
-              "7" = "7";
-              "8" = "8";
-              "9" = "9";
-            };
-          };
-
-          "niri/workspaces" = {
-            format = "󱓻";
-            format-icons = {
-              "1" = "1";
-              "2" = "2";
-              "3" = "3";
-              "4" = "4";
-              "5" = "5";
-              "6" = "6";
-              "7" = "7";
-              "8" = "8";
-              "9" = "9";
-              "0" = "10";
-            };
-            #sort-by-number = true;
-          };
-
-          backlight = {
-            device = "apple-panel-bl";
-            format = "{icon}";
-            tooltip = true;
-            tooltip-format = "{percent}%";
-            format-icons = ["󰃞 " "󰃟 " "󰃠 "];
-          };
-
-          network = {
-            tooltip = false;
-            format-wifi = "  {essid}"; # 󱐋 {frequency}
-            family = "ipv4";
-            format-disconnected = " ";
-            format-ethernet = " ";
-            interval = 10;
-          };
-
-          mpris = {
-            tooltip = false;
-            format = "{player_icon} {artist} - {title}";
-            format-paused = "{status_icon} {artist} - {title}";
-            player-icons = {
-              default = " ";
-            };
-            artist-len = 24;
-            title-len = 34;
-            status-icons = {
-              paused = " ";
-            };
-          };
-        }
-      ];
-
-      style = ''
-        * {
-          font-family: "Departure Mono", "FiraCode Mono Nerd Font";
-          font-size: 11px;
-          background-color: transparent;
-        }
-
-        /* Base styles */
-        window#waybar {
-          background-color: #000000;
-          border-radius: 0 0 4px 4px;
-        }
-
-        /* Tooltip styling */
-        tooltip {
-          background: #000000;
-          border: 1px solid rgba(255, 255, 255, 1);
-        }
-
-        tooltip label {
-          color: white;
-        }
-
-        /* Common module styling */
-        #memory,
-        #custom-power,
-        #battery,
-        #backlight,
-        #pulseaudio,
-        #network,
-        #cpu,
-        #temperature,
-        #disk,
-        #mpris,
-        #tray {
-          margin: 4px 2px;
-          padding: 2px 4px;
-          border-radius: 2px;
-          color: #ffffff;
-        }
-        
-        /* Hide unused dwl tags */
-        #tags button:not(.occupied):not(.focused) {
-          color: #000000;
-        }
-
-        /* Workspace button styling */
-        #tags button,
-        #workspaces button {
-          all: initial;
-          min-width: 0;
-          padding: 4px 4px;
-          border-radius: 6px;
-          color: rgb(138, 138, 138);
-        }
-        
-        #tags button.focused,
-        #workspaces button.active,
-        #workspaces button.focused {
-          color: #ffffff;
-        }
-
-        /* Special module styling */
-        #mpris {
-          background-color: #ffffff;
-          color: #000000;
-        }
-
-        #battery {
-          margin-right: 8px;
-        }
-
-        #clock {
-          margin: 4px 0 4px 16px;
-          margin-right: 4px;
-          padding: 2px 4px;
-          border-radius: 2px;
-          background-color: #ffffff;
-          color: #000000;
-        }
-      '';
     };
-  };
-}
+
+    xdg.configFile = if osConfig.networking.hostName == "jet" then {
+      "waybar/config".source = ./config/jet/config;
+      "waybar/style.css".source = ./config/jet/style.css;
+    } else {
+      "waybar/config".source = ./config/jade/config;
+      "waybar/style.css".source = ./config/jade/style.css;
+    };
+};
