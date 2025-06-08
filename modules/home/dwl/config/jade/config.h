e95434dc0406b2ee8b918509f5f5c69a17bd05e9
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-08 15:33:37 +0100

    fix scaling

--- modules/home/dwl/config/jade/config.h
+++ modules/home/dwl/config/jade/config.h
@@ -67,7 +67,7 @@ static const MonitorRule monrules[] = {
 	{ "eDP-1",    0.5f,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 	*/
 	/* defaults */
-	{ NULL,       0.55f, 1,      1.5,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
+	{ NULL,       0.55f, 1,      1.2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 };
 
 // Keyboard
