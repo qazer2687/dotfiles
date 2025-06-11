82c4ad329162a18137f13c2b194064cb3af2d8c4
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-11 15:41:11 +0100

    gamemode tweaks

--- modules/nixos/gamemode/default.nix
+++ modules/nixos/gamemode/default.nix
@@ -1,26 +1,42 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.modules.gamemode.enable = lib.mkEnableOption "";
-
+  
   config = lib.mkIf config.modules.gamemode.enable {
     programs.gamemode = {
       enable = true;
       enableRenice = true;
       settings = {
         general = {
-          desiredgov = "performance";
-          defaultgov = "performance";
+          desiredgov = "schedutil";
+          defaultgov = "schedutil";
+          renice = -10;
+          ioprio = 0;
+          inhibit_screensaver = 1;
         };
         gpu = {
           apply_gpu_optimisations = "accept-responsibility";
-          # This is /sys/class/drm/card<x>.
           gpu_device = 1;
-          nv_powermizer_mode = 1; # "Prefer Maximum Performance"
+          # Prefer maximum performance.
+          nv_powermizer_mode = 1;
+          # Set maximum GPU memory and core clocks.
+          nv_core_clock_mhz_offset = 100;
+          nv_mem_clock_mhz_offset = 500;
+        };
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode' 'Activated'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode' 'Deactivated'";
         };
       };
     };
+    
+    # Ensure user is in gamemode group for proper permissions.
+    users.users.alex.extraGroups = [ "gamemode" ];
+    
+    powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
   };
 }
