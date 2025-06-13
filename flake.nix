3a0a050adabe75ed252158df4fd041474a58fb28
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-13 15:58:25 +0100

    unstable

--- flake.nix
+++ flake.nix
@@ -2,19 +2,21 @@
   description = "qazer2687's NixOS Flake";
 
   inputs = {
-    /*
+    
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       };
-    */
+    
 
+    /*
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    */
 
     sops-nix.url = "github:Mic92/sops-nix";
     asahi.url = "github:tpwrules/nixos-apple-silicon";
