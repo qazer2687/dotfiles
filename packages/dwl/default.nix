3d56c1783024219f95d1cdaa265e0f7db6bd85fe
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-13 16:06:30 +0100

    test fix

--- packages/dwl/default.nix
+++ packages/dwl/default.nix
@@ -80,7 +80,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   makeFlags =
     [
-      "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
+     # "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
       "WAYLAND_SCANNER=wayland-scanner"
       "PREFIX=$(out)"
       "MANDIR=$(man)/share/man"
