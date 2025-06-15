11f4509a61bb405a02a1635721eebd26d7d68ad5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-16 00:50:59 +0100

    test

--- modules/home/dwl/default.nix
+++ modules/home/dwl/default.nix
@@ -8,19 +8,15 @@
   options.modules.dwl.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.dwl.enable {
-    programs.dwl = {
-      enable = true;
-      package = (pkgs.dwl.override {
+    home.packages = with pkgs; [
+      (pkgs.dwl.override {
         configH = if osConfig.networking.hostName == "jet"
           then ./config/jet/config.h
           else ./config/jade/config.h;
         enableXWayland = if osConfig.networking.hostName == "jet"
           then false
           else true;
-      });
-    };
-    
-    home.packages = with pkgs; [
+      })
       wbg
       brightnessctl
       pamixer
