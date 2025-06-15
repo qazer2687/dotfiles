8f9dd694aee1abb03ccf11a6fe42a10da20ff386
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-15 01:41:52 +0100

    test

--- packages/dwl/default.nix
+++ packages/dwl/default.nix
@@ -1,135 +1,74 @@
-
 {
   lib,
+  stdenv,
   installShellFiles,
   libX11,
   libinput,
   libxcb,
-  libdrm,
   libxkbcommon,
   pixman,
   pkg-config,
-  stdenv,
-  fcft,
-  testers,
-  nixosTests,
+  wayland-scanner,
   wayland,
   wayland-protocols,
-  wayland-scanner,
-  wlroots,
-  writeText,
+  wlroots_0_18,
   xcbutilwm,
   xwayland,
-  # Boolean flags
+  gnumake,
+  tllist,
+  fcft,
+  libdrm,
   enableXWayland ? true,
-  withCustomConfigH ? (configH != null),
-  # Configurable options
-  configH ?
-    if conf != null then
-      lib.warn ''
-        conf parameter is deprecated;
-        use configH instead
-      '' conf
-    else
-      null,
-  # Deprecated options
-  # Remove them before next version of either Nixpkgs or dwl itself
-  conf ? null,
 }:
 
-# If we set withCustomConfigH, let's not forget configH
-assert withCustomConfigH -> (configH != null);
-stdenv.mkDerivation (finalAttrs: {
+stdenv.mkDerivation {
   pname = "dwl";
   version = "0.7";
   src = ./src;
-  
-  nativeBuildInputs = [
+
+  nativeBuildInputs = [ 
     installShellFiles
     pkg-config
-    wayland-scanner
+    gnumake
   ];
   
-  buildInputs =
-    [
-      libinput
-      libxcb
-      libxkbcommon
-      pixman
-      wayland
-      wayland-protocols
-      wlroots
-      
-      fcft.dev
-      libdrm.dev  
-    ]
-    ++ lib.optionals enableXWayland [
-      libX11
-      xcbutilwm
-      xwayland
-    ];
-
-  outputs = [
-    "out"
-    "man"
+  buildInputs = [
+    libinput
+    libxcb
+    libxkbcommon
+    pixman
+    wayland
+    wayland-protocols
+    wlroots_0_18
+    wayland-scanner
+    tllist
+    fcft
+    libdrm
+  ] ++ lib.optionals enableXWayland [
+    libX11
+    xcbutilwm
+    xwayland
   ];
+  
+  outputs = [ "out" "man" ];
 
-  postPatch =
-    let
-      configFile =
-        if lib.isDerivation configH || builtins.isPath configH then
-          configH
-        else
-          writeText "config.h" configH;
-    in
-    lib.optionalString withCustomConfigH "cp ${configFile} config.h";
-
-  makeFlags =
-    [
-      "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
-      "WAYLAND_SCANNER=wayland-scanner"
-      "PREFIX=$(out)"
-      "MANDIR=$(man)/share/man"
-    ]
-    ++ lib.optionals enableXWayland [
-      ''XWAYLAND="-DXWAYLAND"''
-      ''XLIBS="xcb xcb-icccm"''
-    ];
-
-  strictDeps = true;
-
-  # required for whitespaces in makeFlags
-  __structuredAttrs = true;
-
-  passthru = {
-    tests = {
-      version = testers.testVersion {
-        package = finalAttrs.finalPackage;
-        # `dwl -v` emits its version string to stderr and returns 1
-        command = "dwl -v 2>&1; return 0";
-      };
-      basic = nixosTests.dwl;
-    };
-  };
+  makeFlags = [
+    "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
+    "WAYLAND_SCANNER=wayland-scanner"
+    "PREFIX=$(out)"
+    "MANDIR=$(man)/share/man"
+  ];
+  
+  buildPhase = ''
+    make clean
+    make
+  '';
 
   meta = {
-    homepage = "https://codeberg.org/dwl/dwl";
-    changelog = "https://codeberg.org/dwl/dwl/src/branch/${finalAttrs.version}/CHANGELOG.md";
-    description = "Dynamic window manager for Wayland";
-    longDescription = ''
-      dwl is a compact, hackable compositor for Wayland based on wlroots. It is
-      intended to fill the same space in the Wayland world that dwm does in X11,
-      primarily in terms of philosophy, and secondarily in terms of
-      functionality. Like dwm, dwl is:
-
-      - Easy to understand, hack on, and extend with patches
-      - One C source file (or a very small number) configurable via config.h
-      - Tied to as few external dependencies as possible
-    '';
+    homepage = "https://github.com/qazer2687/dotfiles";
     license = lib.licenses.gpl3Only;
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.AndersonTorres ];
     inherit (wayland.meta) platforms;
     mainProgram = "dwl";
   };
-})
-# TODO: custom patches from upstream website
+}
