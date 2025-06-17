0d5e803106df6df7bbd623f2e8006900cb07512d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-17 20:43:34 +0100

    fastfox only test

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -36,7 +36,7 @@
           .titlebar-buttonbox,.titlebar-spacer,#back-button,#forward-button,#tracking-protection-icon-container,#page-action-buttons,#PanelUI-button{display:none!important}#navigator-toolbox{border:0!important}#TabsToolbar{margin-left:20vw!important}#nav-bar{background:transparent!important;margin-right:80vw!important;margin-top:-36px!important}#urlbar-container{width:auto!important}#urlbar{background:transparent!important;border:none!important;box-shadow:none!important}#urlbar[breakout-extend]{width:100vw!important}
         '';
 
-        extraConfig = builtins.readFile ./config/user.js;
+        extraConfig = builtins.readFile ./config/fastfox.js;
       };
     };
 
