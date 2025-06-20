a8db31959d985cda68bb4ba0af46b53790eead3e
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-21 00:23:41 +0100

    remove slop

--- modules/nixos/docker/default.nix
+++ modules/nixos/docker/default.nix
@@ -8,23 +8,7 @@
 
   config = lib.mkIf config.modules.docker.enable {
 
-    users = {
-      groups.alex.gid = 1000;
-      users.alex = {
-        uid = 1000;
-        group = "alex";
-        extraGroups = ["docker"];
-      };
-    };
-     
-    # This option gives docker containers running
-    # as 1000:1000 permission to bind to any port
-    # starting from port 80. This is specifically
-    # required so that I can use caddy without
-    # running the container as root.
-    boot.kernel.sysctl = {
-      "net.ipv4.ip_unprivileged_port_start" = 80;
-    };
+    users.users.alex.extraGroups = ["docker"];
     
     virtualisation.docker = {
       enable = true;
