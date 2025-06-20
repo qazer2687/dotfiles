ca909c0720dbbc040d6ca1748ef788de990c89bd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-20 20:20:47 +0100

    test

--- modules/nixos/docker/default.nix
+++ modules/nixos/docker/default.nix
@@ -8,10 +8,13 @@
 
   config = lib.mkIf config.modules.docker.enable {
 
-    users.users.alex = {
-      subUidRanges = [{ startUid = 100000; count = 65536; }];
-      subGidRanges = [{ startGid = 100000; count = 65536; }];
-      extraGroups = ["docker"];
+    users = {
+      groups.alex.gid = 1000;
+      users.alex = {
+        uid = 1000;
+        group = "alex";
+        extraGroups = ["docker"];
+      };
     };
     
     virtualisation.docker = {
@@ -23,8 +26,6 @@
         # A fix for s6-svscan hanging on shutdown.
         # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
         live-restore = false;
-
-        userns-remap = "alex";
         
         # Use crun as the default runtime
         "default-runtime" = "crun";
