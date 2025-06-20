f15796aa93b55715762fe5a183fead5ba7c98cd8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-20 18:53:52 +0100

    docker stuff

--- modules/nixos/docker/default.nix
+++ modules/nixos/docker/default.nix
@@ -13,12 +13,17 @@
     users.users.alex.extraGroups = ["docker"];
     virtualisation.docker = {
       enable = true;
-      # A fix for s6-svscan hanging on shutdown.
-      # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
-      liveRestore = false;
       daemon.settings = {
         data-root = "/var/lib/docker";
         "hosts" = ["unix:///var/run/docker.sock"];
+        
+        # A fix for s6-svscan hanging on shutdown.
+        # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
+        live-restore = false;
+        
+        # Make all containers be owned by my user instead of root.
+        userns-remap = "alex";
+        
         # Use crun as the default runtime
         "default-runtime" = "crun";
         "runtimes" = {
