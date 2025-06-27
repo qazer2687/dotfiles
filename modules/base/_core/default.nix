cfcd94a933e74742613be64eca6128ae3a3b3347
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-27 13:29:11 +0100

    fix

--- modules/base/_core/default.nix
+++ modules/base/_core/default.nix
@@ -135,7 +135,7 @@
       extraPortals = [
         pkgs.xdg-desktop-portal-wlr
         pkgs.xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-hyprland
       ];
       xdgOpenUsePortal = true;
       config = {
