b801239c586dedff2572fad127815e99abfb54b8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-06-28 20:58:10 +0100

    tidy

--- packages/default.nix
+++ packages/default.nix
@@ -4,7 +4,5 @@ pkgs: {
   # example = pkgs.callPackage ./example { };
   widevinecdm-aarch64 = pkgs.callPackage ./widevinecdm-aarch64 {};
   arnis = pkgs.callPackage ./arnis {};
-  zen-browser = pkgs.callPackage ./zen-browser {};
   pragmatapro = pkgs.callPackage ./pragmatapro {};
-  dwl = pkgs.callPackage ./dwl {};
 }
