c7a85d1c5c11cec830e5fbcc149e0d3972ee2754
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-07-02 00:15:15 +0100

    syntax

--- modules/base/mica/ntp/default.nix
+++ modules/base/mica/ntp/default.nix
@@ -9,18 +9,18 @@
 
   config = lib.mkIf config.modules.chrony.enable {
     services.chrony = {
-    enable = true;
+      enable = true;
 
-    # Enable Network Time Security (NTS) for enhanced security.
-    enableNTS = true;
-    servers = [
-      "time.cloudflare.com iburst nts"
-      "time.google.com iburst nts"
-    ];
-  };
+      # Enable Network Time Security (NTS) for enhanced security.
+      enableNTS = true;
+      servers = [
+        "time.cloudflare.com iburst nts"
+        "time.google.com iburst nts"
+      ];
+    };
 
-  # Allow outgoing UDP port 123 (NTP) and outgoing TCP port 4460 (NTS Key Establishment).
-  networking.firewall.allowedUDPPorts = [ 123 ];
-  networking.firewall.allowedTCPPorts = [ 4460 ];
+    # Allow outgoing UDP port 123 (NTP) and outgoing TCP port 4460 (NTS Key Establishment).
+    networking.firewall.allowedUDPPorts = [ 123 ];
+    networking.firewall.allowedTCPPorts = [ 4460 ];
   };
 }
