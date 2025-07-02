0cee98a2752d2a20efc0fc03b8b77e75a0616a19
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-07-02 15:55:30 +0100

    pam

--- modules/base/mica/pam/default.nix
+++ modules/base/mica/pam/default.nix
@@ -1,21 +1,16 @@
 {
   lib,
   config,
-  pkgs,
-  inputs,
   ...
 }: {
   options.modules.pam.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.pam.enable {
-    environment.systemPackages = [ pkgs.pam_passwdqc ];
-    security.pam = {
-      loginDefs = {
-        SHA_CRYPT_MIN_ROUNDS = 5000;
-        SHA_CRYPT_MAX_ROUNDS = 5000;
-        PASS_MIN_DAYS = 1;
-        PASS_MAX_DAYS = 360;
-      };
+    security.loginDefs.settings = {
+      SHA_CRYPT_MIN_ROUNDS = 10000;
+      SHA_CRYPT_MAX_ROUNDS = 10000;
+      PASS_MIN_DAYS = 1;
+      PASS_MAX_DAYS = 360;
     };
   };
 }
