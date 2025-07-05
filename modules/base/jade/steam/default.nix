4f00a31df0e6f1e62eabc546f4c6490e12a7a6a5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-07-05 13:35:11 +0100

    test

--- modules/base/jade/steam/default.nix
+++ modules/base/jade/steam/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.modules.steam.enable = lib.mkEnableOption "";
@@ -8,6 +9,26 @@
   config = lib.mkIf config.modules.steam.enable {
     programs.steam = {
       enable = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs': with pkgs'; [
+          # Required for gamescope support.
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib # Provides libstdc++.so.6
+          libkrb5
+          keyutils
+          
+          # Misc
+          gamescope
+          gamemode
+          mangohud
+        ];
+      };
     };
   };
 }
