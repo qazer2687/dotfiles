6859d303ebb7b226a9f66599c84593ae7128e97d
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-07-13 22:45:41 +0100

    firewall

--- modules/base/mica/firewall/default.nix
+++ modules/base/mica/firewall/default.nix
@@ -9,11 +9,12 @@
     networking.firewall = {
       enable = true; # Ensure the firewall is enabled
 
-      trustedInterfaces = ["tailscale0" "docker0"];
+      #trustedInterfaces = ["tailscale0" "docker0"];
 
       allowedTCPPorts = [
         # HTTP
         80
+        81
         # HTTPS
         443
         # DNS
