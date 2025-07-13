155e84c77a32c7c510534c57d576c393502f7cae
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-07-13 23:11:05 +0100

    docker dns

--- modules/base/mica/docker/default.nix
+++ modules/base/mica/docker/default.nix
@@ -14,6 +14,7 @@
       daemon.settings = {
         data-root = "/var/lib/docker";
         "hosts" = ["unix:///var/run/docker.sock"];
+        dns = [ "1.1.1.1" ];
 
         # A fix for s6-svscan hanging on shutdown.
         # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
