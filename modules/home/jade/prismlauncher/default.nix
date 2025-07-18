350cfe046c5327a65e6bd9fe3efe1ac010c0f449
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-07-18 16:40:20 +0100

    fix

---
+++ modules/home/jade/prismlauncher/default.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.modules.prismlauncher.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.modules.prismlauncher.enable {
+    home.packages = with pkgs; [
+      prismlauncher
+      glfw
+    ];
+
+    home.file = {
+      ".jdks/8".source = lib.getBin pkgs.jdk8;
+      
+      ".jdks/11".source = lib.getBin pkgs.jdk11;
+
+      ".jdks/17".source = lib.getBin pkgs.jdk17;
+      
+      ".jdks/21".source = lib.getBin pkgs.jdk;
+    };
+  };
+}
