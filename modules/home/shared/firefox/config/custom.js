2268cae8e29855b5c313ab4c91e66297447f906a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-08-29 13:35:46 +0100

    disable hwaccel ff

--- modules/home/shared/firefox/config/custom.js
+++ modules/home/shared/firefox/config/custom.js
@@ -1,5 +1,7 @@
 /** Hardware Acceleration */
+/* these make the browser very laggy so either asahi doesn't support this yet or these aren't the right options
 user_pref(layers.acceleration.force-enabled);
 user_pref(gfx.webrender.all);
 user_pref(media.ffmpeg.vaapi.enabled);
 user_pref(media.hardware-video-decoding.force-enabled);
+*/
