68bceb9598029e3215cbaa1478b88fa0f9d56e56
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-03 17:09:19 +0100

    fix

--- modules/home/jet/hyprland/default.nix
+++ modules/home/jet/hyprland/default.nix
@@ -38,18 +38,18 @@
           border_size = 2;
 
           col = rec {
-            active_border = "#a6e3a1";
+        active_border = "#a6e3a1";
 
-            inactive_border =
-            let
-              hexToInt = hex: builtins.fromJSON ("\"0x${hex}\"");
-              intToHex = i: let s = builtins.toString i; in if i < 16 then "0${s}" else s;
-              r = hexToInt (builtins.substring 1 2 active_border);
-              g = hexToInt (builtins.substring 3 2 active_border);
-              b = hexToInt (builtins.substring 5 2 active_border);
-            in
-              "#" + intToHex (r * 80 / 100) + intToHex (g * 80 / 100) + intToHex (b * 80 / 100);
-          };
+        inactive_border =
+          let
+            hexToInt = hex: builtins.parseInt hex 16;  # <--- use parseInt base 16
+            intToHex = i: let s = builtins.toString i; in if i < 16 then "0${s}" else s;
+            r = hexToInt (builtins.substring 1 2 active_border);
+            g = hexToInt (builtins.substring 3 2 active_border);
+            b = hexToInt (builtins.substring 5 2 active_border);
+          in
+            "#" + intToHex (r * 80 / 100) + intToHex (g * 80 / 100) + intToHex (b * 80 / 100);
+      };
         };
 
         master = {
