3971eceef15789fee117e57bf68856cfe321eeea
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-05 13:51:22 +0100

    test

--- modules/home/jet/hyprlock/default.nix
+++ modules/home/jet/hyprlock/default.nix
@@ -42,10 +42,10 @@
         "input-field" = {
           size = "920, 72";
           outline_thickness = 0;
-          dots_size = 0.5;
-          font_family = "Departure Mono";
-          dots_text_format = "x";
-          dots_spacing = 1.5;
+          dots_size = 0.4;
+          font_family = "TX02";
+          dots_text_format = "X";
+          dots_spacing = 0.8;
           dots_center = true;
           outer_color = "rgba(202, 158, 230, 1)";  # Frappe Mauve
           inner_color = "rgba(48, 52, 70, 1)"; # Frappe Base
