348f3efd1584d2b06034cedb6518e33bb0f9451e
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-05 01:46:01 +0100

    fix

--- modules/home/jet/foot/default.nix
+++ modules/home/jet/foot/default.nix
@@ -26,40 +26,40 @@
           hide-when-typing = "no";
         };
         colors = {
-            # Catppuccin Frappe
-            cursor = "232634 f2d5cf";
-            foreground = "c6d0f5";
-            background = "303446";
+          # Catppuccin Frappe
+          cursor = "232634 f2d5cf";
+          foreground = "c6d0f5";
+          background = "303446";
 
-            regular0 = "51576d";
-            regular1 = "e78284";
-            regular2 = "a6d189";
-            regular3 = "e5c890";
-            regular4 = "8caaee";
-            regular5 = "f4b8e4";
-            regular6 = "81c8be";
-            regular7 = "b5bfe2";
+          regular0 = "51576d";
+          regular1 = "e78284";
+          regular2 = "a6d189";
+          regular3 = "e5c890";
+          regular4 = "8caaee";
+          regular5 = "f4b8e4";
+          regular6 = "81c8be";
+          regular7 = "b5bfe2";
 
-            bright0 = "626880";
-            bright1 = "e78284";
-            bright2 = "a6d189";
-            bright3 = "e5c890";
-            bright4 = "8caaee";
-            bright5 = "f4b8e4";
-            bright6 = "81c8be";
-            bright7 = "a5adce";
+          bright0 = "626880";
+          bright1 = "e78284";
+          bright2 = "a6d189";
+          bright3 = "e5c890";
+          bright4 = "8caaee";
+          bright5 = "f4b8e4";
+          bright6 = "81c8be";
+          bright7 = "a5adce";
 
-            "16" = "ef9f76";
-            "17" = "f2d5cf";
+          "16" = "ef9f76";
+          "17" = "f2d5cf";
 
-            selection-foreground = "c6d0f5";
-            selection-background = "4f5369";
+          selection-foreground = "c6d0f5";
+          selection-background = "4f5369";
 
-            search-box-no-match = "232634 e78284";
-            search-box-match = "c6d0f5 414559";
+          search-box-no-match = "232634 e78284";
+          search-box-match = "c6d0f5 414559";
 
-            jump-labels = "232634 ef9f76";
-            urls = "8caaee";
+          jump-labels = "232634 ef9f76";
+          urls = "8caaee";
         };
         key-bindings = {
           clipboard-copy = "Control+c XF86Copy";
