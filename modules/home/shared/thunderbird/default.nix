d22f322faa758ffc3b8f0ff98f10f4407802f406
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-13 14:39:32 +0100

    fix

--- modules/home/shared/thunderbird/default.nix
+++ modules/home/shared/thunderbird/default.nix
@@ -12,14 +12,6 @@ in {
   options.modules.thunderbird.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.thunderbird.enable {
-
-    sops.secrets = {
-      personal-info = {
-        sopsFile = ../../../../secrets/email.yaml;
-        mode = "0400";
-      };
-    };
-
     programs.thunderbird = {
       enable = true;
 
@@ -30,35 +22,7 @@ in {
             "extensions.autoDisableScopes" = 0;
           };
           extensions = [ frappe ];
-          accountsOrder = [ "gmail" "outlook" "sussex" "Local Folders" ];
         };
-      };
-    };
-
-    accounts.email.accounts = {
-      gmail = {
-        address = builtins.readFile (config.sops.secrets.email.path + "/gmail");
-        realName = builtins.readFile (config.sops.secrets.email.path + "/realname");
-        imap.host = "imap.gmail.com";
-        smtp.host = "smtp.gmail.com";
-        thunderbird.enable = true;
-        primary = true;
-      };
-
-      outlook = {
-        address = builtins.readFile (config.sops.secrets.email.path + "/outlook");
-        realName = builtins.readFile (config.sops.secrets.email.path + "/realname");
-        imap.host = "outlook.office365.com";
-        smtp.host = "smtp.office365.com";
-        thunderbird.enable = true;
-      };
-
-      sussex = {
-        address = builtins.readFile (config.sops.secrets.email.path + "/sussex");
-        realName = builtins.readFile (config.sops.secrets.email.path + "/realname");
-        imap.host = "outlook.office365.com";
-        smtp.host = "smtp.office365.com";
-        thunderbird.enable = true;
       };
     };
   };
