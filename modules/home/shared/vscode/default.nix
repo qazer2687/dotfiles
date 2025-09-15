b2a2314a022f94308d2337105e56e1ea5d93f3d3
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-15 18:27:12 +0100

    test

--- modules/home/shared/vscode/default.nix
+++ modules/home/shared/vscode/default.nix
@@ -77,59 +77,55 @@ in {
         # Direnv
         "direnv.restart.automatic" = true;
 
-        # Base16 Core
-        "editor.background" = "#${scheme.base00}";
-        "editor.foreground" = "#${scheme.base05}";
-        "editor.lineHighlightBackground" = "#${scheme.base01}";
-        "editor.selectionBackground" = "#${scheme.base02}";
-        "editor.selectionHighlightBackground" = "#${scheme.base03}";
-        "editorCursor.foreground" = "#${scheme.base05}";
-        "editorWhitespace.foreground" = "#${scheme.base03}";
-        "editorIndentGuide.background" = "#${scheme.base01}";
-        "editorIndentGuide.activeBackground" = "#${scheme.base03}";
-        "editorLineNumber.foreground" = "#${scheme.base04}";
-        "editorLineNumber.activeForeground" = "#${scheme.base05}";
-
-        # Base16 UI Panels & Tabs
-        "sideBar.background" = "#${scheme.base01}";
-        "sideBar.foreground" = "#${scheme.base04}";
-        "activityBar.background" = "#${scheme.base00}";
-        "activityBar.foreground" = "#${scheme.base05}";
-        "statusBar.background" = "#${scheme.base00}";
-        "statusBar.foreground" = "#${scheme.base05}";
-        "titleBar.activeBackground" = "#${scheme.base00}";
-        "titleBar.activeForeground" = "#${scheme.base05}";
-        "panel.background" = "#${scheme.base01}";
-        "panel.border" = "#${scheme.base02}";
-        "tab.activeBackground" = "#${scheme.base00}";
-        "tab.inactiveBackground" = "#${scheme.base01}";
-        "tab.activeForeground" = "#${scheme.base05}";
-        "tab.inactiveForeground" = "#${scheme.base04}";
-
-        # Base16 Bracket Matching & Highlights
-        "editorBracketMatch.border" = "#${scheme.base04}";
-        "editorBracketMatch.background" = "#${scheme.base01}";
-        "editorBracketHighlight.foreground1" = "#${scheme.base08}";
-        "editorBracketHighlight.foreground2" = "#${scheme.base09}";
-        "editorBracketHighlight.foreground3" = "#${scheme.base0A}";
-        "editorBracketHighlight.foreground4" = "#${scheme.base0B}";
-        "editorBracketHighlight.foreground5" = "#${scheme.base0C}";
-        "editorBracketHighlight.foreground6" = "#${scheme.base0D}";
-
-        # Base16 Misc
-        "minimap.background" = "#${scheme.base00}";
+        "workbench.colorCustomizations" = {
+          "editor.background" = "#${scheme.base00}";
+          "editor.foreground" = "#${scheme.base05}";
+          "editorGroup.background" = "#${scheme.base00}";
+          "editorGroupHeader.tabsBackground" = "#${scheme.base00}";
+          "editorGroup.emptyBackground" = "#${scheme.base00}";
+          "editorWidget.background" = "#${scheme.base00}";
+          "editorHoverWidget.background" = "#${scheme.base00}";
+          "editor.lineHighlightBackground" = "#${scheme.base01}";
+          "editor.selectionBackground" = "#${scheme.base02}";
+          "editor.selectionHighlightBackground" = "#${scheme.base01}";
+          "editorCursor.foreground" = "#${scheme.base05}";
+          "editorLineNumber.foreground" = "#${scheme.base03}";
+          "editorLineNumber.activeForeground" = "#${scheme.base05}";
+          "editorWhitespace.foreground" = "#${scheme.base03}";
+          "editorIndentGuide.background" = "#${scheme.base01}";
+          "editorIndentGuide.activeBackground" = "#${scheme.base03}";
+          "editorBracketMatch.border" = "#${scheme.base04}";
+          "editorBracketMatch.background" = "#${scheme.base01}";
+          "sideBar.background" = "#${scheme.base01}";
+          "sideBar.foreground" = "#${scheme.base04}";
+          "activityBar.background" = "#${scheme.base00}";
+          "activityBar.foreground" = "#${scheme.base05}";
+          "statusBar.background" = "#${scheme.base00}";
+          "statusBar.foreground" = "#${scheme.base05}";
+          "titleBar.activeBackground" = "#${scheme.base00}";
+          "titleBar.activeForeground" = "#${scheme.base05}";
+          "tab.activeBackground" = "#${scheme.base00}";
+          "tab.inactiveBackground" = "#${scheme.base01}";
+          "tab.activeForeground" = "#${scheme.base05}";
+          "tab.inactiveForeground" = "#${scheme.base04}";
+          "panel.background" = "#${scheme.base00}";
+          "panel.border" = "#${scheme.base01}";
+          "terminal.background" = "#${scheme.base00}";
+          "minimap.background" = "#${scheme.base00}";
+        };
 
         "editor.tokenColorCustomizations" = {
-          "textMateRules" = [
+          textMateRules = [
             { scope = "comment";              settings.foreground = "#${scheme.base03}"; settings.fontStyle = "italic"; }
-            { scope = "keyword";              settings.foreground = "#${scheme.base0E}"; settings.fontStyle = "bold"; }
-            { scope = "variable";             settings.foreground = "#${scheme.base08}"; }
-            { scope = "string";               settings.foreground = "#${scheme.base0B}"; }
-            { scope = "constant.numeric";     settings.foreground = "#${scheme.base09}"; }
+            { scope = "keyword";              settings.foreground = "#${scheme.base0E}"; }
             { scope = "storage.type";         settings.foreground = "#${scheme.base0A}"; }
+            { scope = "variable";             settings.foreground = "#${scheme.base08}"; }
+            { scope = "variable.parameter";   settings.foreground = "#${scheme.base08}"; }
             { scope = "entity.name.function"; settings.foreground = "#${scheme.base0D}"; }
             { scope = "entity.name.class";    settings.foreground = "#${scheme.base0A}"; }
-            { scope = "support.type";         settings.foreground = "#${scheme.base0A}"; }
+            { scope = "entity.name.tag";      settings.foreground = "#${scheme.base08}"; }
+            { scope = "string";               settings.foreground = "#${scheme.base0B}"; }
+            { scope = "constant.numeric";     settings.foreground = "#${scheme.base09}"; }
             { scope = "support.constant";     settings.foreground = "#${scheme.base0C}"; }
             { scope = "punctuation";          settings.foreground = "#${scheme.base05}"; }
             { scope = "keyword.operator";     settings.foreground = "#${scheme.base0C}"; }
