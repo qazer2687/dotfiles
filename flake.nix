2379ee096b46858852b5ea592973a353bb2aec5b
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-18 22:59:49 +0100

    rollback

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Unable to play videos on firefox with latest.
     nixpkgs.url = "github:NixOS/nixpkgs/8a6d5427d99ec71c64f0b93d45778c889005d9c2";
 
     home-manager = {
