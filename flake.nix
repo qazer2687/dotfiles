1baecccf131f62ba94b4586e8d38d642d7df4957
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-18 22:59:29 +0100

    rollback

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:NixOS/nixpkgs/8a6d5427d99ec71c64f0b93d45778c889005d9c2";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/8a6d5427d99ec71c64f0b93d45778c889005d9c2";
 
     home-manager = {
       url = "github:nix-community/home-manager";
