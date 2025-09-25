a51d482e86f28880926dfc4a2bec37a8d39130d6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-25 13:39:55 +0100

    test

--- modules/base/jet/networkmanager/default.nix
+++ modules/base/jet/networkmanager/default.nix
@@ -15,39 +15,40 @@
     in
       (mkNetworkSecrets "wifinity" [ "id" "ssid" "psk" ] ../../../../secrets/networks/wifinity.yaml) //
       (mkNetworkSecrets "eduroam" [ "id" "ssid" "identity" "anonymous-identity" "phase2-identity" "phase2-password" ] ../../../../secrets/networks/eduroam.yaml);
-
-    /*
+    
     networking.wireless.iwd = {
       enable = true;
       settings.General.EnableNetworkConfiguration = true;
     };
-    */
+    
 
     networking.networkmanager = {
       enable = true;
       wifi = {
         powersave = true;
-        #backend = "iwd";
+        backend = "iwd";
       };
     };
 
     users.users.alex.extraGroups = [ "networkmanager" ];
 
-    environment.etc = {
+    
+    sops.templates = {
       "NetworkManager/system-connections/wifinity.nmconnection" = {
-        text = ''
+        content = ''
           [connection]
-          id=$(cat ${config.sops.secrets."wifinity/id".path})
+          id=${config.sops.placeholder."wifinity/id"}
           type=wifi
+          autoconnect=true
 
           [wifi]
           mode=infrastructure
-          ssid=$(cat ${config.sops.secrets."wifinity/ssid".path})
+          ssid=${config.sops.placeholder."wifinity/ssid"}
 
           [wifi-security]
           auth-alg=open
           key-mgmt=wpa-psk
-          psk=$(cat ${config.sops.secrets."wifinity/psk".path})
+          psk=${config.sops.placeholder."wifinity/psk"}
 
           [ipv4]
           method=auto
@@ -56,29 +57,33 @@
           addr-gen-mode=default
           method=auto
         '';
+        path = "/etc/NetworkManager/system-connections/wifinity.nmconnection";
         mode = "0600";
+        owner = "root";
+        group = "root";
       };
 
       "NetworkManager/system-connections/eduroam.nmconnection" = {
-        text = ''
+        content = ''
           [connection]
-          id=$(cat ${config.sops.secrets."eduroam/id".path})
+          id=${config.sops.placeholder."eduroam/id"}
           type=wifi
+          autoconnect=true
 
           [wifi]
           mode=infrastructure
-          ssid=$(cat ${config.sops.secrets."eduroam/ssid".path})
+          ssid=${config.sops.placeholder."eduroam/ssid"}
 
           [wifi-security]
           key-mgmt=wpa-eap
 
           [802-1x]
           eap=peap
-          identity=$(cat ${config.sops.secrets."eduroam/identity".path})
-          anonymous-identity=$(cat ${config.sops.secrets."eduroam/anonymous-identity".path})
+          identity=${config.sops.placeholder."eduroam/identity"}
+          anonymous-identity=${config.sops.placeholder."eduroam/anonymous-identity"}
           phase2-auth=mschapv2
-          phase2-identity=$(cat ${config.sops.secrets."eduroam/phase2-identity".path})
-          password=$(cat ${config.sops.secrets."eduroam/phase2-password".path})
+          password=${config.sops.placeholder."eduroam/phase2-password"}
+          phase2-identity=${config.sops.placeholder."eduroam/phase2-identity"}
 
           [ipv4]
           method=auto
@@ -87,7 +92,10 @@
           addr-gen-mode=default
           method=auto
         '';
+        path = "/etc/NetworkManager/system-connections/eduroam.nmconnection";
         mode = "0600";
+        owner = "root";
+        group = "root";
       };
     };
   };
