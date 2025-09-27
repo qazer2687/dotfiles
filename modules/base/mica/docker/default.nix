461fcbcff00b7523b7929750cba469f085a5e937
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-09-27 01:55:53 +0100

    fix

--- modules/base/mica/docker/default.nix
+++ modules/base/mica/docker/default.nix
@@ -16,7 +16,8 @@
         "hosts" = ["unix:///var/run/docker.sock"];
         # Fix for some services just not being able to communicate
         # with the web, for completely unknown reasons.
-        dns = ["1.1.1.1"];
+        # Enabling this seems to break netbird.
+        #dns = ["1.1.1.1"];
 
         # A fix for s6-svscan hanging on shutdown.
         # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
