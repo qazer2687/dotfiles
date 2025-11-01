1216dcc7f2a19d669e9e25ea797bf1db61645954
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-01 13:04:18 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     nyx.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
     flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    #textfox.url = "github:adriankarlen/textfox";
+    textfox.url = "github:adriankarlen/textfox";
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
