863571f81570ac60a98004c7aa0becd6b807cc61
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-01 14:37:43 +0000

    vicinae

--- modules/home/jet/hyprland/default.nix
+++ modules/home/jet/hyprland/default.nix
@@ -140,7 +140,8 @@ in {
         bind = [
           # Core
           "SUPER, Return, exec, foot"
-          "SUPER, E, exec, tofi-run | sh"
+          #"SUPER, E, exec, tofi-run | sh"
+          "SUPER, E, exec, vicinae open"
           "SUPER, Q, killactive"
           "SUPER, F, fullscreen"
 
