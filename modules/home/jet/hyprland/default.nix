564573214ab4e0dcd5c3ce97f02199493086af46
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-02 01:17:05 +0000

    test

--- modules/home/jet/hyprland/default.nix
+++ modules/home/jet/hyprland/default.nix
@@ -69,8 +69,8 @@ in {
 
           blur = {
             enabled = true;
-            size = 1;
-            passes = 4;
+            size = 4;
+            passes = 2;
             ignore_opacity = true;
             new_optimizations = true;
           };
