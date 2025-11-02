2a19d6757315ab8dc9c38f1567b0d3399744d290
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-02 01:55:35 +0000

    test

--- modules/home/jet/waybar/default.nix
+++ modules/home/jet/waybar/default.nix
@@ -123,7 +123,7 @@ in {
         }
 
         window#waybar {
-          background-color: rgba(15, 15, 15, 0.9)
+          background-color: rgba(15, 15, 15, 0.85)
         }
 
         #mpris, #clock, #language, #pulseaudio, #bluetooth, #network,
