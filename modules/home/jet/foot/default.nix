5a8d295a66f204056b809694bba07f1310c77482
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-17 03:20:08 +0000

    fix

--- modules/home/jet/foot/default.nix
+++ modules/home/jet/foot/default.nix
@@ -13,8 +13,8 @@ in {
       server.enable = true;
       settings = {
         main = {
-          font = "ProggySquare.pcf:size=18, FiraCode Nerd Font:size=14";
-          font-bold = "ProggySquare.pcf:size=18:style=Bold, FiraCode Nerd Font:size=14";
+          font = "FiraCode Nerd Font:size=18, FiraCode Nerd Font:size=14";
+          font-bold = "FiraCode Nerd Font:size=18:style=Bold, FiraCode Nerd Font:size=14";
           line-height = "22px";
           pad = "8x8";
         };
