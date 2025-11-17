ebdc61d39020dc6917fa067fc45b2bf24b14a4d2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-17 02:57:10 +0000

    test

--- modules/home/jet/foot/default.nix
+++ modules/home/jet/foot/default.nix
@@ -13,8 +13,8 @@ in {
       server.enable = true;
       settings = {
         main = {
-          font = "ProggySquare:size=18, FiraCode Nerd Font:size=14";
-          font-bold = "ProggySquare:size=18:style=Bold, FiraCode Nerd Font:size=14";
+          font = "ProggyCleanSZ:size=18, FiraCode Nerd Font:size=14";
+          font-bold = "ProggyCleanSZ:size=18:style=Bold, FiraCode Nerd Font:size=14";
           line-height = "22px";
           pad = "8x8";
         };
