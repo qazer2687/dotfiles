6f99ae4f719b6b0dce6f4e12498193189cbeb882
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-20 19:09:02 +0000

    mesa fix

--- hosts/jet/default.nix
+++ hosts/jet/default.nix
@@ -25,6 +25,15 @@
     };
   };
 
+  hardware.graphics.package =
+    # Workaround for Mesa 25.3.0 regression
+    # https://github.com/nix-community/nixos-apple-silicon/issues/380
+    assert pkgs.mesa.version == "25.3.0";
+    (import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/c5ae371f1a6a7fd27823bc500d9390b38c05fa55.tar.gz";
+      sha256 = "sha256-4PqRErxfe+2toFJFgcRKZ0UI9NSIOJa+7RXVtBhy4KE=";
+  }) { localSystem = pkgs.stdenv.hostPlatform; }).mesa;
+
   services.udev = {
     extraRules = ''
       # Allow backlight control for non-root users.
