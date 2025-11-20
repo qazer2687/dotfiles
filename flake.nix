698873e23aee3e2fea63d5fc73930d719d9b18b6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-20 19:06:01 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ashai.url = "github:nix-community/nixos-apple-silicon";
+    asahi.url = "github:nix-community/nixos-apple-silicon";
     #asahi.url = "github:nix-community/nixos-apple-silicon/b99bf9bf7445416fe55da09034fc4a6cd733805c";
 
     nyx.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
