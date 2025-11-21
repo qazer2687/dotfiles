5c69a8c50782a3d4b0beae7daeac7a623b529579
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-21 22:44:35 +0000

    not that

--- hosts/sage/default.nix
+++ hosts/sage/default.nix
@@ -13,16 +13,7 @@
       hashedPassword = "$6$qRDf73LqqlnrtGKd$fwNbmyhVjAHfgjPpM.Wn8YoYVbLRq1oFWN15fjP3b.cVW8Dv3s/7q8NY4WBYY7x1Xe71S.AHpuqL1PY6IJe0x1";
     };
   };
-
-  hardware.graphics.package =
-    # Workaround for Mesa 25.3.0 regression
-    # https://github.com/nix-community/nixos-apple-silicon/issues/380
-    assert pkgs.mesa.version == "25.3.0";
-    (import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/c5ae371f1a6a7fd27823bc500d9390b38c05fa55.tar.gz";
-      sha256 = "sha256-4PqRErxfe+2toFJFgcRKZ0UI9NSIOJa+7RXVtBhy4KE=";
-  }) { localSystem = pkgs.stdenv.hostPlatform; }).mesa;
-
+  
   programs.fish.enable = true;
 
   boot = {
