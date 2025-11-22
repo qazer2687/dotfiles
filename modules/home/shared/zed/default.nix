7110cd72d07065c83ae08f107e86230bff935022
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-22 01:29:27 +0000

    zed fhs

--- modules/home/shared/zed/default.nix
+++ modules/home/shared/zed/default.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   osConfig,
+  pkgs,
   ...
 }: {
   options.modules.zed.enable = lib.mkEnableOption "";
@@ -9,6 +10,7 @@
   config = lib.mkIf config.modules.zed.enable {
     programs.zed-editor = {
       enable = true;
+      package = pkgs.zed-editor-fhs;
       installRemoteServer = (osConfig.networking.hostName == "mica");
     };
 
