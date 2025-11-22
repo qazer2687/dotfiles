d5ef2d221fc32ba4ee6772bfdb2e8486984545ff
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-22 13:03:53 +0000

    test

--- modules/home/sage/firefox/config/custom.js
+++ modules/home/sage/firefox/config/custom.js
@@ -12,3 +12,13 @@ user_pref("gfx.webrender.software", false);
 user_pref("webgl.disabled", false);
 user_pref("media.hardware-video-decoding.force-enabled", true);
 */
+
+user_pref("gfx.webrender.all", false);
+user_pref("gfx.webrender.enabled", false);
+user_pref("layers.acceleration.force-enabled", false);
+user_pref("gfx.canvas.azure.accelerated", false);
+user_pref("gfx.webrender.compositor", false);
+user_pref("gfx.webrender.compositor.force-enabled", false);
+user_pref("gfx.webrender.software", true);
+user_pref("webgl.disabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", false);
