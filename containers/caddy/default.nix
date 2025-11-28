9bc5cdc5717587e92e576b8abcd6af2cef681fee
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-28 22:35:23 +0000

    fix

--- containers/caddy/default.nix
+++ containers/caddy/default.nix
@@ -2,277 +2,173 @@
   lib,
   config,
   pkgs,
-  inputs,
   ...
 }: {
   options.modules.containers.caddy.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.containers.caddy.enable {
-
     sops.secrets.CLOUDFLARE_API_TOKEN = {
       sopsFile = ../../secrets/containers/caddy.yaml;
     };
     
-    sops.templates."caddy" = {
-      content = ''
-        CLOUDFLARE_API_TOKEN=${config.sops.placeholder.CLOUDFLARE_API_TOKEN}
-      '';
+    sops.templates."caddy".content = ''
+      CLOUDFLARE_API_TOKEN=${config.sops.placeholder.CLOUDFLARE_API_TOKEN}
+    '';
+
+    systemd.services.caddy.serviceConfig = {
+      EnvironmentFile = config.sops.templates."caddy".path;
     };
 
-    users.users.caddy = {
-      isSystemUser = true;
-      group = "caddy";
-    };
-    users.groups.caddy = {};
-
-    systemd.tmpfiles.rules = [
-      "d /srv/caddy/etc/caddy 0755 caddy caddy -"
-      "d /srv/caddy/data 0755 caddy caddy -"
-      "d /srv/caddy/config 0755 caddy caddy -"
-      "d /srv/caddy/srv/assets 0755 caddy caddy -"
-    ];
-
-    containers.caddy = {
-      autoStart = true;
-      # Enable host networking.
-      privateNetwork = false;
-      privateUsers = "no";
-      ephemeral = true;
-
-      # NET_ADMIN
-      allowedDevices = [
-        {
-          modifier = "rwm";
-          node = "/dev/net/tun";
-        }
-      ];
-
-      bindMounts = {
-        "/etc/caddy" = {
-          hostPath = "/srv/caddy/etc/caddy";
-          isReadOnly = false;
-        };
-        "/data" = {
-          hostPath = "/srv/caddy/data";
-          isReadOnly = false;
-        };
-        "/config" = {
-          hostPath = "/srv/caddy/config";
-          isReadOnly = false;
-        };
-        "/srv/assets" = {
-          hostPath = "/srv/caddy/srv/assets";
-          isReadOnly = false;
-        };
-        "/run/secrets/caddy" = {
-          hostPath = config.sops.templates."caddy".path;
-          isReadOnly = true;
-        };
+    services.caddy = {
+      enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/cloudflare@2fc25ee62f40fe21b240f83ab2fb6e2be6dbb953" ];
+        hash = "sha256-RLOwzx7+SH9sWVlr+gTOp5VKlS1YhoTXHV4k6r5BJ3U=";
+        doInstallCheck = false;
       };
-
-      config = { config, pkgs, ... }: {
+      
+      virtualHosts = {
+        "assets.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          root * /srv/assets
+          file_server
+        '';
         
-        users.users.caddy = {
-          isSystemUser = true;
-          group = "caddy";
+        "portainer.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:9000
+        '';
+        
+        "vaultwarden.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:8181
+        '';
+        
+        "searxng.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:8585
+        '';
+        
+        "prowlarr.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:9696
+        '';
+        
+        "radarr.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:7878
+        '';
+        
+        "bazarr.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:6767
+        '';
+        
+        "sonarr.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:8989
+        '';
+        
+        "readarr.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:8787
+        '';
+        
+        "qbittorrent.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:8080
+        '';
+        
+        "grafana.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:8084
+        '';
+        
+        "copyparty.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:3923
+        '';
+        
+        "speedtest.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:7978
+        '';
+        
+        "glance.qazer.org" = {
+          extraConfig = ''
+            tls {
+              dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+            }
+            reverse_proxy localhost:8140
+          '';
         };
-        users.groups.caddy = {};
-
-        systemd.services.caddy.serviceConfig = {
-          EnvironmentFile = "/run/secrets/caddy";
-          # Override DynamicUser to use our static caddy user
-          DynamicUser = false;
-          User = "caddy";
-          Group = "caddy";
-        };
-
-        services.caddy = {
-          enable = true;
-          package = pkgs.caddy.withPlugins {
-            plugins = [ "github.com/caddy-dns/cloudflare@2fc25ee62f40fe21b240f83ab2fb6e2be6dbb953" ];
-            hash = "sha256-RLOwzx7+SH9sWVlr+gTOp5VKlS1YhoTXHV4k6r5BJ3U=";
-            doInstallCheck = false;
-          };
-          dataDir = "/data";
-          # configFile = "/etc/caddy/Caddyfile";
-          
-          # Move all of these into the container configurations themselves.
-          virtualHosts = {
-            "assets.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                root * /srv/assets
-                file_server
-              '';
-            };
-            
-            "portainer.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:9000
-              '';
-            };
-            
-            "vaultwarden.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8181
-              '';
-            };
-            
-            "searxng.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8585
-              '';
-            };
-            
-            "prowlarr.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:9696
-              '';
-            };
-            
-            "radarr.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:7878
-              '';
-            };
-            
-            "bazarr.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:6767
-              '';
-            };
-            
-            "sonarr.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8989
-              '';
-            };
-            
-            "readarr.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8787
-              '';
-            };
-            
-            "qbittorrent.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8080
-              '';
-            };
-            
-            "grafana.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8084
-              '';
-            };
-            
-            "copyparty.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:3923
-              '';
-            };
-            
-            "speedtest.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:7978
-              '';
-            };
-            
-            "glance.qazer.org" = {
-              serverAliases = [ "start.qazer.org" "qazer.org" ];
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:8140
-              '';
-            };
-            
-            "immich.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:2283
-              '';
-            };
-            
-            "audiobookshelf.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:13378
-              '';
-            };
-            
-            "jellyseerr.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:5055
-              '';
-            };
-            
-            "n8n.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-                }
-                reverse_proxy localhost:5678
-              '';
-            };
-            
-            "jellyfin.qazer.org" = {
-              extraConfig = ''
-                tls {
-                  ca https://acme-v02.api.letsencrypt.org/directory
-                }
-                reverse_proxy localhost:8096
-              '';
-            };
-          };
-        };
+        
+        "immich.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:2283
+        '';
+        
+        "audiobookshelf.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:13378
+        '';
+        
+        "jellyseerr.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:5055
+        '';
+        
+        "n8n.qazer.org".extraConfig = ''
+          tls {
+            dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy localhost:5678
+        '';
+        
+        "jellyfin.qazer.org".extraConfig = ''
+          tls {
+            ca https://acme-v02.api.letsencrypt.org/directory
+          }
+          reverse_proxy localhost:8096
+        '';
       };
     };
+
+    # Create assets directory
+    systemd.tmpfiles.rules = [
+      "d /srv/assets 0755 caddy caddy -"
+    ];
   };
 }
