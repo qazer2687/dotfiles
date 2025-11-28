539174d50fb297e2a23ef4f1d63505dceebcc9f6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-11-28 20:24:51 +0000

    test

--- modules/base/mica/firewall/default.nix
+++ modules/base/mica/firewall/default.nix
@@ -6,63 +6,61 @@
   options.modules.firewall.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.firewall.enable {
-    networking = {
+    bridges = {
+      public = {};
+      private = {};
+    };
 
-      interfaces = {
-        public = {
-          type = "bridge";
-          ipv4.addresses = [ { address = "10.0.10.1"; prefixLength = 24; } ];
-        };
-
-        private = {
-          type = "bridge";
-          ipv4.addresses = [ { address = "10.0.20.1"; prefixLength = 24; } ];
-        };
+    interfaces = {
+      public = {
+        ipv4.addresses = [ { address = "10.0.10.1"; prefixLength = 24; } ];
       };
-
-    
-    
-      firewall = {
-        enable = true;
-
-        #trustedInterfaces = ["tailscale0" "docker0"];
-
-        allowedTCPPorts = [
-          # SSH
-          22
-          # HTTP
-          80
-          81
-          # HTTPS
-          443
-          # DNS
-          53
-          # qBittorrent
-          6881
-          # Netbird
-          33073
-          10000
-        ];
-
-        allowedUDPPorts = [
-          # DNS
-          53
-          # qBittorrent
-          6881
-          # Netbird
-          443
-          3478
-          51820
-        ];
-
-        allowedUDPPortRanges = [
-          {
-            # Netbird
-            from = 49152;
-            to = 65535;
-          }
-        ];
+      private = {
+        ipv4.addresses = [ { address = "10.0.20.1"; prefixLength = 24; } ];
       };
+    };
+  
+  
+    firewall = {
+      enable = true;
+
+      #trustedInterfaces = ["tailscale0" "docker0"];
+
+      allowedTCPPorts = [
+        # SSH
+        22
+        # HTTP
+        80
+        81
+        # HTTPS
+        443
+        # DNS
+        53
+        # qBittorrent
+        6881
+        # Netbird
+        33073
+        10000
+      ];
+
+      allowedUDPPorts = [
+        # DNS
+        53
+        # qBittorrent
+        6881
+        # Netbird
+        443
+        3478
+        51820
+      ];
+
+      allowedUDPPortRanges = [
+        {
+          # Netbird
+          from = 49152;
+          to = 65535;
+        }
+      ];
     };
   };
 }
