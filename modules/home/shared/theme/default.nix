0f036211e3e49d597a51190f6de6c5daee1148bb
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-03 02:03:58 +0000

    test

--- modules/home/shared/theme/default.nix
+++ modules/home/shared/theme/default.nix
@@ -6,12 +6,39 @@
 }: {
   options.modules.theme.enable = lib.mkEnableOption "";
   config = lib.mkIf config.modules.theme.enable {
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
-      };
+    home.pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 16;
+    };
+    
+    # Don't use gtk.enable, configure files directly
+    home.packages = with pkgs; [
+      gnome-themes-extra
+      adwaita-icon-theme
+    ];
+    
+    xdg.configFile."gtk-3.0/settings.ini".text = ''
+      [Settings]
+      gtk-theme-name=Adwaita-dark
+      gtk-icon-theme-name=Adwaita
+      gtk-cursor-theme-name=Bibata-Modern-Ice
+      gtk-cursor-theme-size=16
+      gtk-application-prefer-dark-theme=true
+    '';
+    
+    xdg.configFile."gtk-4.0/settings.ini".text = ''
+      [Settings]
+      gtk-theme-name=Adwaita-dark
+      gtk-icon-theme-name=Adwaita
+      gtk-cursor-theme-name=Bibata-Modern-Ice
+      gtk-cursor-theme-size=16
+      gtk-application-prefer-dark-theme=true
+    '';
+    
+    home.sessionVariables = {
+      GTK_THEME = "Adwaita-dark";
     };
   };
 }
