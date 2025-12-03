bd1bf5b3377818505689924dea0d55d547a26190
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-03 01:58:36 +0000

    test

--- modules/home/shared/theme/default.nix
+++ modules/home/shared/theme/default.nix
@@ -6,38 +6,6 @@
 }: {
   options.modules.theme.enable = lib.mkEnableOption "";
   config = lib.mkIf config.modules.theme.enable {
-    home.pointerCursor = {
-      gtk.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 16;
-    };
-    
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
-      };
-      iconTheme = {
-        name = "Adwaita";
-        package = pkgs.adwaita-icon-theme;
-      };
-      cursorTheme = {
-        name = "Bibata-Modern-Ice";
-        package = pkgs.bibata-cursors;
-      };
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-      };
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-      };
-    };
-    
-    # Environment variables only - no dconf, no qt module
-    home.sessionVariables = {
-      GTK_THEME = "Adwaita-dark";
-    };
+    # Literally nothing here
   };
 }
