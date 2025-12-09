5a9a1a2a53dcb01f746c20807e8234181e8107c6
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-09 01:13:52 +0000

    theme

--- hosts/jet/default.nix
+++ hosts/jet/default.nix
@@ -105,8 +105,6 @@
     withUWSM = true;
   };
 
-  programs.dconf.enable = true;
-
   environment = {
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
--- modules/home/shared/theme/default.nix
+++ modules/home/shared/theme/default.nix
@@ -13,37 +13,18 @@
       size = 16;
     };
 
-    dconf = {
+    gtk = {
       enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    };
-    
-    # Don't use gtk.enable, configure files directly
-    home.packages = with pkgs; [
-      gnome-themes-extra
-      adwaita-icon-theme
-    ];
-    
-    xdg.configFile."gtk-3.0/settings.ini".text = ''
-      [Settings]
-      gtk-theme-name=Adwaita-dark
-      gtk-icon-theme-name=Adwaita
-      gtk-cursor-theme-name=Bibata-Modern-Ice
-      gtk-cursor-theme-size=16
-      gtk-application-prefer-dark-theme=true
-    '';
-    
-    xdg.configFile."gtk-4.0/settings.ini".text = ''
-      [Settings]
-      gtk-theme-name=Adwaita-dark
-      gtk-icon-theme-name=Adwaita
-      gtk-cursor-theme-name=Bibata-Modern-Ice
-      gtk-cursor-theme-size=16
-      gtk-application-prefer-dark-theme=true
-    '';
-    
-    home.sessionVariables = {
-      GTK_THEME = "Adwaita-dark";
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
     };
   };
 }
