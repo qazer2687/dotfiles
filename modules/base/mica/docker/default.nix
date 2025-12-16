4226e82b4e18f3d441c5f04b3684922b4cd03f51
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-16 00:51:34 +0000

    fix

--- modules/base/mica/docker/default.nix
+++ modules/base/mica/docker/default.nix
@@ -11,21 +11,6 @@
     users.users.alex = {
       extraGroups = ["docker"];
     };
-    
-    users.groups.dockremap.gid = 10000;
-    users.users.dockremap = {
-      isSystemUser = true;
-      uid = 10000;
-      group = "dockremap";
-      subUidRanges = [
-        { startUid = 1000; count = 1; }
-        { startUid = 100001; count = 65535; }
-      ];
-      subGidRanges = [
-        { startGid = 1000; count = 1; }
-        { startGid = 100001; count = 65535; }
-      ];
-    };
 
     virtualisation.docker = {
       enable = true;
@@ -35,10 +20,6 @@
         # Fix for some services just not being able to communicate
         # with the web, for completely unknown reasons.
         dns = ["1.1.1.1"];
-        
-        # Enable user namespace remapping so containers run as root
-        # but are seen by the host as a non-privileged user.
-        userns-remap = "default";
 
         # A fix for s6-svscan hanging on shutdown.
         # https://github.com/NixOS/nixpkgs/issues/182916#issuecomment-1364504677
