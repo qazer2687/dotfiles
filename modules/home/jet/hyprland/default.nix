4e5174c2877e0cf970094c16048850072e8136d4
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2025-12-29 20:57:15 +0000

    test

--- modules/home/jet/hyprland/default.nix
+++ modules/home/jet/hyprland/default.nix
@@ -20,7 +20,7 @@ in {
 
     wayland.windowManager.hyprland = {
       enable = true;
-      xwayland.enable = true;
+      xwayland.enable = false;
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
 
