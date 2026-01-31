534d4e0bb60f33a9ae441f1aabd0878f6a069b45
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-01-31 10:58:20 +0000

    firefox

--- modules/home/sage/firefox/config/custom.js
+++ modules/home/sage/firefox/config/custom.js
@@ -1,28 +1,5 @@
 /* 1.6x Scaling */
 user_pref("layout.css.devPixelsPerPx", 1.2);
 
-/*
-user_pref("gfx.webrender.all", true);
-user_pref("gfx.webrender.enabled", true);
-user_pref("layers.acceleration.force-enabled", true);
-user_pref("gfx.canvas.azure.accelerated", true);
-user_pref("gfx.webrender.compositor", true);
-user_pref("gfx.webrender.compositor.force-enabled", true);
-user_pref("gfx.webrender.software", false);
-user_pref("webgl.disabled", false);
-user_pref("media.hardware-video-decoding.force-enabled", true);
-*/
-
-/* this gets rid of the dots and weird window corruption */
-user_pref("gfx.webrender.all", false);
-user_pref("gfx.webrender.enabled", false);
-user_pref("layers.acceleration.force-enabled", false);
-user_pref("gfx.canvas.azure.accelerated", false);
-user_pref("gfx.webrender.compositor", false);
-user_pref("gfx.webrender.compositor.force-enabled", false);
-user_pref("gfx.webrender.software", true);
-user_pref("webgl.disabled", true);
-user_pref("media.hardware-video-decoding.force-enabled", false);
-
 /* Don't sync theme */
 user_pref("services.sync.addons.ignoreUserEnabledChanges", true);
