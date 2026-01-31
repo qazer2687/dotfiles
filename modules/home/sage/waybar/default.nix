0cfcf59b019f66eeff4c66a90d6f8d7700d13554
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-01-31 11:03:59 +0000

    fix

--- modules/home/sage/waybar/default.nix
+++ modules/home/sage/waybar/default.nix
@@ -2,6 +2,7 @@
   lib,
   config,
   base16,
+  pkgs,
   ...
 }: let
   scheme = base16 "mountain";
@@ -12,25 +13,27 @@ in {
     home.file.".config/waybar/scripts/pingServer.sh".text = builtins.readFile ./scripts/pingServer.sh;
     home.file.".config/waybar/scripts/pingServer.sh".executable = true;
 
+    home.packages = [pkgs.jq];
+
     programs.waybar = {
       enable = true;
       settings = {
         mainBar = {
           layer = "top";
-          height = 24;
+          height = 28;
           margin = "0 0 0 0";
-          modules-left = ["custom/pingServer"];
-          modules-center = ["hyprland/workspaces"];
-          modules-right = ["tray" "network" "pulseaudio" "clock"];
+          modules-left = ["clock" "hyprland/workspaces"];
+          modules-center = [];
+          modules-right = ["tray" "network" "pulseaudio" "battery"];
 
           pulseaudio = {
-            format = "VOL: {volume}%";
+            format = "vol: {volume}%";
             tooltip = false;
-            format-muted = "VOL: MUTED";
+            format-muted = "vol: muted";
           };
 
           "custom/hyprsunset" = {
-            exec = ''printf "TEMP: %sK" "$(hyprctl hyprsunset temperature)"'';
+            exec = ''printf "󰖨 %sK" "$(hyprctl hyprsunset temperature)"'';
             signal = 1;
             format = "{}";
             tooltip = false;
@@ -38,8 +41,7 @@ in {
 
           "custom/pingServer" = {
             exec = "$HOME/.config/waybar/scripts/pingServer.sh";
-            interval = 30;
-            format = "{}";
+            interval = 20;
             return-type = "json";
           };
 
@@ -57,18 +59,28 @@ in {
 
           battery = {
             tooltip = false;
-            format = "BAT: {capacity}%";
-            format-charging = "BAT: {capacity}%";
-            interval = 10;
+            format = "bat: {capacity}%";
+            format-charging = "bat: {capacity}% (charging)";
+            interval = 30;
             states = {
-              warning = 25;
-              critical = 10;
+              sub50 = 50;
+              sub25 = 25;
+              sub10 = 10;
             };
+            format-icons = ["/    " "//   " "///  " "//// " "/////"];
             margin-left = 15;
           };
 
+          network = {
+            tooltip = false;
+            format = "net: up";
+            format-wifi = "net: up";
+            format-disconnected = "net: down";
+            interval = 30;
+          };
+
           "hyprland/workspaces" = {
-            format = "{icon}";
+            format = "";
             format-icons = {
               "1" = "1";
               "2" = "2";
@@ -79,7 +91,7 @@ in {
               "7" = "7";
               "8" = "8";
               "9" = "9";
-              "0" = "0";
+              "10" = "10";
             };
           };
 
@@ -88,37 +100,14 @@ in {
             format = "BKL: {percent}%";
             tooltip = false;
           };
-
-          network = {
-            tooltip = false;
-            format-wifi = "NET: {essid}";
-            format-alt = "{essid} - {ifname} - {ipaddr}";
-            format-disconnected = "NET: OFFLINE";
-            format-ethernet = "NET: WIRED";
-            interval = 10;
-          };
-
-          mpris = {
-            tooltip = false;
-            format = "{title}";
-            format-paused = "{title}";
-            artist-len = 20;
-            title-len = 20;
-            player-icons = {
-              default = "";
-            };
-            status-icons = {
-              paused = "";
-            };
-          };
         };
       };
 
       style = ''
         * {
           border: none;
-          border-radius: 0px;
-          font-family: TX02;
+          border-radius: 0;
+          font-family: "DepartureMono";
           font-size: 11px;
           min-height: 0;
         }
@@ -127,72 +116,43 @@ in {
           background: #${scheme.base00};
         }
 
-        #mpris, #clock, #language, #pulseaudio, #bluetooth, #network,
-        #memory, #cpu, #temperature, #disk, #custom-kernel, #custom-hyprsunset, #idle_inhibitor, #mode,
-        #backlight, #battery, #custom-pingServer, #workspaces button, #workspaces button.active {
+        #mpris, #clock, #language, #bluetooth, #custom-pingServer, #tray, #network, #battery, #pulseaudio {
           padding: 0 8px;
-          margin: 2px 2px;
+          margin: 2px;
           border-radius: 2px;
-          background: #${scheme.base02};
+          background: #${scheme.base01};
           color: #${scheme.base05};
         }
 
+
         #workspaces {
-          color: transparent;
-          background: #${scheme.base02};
+          padding: 0 1px;
+          margin: 2px;
           border-radius: 2px;
-          margin: 2px 4px;
+          background-color: #${scheme.base01};
         }
+
         #workspaces button {
-          background-color: transparent;
-          border-radius: 0;
-          margin: 0px 0px;
+          padding: 0 8px;
+          margin: 2px 1px;
+          border-radius: 2px;
+          background-color: #${scheme.base02};
         }
+
         #workspaces button.active {
-          background-color: transparent;
-          border-radius: 0;
-          margin: 0px 0px;
-          box-shadow: inset 0 -1px 0 #${scheme.base01};
+          padding: 0 8px;
+          margin: 2px 1px;
+          border-radius: 2px;
+          background-color: #${scheme.base0E};
         }
 
-        #custom-pingServer.up {
-          color: #${scheme.base0B};
-        }
-        #custom-pingServer.down {
-          color: #${scheme.base04};
-        }
+        #battery.charging { color: #${scheme.base0B}; }
+        #battery.sub50:not(.charging) { color: #${scheme.base0A}; }
+        #battery.sub25:not(.charging) { color: #${scheme.base09}; }
+        #battery.sub10:not(.charging) { color: #${scheme.base08}; }
 
-        /* BATTERY */
-
-        #battery.warning:not(.charging) {
-          background-color: #${scheme.base09};
-          color: #${scheme.base00};
-        }
-
-        #battery.critical:not(.charging) {
-          background-color: #${scheme.base08};
-          color: #${scheme.base00};
-        }
-
-        #battery.charging {
-          background-color: #${scheme.base0B};
-          color: #${scheme.base00};
-        }
-
-        /* EDGE PADDING */
-
-        #workspaces {
-          margin-left: 16px;
-          margin-right: 16px;
-        }
-
-        #tray {
-          margin-right: 4px;
-        }
-
-        #battery {
-          margin-right: 16px;
-        }
+        #clock { margin-left: 20px; }
+        #battery { margin-right: 20px; }
       '';
     };
   };
