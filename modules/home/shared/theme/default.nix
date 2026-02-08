839de11acbedac590f0a4f09cd50b40ede1ba847
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-02-08 10:06:49 +0000

    theme

--- modules/home/shared/theme/default.nix
+++ modules/home/shared/theme/default.nix
@@ -16,14 +16,26 @@
     gtk = {
       enable = true;
       theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
+        # This targets GTK2 and GTK3 specifically. 
+        # adw-gtk3 is designed to make GTK3 apps look like GTK4 Libadwaita.
+        name = "adw-gtk3-dark"; 
+        package = pkgs.adw-gtk3;
       };
+      
       gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
+        gtk-application-prefer-dark-theme = 1; # Use 1 for boolean in GTK ini
       };
+      
       gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
+        gtk-application-prefer-dark-theme = 1;
+      };
+    };
+
+    # This ensures GTK4/Libadwaita apps (which ignore themes) 
+    # are forced into dark mode via dconf.
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
       };
     };
   };
