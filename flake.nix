5e012f3b1e09c73245d6fe7e3bce8944c8845e05
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-14 16:29:56 +0000

    flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,11 @@
     asahi.url = "github:nix-community/nixos-apple-silicon";
     sops-nix.url = "github:Mic92/sops-nix";
     flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
@@ -18,18 +22,20 @@
     nix-base16.url = "github:qazer2687/nix-base16";
   };
 
-  outputs = {self, ...} @ inputs: {
-    overlays = import ./overlays {inherit inputs;};
+  outputs =
+    { self, ... }@inputs:
+    {
+      overlays = import ./overlays { inherit inputs; };
 
-    nixosConfigurations = {
-      # Desktop
-      sage = (import ./flake/sage) {inherit inputs self;};
+      nixosConfigurations = {
+        # Desktop
+        sage = (import ./flake/sage) { inherit inputs self; };
 
-      # Primary Laptop
-      jet = (import ./flake/jet) {inherit inputs self;};
+        # Primary Laptop
+        jet = (import ./flake/jet) { inherit inputs self; };
 
-      # Secondary Laptop
-      ivy = (import ./flake/ivy) {inherit inputs self;};
+        # Secondary Laptop
+        ivy = (import ./flake/ivy) { inherit inputs self; };
+      };
     };
-  };
 }
