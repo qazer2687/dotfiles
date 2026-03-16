7db52e791c8d534cb533d21517481a01c13a0ce5
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-16 14:29:20 +0000

    fix

--- modules/home/shared/activitywatch/default.nix
+++ modules/home/shared/activitywatch/default.nix
@@ -8,9 +8,10 @@
   options.modules.activitywatch.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.activitywatch.enable {
+    home.packages = [ pkgs.aw-qt ];
     services.activitywatch = {
       enable = true;
-      package = pkgs.aw-server-rust;
+      #package = pkgs.aw-server-rust;
     };
   };
 }
