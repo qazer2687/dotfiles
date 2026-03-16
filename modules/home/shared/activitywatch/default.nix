e90155cab86d5e73e5cb604eca7fecc4f9a4a5fd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-16 14:50:39 +0000

    test

--- modules/home/shared/activitywatch/default.nix
+++ modules/home/shared/activitywatch/default.nix
@@ -11,7 +11,26 @@
     home.packages = [ pkgs.aw-qt ];
     services.activitywatch = {
       enable = true;
-      #package = pkgs.aw-server-rust;
+      package = pkgs.activitywatch;
+      watchers = {
+        aw-watcher-afk = {
+          package = pkgs.activitywatch;
+          settings = {
+            timeout = 180;
+            poll_time = 2;
+          };
+        };
+        aw-watcher-window = {
+          package = pkgs.activitywatch;
+          settings = {
+            exclude_title = false;
+            poll_time = 1;
+          };
+        };
+      };
+      settings = {
+        timeout = 180;
+      };
     };
   };
 }
