e2d02d918d13fce9dc0a3d0f459f61d790482314
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-18 11:24:21 +0000

    alias

--- modules/home/shared/fish/default.nix
+++ modules/home/shared/fish/default.nix
@@ -50,6 +50,9 @@
       "rebuild" = "nh os switch github:qazer2687/dotfiles -H $(hostname) -- --refresh --option eval-cache false";
       "reboot" = ''printf "Are you sure you want to reboot? [N/y]\n"; read -n 1 confirm; test "$confirm" = y && sudo reboot'';
       "nvim" = "nix run github:qazer2687/neovim --refresh";
+      
+      "luks unlock" = "cd ~/luks && ./mount.sh && cd encrypted";
+      "luks lock" = "cd ~/luks && ./unmount.sh";
     };
   };
 }
