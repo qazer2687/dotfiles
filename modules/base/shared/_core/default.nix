3bd7b46b5eeb55566a942627f6894ef2e81b2882
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-22 18:53:22 +0000

    fix

--- modules/base/shared/_core/default.nix
+++ modules/base/shared/_core/default.nix
@@ -93,7 +93,7 @@
     */
 
     # Block AI-related domains.
-    networking.extraHosts = ''
+    /*networking.extraHosts = ''
       127.0.0.1 chat.openai.com
       127.0.0.1 openai.com
       127.0.0.1 claude.ai
@@ -104,7 +104,7 @@
       127.0.0.1 perplexity.ai
       127.0.0.1 copilot.microsoft.com
       127.0.0.1 deepseek.com
-    '';
+    '';*/
 
     ########## KEYMAP ##########
 
