e2721e1137caed0cc3c44f5e304890cf3cc48b24
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-23 15:12:23 +0000

    test

--- modules/home/jet/kitty/default.nix
+++ modules/home/jet/kitty/default.nix
@@ -19,61 +19,58 @@ in {
 
         # Layout
         padding = "8 8";
-        # line_height not directly convertible; kitty uses a multiplier.
-        # Omitting lets kitty use its default (around 1.0). If you need
-        # a specific height, set e.g. line_height = 1.2.
 
         # Scrollback
         scrollback_lines = 10000;
 
         # Cursor
         cursor_shape = "beam";
-        cursor_blink_interval = 0.5;   # matches foot’s `blink = yes`
+        cursor_blink_interval = 0.5;
 
         # Mouse
         hide_mouse_when_typing = false;
 
-        # Colors – base16 “oxocarbon-dark” mapped to kitty’s options
-        foreground = scheme.base05;
-        background = scheme.base00;
+        # Colors – with "#" prefix
+        foreground = "#${scheme.base05}";
+        background = "#${scheme.base00}";
 
-        cursor = scheme.base00;
-        cursor_text_color = scheme.base06;
+        cursor = "#${scheme.base00}";
+        cursor_text_color = "#${scheme.base06}";
 
-        selection_foreground = scheme.base05;
-        selection_background = scheme.base01;
+        selection_foreground = "#${scheme.base05}";
+        selection_background = "#${scheme.base01}";
 
-        url_color = scheme.base0D;
+        url_color = "#${scheme.base0D}";
 
         # Standard 16 ANSI colors
-        color0  = scheme.base03;  # regular0
-        color1  = scheme.base08;  # regular1
-        color2  = scheme.base0B;  # regular2
-        color3  = scheme.base0A;  # regular3
-        color4  = scheme.base0D;  # regular4
-        color5  = scheme.base0E;  # regular5
-        color6  = scheme.base0C;  # regular6
-        color7  = scheme.base04;  # regular7
+        color0  = "#${scheme.base03}";
+        color1  = "#${scheme.base08}";
+        color2  = "#${scheme.base0B}";
+        color3  = "#${scheme.base0A}";
+        color4  = "#${scheme.base0D}";
+        color5  = "#${scheme.base0E}";
+        color6  = "#${scheme.base0C}";
+        color7  = "#${scheme.base04}";
 
-        color8  = scheme.base02;  # bright0
-        color9  = scheme.base08;  # bright1
-        color10 = scheme.base0B;  # bright2
-        color11 = scheme.base0A;  # bright3
-        color12 = scheme.base0D;  # bright4
-        color13 = scheme.base0E;  # bright5
-        color14 = scheme.base0C;  # bright6
-        color15 = scheme.base07;  # bright7
+        color8  = "#${scheme.base02}";
+        color9  = "#${scheme.base08}";
+        color10 = "#${scheme.base0B}";
+        color11 = "#${scheme.base0A}";
+        color12 = "#${scheme.base0D}";
+        color13 = "#${scheme.base0E}";
+        color14 = "#${scheme.base0C}";
+        color15 = "#${scheme.base07}";
 
-        # Extended colors (16 and 17)
-        color16 = scheme.base09;
-        color17 = scheme.base06;
+        # Extended colors
+        color16 = "#${scheme.base09}";
+        color17 = "#${scheme.base06}";
       };
 
       keybindings = {
-        "ctrl+c"      = "copy_to_clipboard";
-        "ctrl+v"      = "paste_from_clipboard";
-        "XF86Copy"    = "copy_to_clipboard";
-        "XF86Paste"   = "paste_from_clipboard";
+        "ctrl+c"    = "copy_to_clipboard";
+        "ctrl+v"    = "paste_from_clipboard";
+        "XF86Copy"  = "copy_to_clipboard";
+        "XF86Paste" = "paste_from_clipboard";
       };
     };
   };
