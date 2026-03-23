771694db9f1717656e68ac921abca9a8ac91b949
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-23 23:27:18 +0000

    test

--- modules/home/jet/rmpc/default.nix
+++ modules/home/jet/rmpc/default.nix
@@ -42,7 +42,7 @@
       
       settings = {
         directory = "/home/alex/Music/library";
-        plugins = [ "chroma" "musicbrainz" "fromfilename" ];
+        plugins = [ "chroma" "discogs" "musicbrainz" "lastgenre"  "fromfilename" ];
       
         import = {
           write = true;
