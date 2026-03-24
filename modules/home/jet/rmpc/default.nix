d78a4c8b3b45eb47363b8ebc8aa16ecb80c20593
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-24 15:47:51 +0000

    test

--- modules/home/jet/rmpc/default.nix
+++ modules/home/jet/rmpc/default.nix
@@ -47,17 +47,61 @@
 
     programs.beets = {
       enable = true;
-      package = (pkgs.python3Packages.beets.override {
+      # https://github.com/arximboldi/dotfiles/blob/42f44ae499356120957b375146c1325b60703914/nix/os/common/desktop.nix#L103
+      package = let
+        beetcamp = (pkgs.python3Packages.buildPythonApplication {
+          pname = "beets-beetcamp";
+          version = "0.21.0";
+          src = pkgs.fetchFromGitHub {
+            repo = "beetcamp";
+            owner = "snejus";
+            rev = "64c7afc9d87682fb2b7c9f2deb76525e44afb248";
+            sha256 = "sha256-d0yvOyfxPPBUpoO6HCWfMq2vVw+CcQo16hx+JRDMkBw=";
+          };
+          format = "pyproject";
+          buildInputs = with pkgs.python3Packages; [ poetry-core ];
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            setuptools
+            requests
+            cached-property
+            pycountry
+            python-dateutil
+            ordered-set
+          ];
+          checkInputs = with pkgs.python3Packages;
+            [
+              # pytestCheckHook
+              pytest-cov
+              pytest-randomly
+              pytest-lazy-fixture
+              rich
+              tox
+              types-setuptools
+              types-requests
+            ] ++ [ pkgs.beets ];
+          meta = {
+            homepage = "https://github.com/snejus/beetcamp";
+            description = "Bandcamp autotagger plugin for beets.";
+            license = pkgs.lib.licenses.gpl2;
+            inherit (pkgs.beets.meta) platforms;
+            maintainers = with pkgs.lib.maintainers; [ rrix ];
+          };
+        });
+      in pkgs.python3.pkgs.beets.override {
         pluginOverrides = {
+          beetcamp = {
+            enable = true;
+            propagatedBuildInputs = [ beetcamp ];
+          };
           fetchart.enable = true;
           fromfilename.enable = true;
           chroma.enable = true;
         };
-      });
-      
+      };
+
       settings = {
         directory = "/home/alex/Music/library";
-        plugins = [ "mpdupdate" "chroma" "discogs" "lastgenre" "fromfilename" "fetchart" "embedart"];
+        plugins = [ "mpdupdate" "bandcamp" "chroma" "discogs" "lastgenre" "fromfilename" "fetchart" "embedart"];
       
         sources = [ "discogs" "musicbrainz" ];
 
