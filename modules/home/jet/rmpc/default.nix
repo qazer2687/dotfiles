1b882b23f0695a6fc9907104b575d6311af1b4ad
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-24 22:28:44 +0000

    test

--- modules/home/jet/rmpc/default.nix
+++ modules/home/jet/rmpc/default.nix
@@ -54,13 +54,13 @@
 
       settings = {
         directory = "/home/alex/Music/library";
-        plugins = [ "mpdupdate" "bandcamp" "chroma" "lastgenre" "fromfilename" "fetchart" "embedart"];
+        plugins = [ "mpdupdate" "bandcamp" "chroma" "discogs" "lastgenre" "fromfilename" "fetchart" "embedart" "musicbrainz" ];
       
-        sources = [ "bandcamp" "musicbrainz" ];
+        sources = [ "bandcamp" "musicbrainz" "discogs" ];
 
-        #discogs = {
-        #  user_token_path = "~/.config/beets/discogs_token";
-        #};
+        discogs = {
+          user_token_path = "~/.config/beets/discogs_token";
+        };
 
         fetchart = {
           auto = true;
