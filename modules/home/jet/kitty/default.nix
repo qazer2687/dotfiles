e7ed909cb3555d3d75bcb4b6f03f6f7fd66aff12
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-25 02:31:31 +0000

    test

--- modules/home/jet/kitty/default.nix
+++ modules/home/jet/kitty/default.nix
@@ -18,8 +18,8 @@ in {
         bold_font = "PragmataPro";
 
         # Padding
-        window_padding_width = 8;
-        window_padding_height = 8;
+        window_padding_width = 4;
+        window_padding_height = 4;
 
         # Scrollback
         scrollback_lines = 10000;
