3182ba291d193cd64f56a7106d39e4bf260f37d7
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-25 02:39:10 +0000

    fix

--- modules/home/jet/waybar/default.nix
+++ modules/home/jet/waybar/default.nix
@@ -98,7 +98,7 @@ in {
           mpris = {
             tooltip = false;
             format = "{player_icon} {artist} - {title}";
-            format-paused = "{status_icon} <s>{artist} - {title}</s>";
+            format-paused = "{status_icon} {artist} - {title}";
             artist-len = 25;
             title-len = 25;
             player-icons = {
