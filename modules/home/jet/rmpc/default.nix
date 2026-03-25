0e6e3be6ac3111247d4e63ddc0027a7f556ce864
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-25 00:35:26 +0000

    test

--- modules/home/jet/rmpc/default.nix
+++ modules/home/jet/rmpc/default.nix
@@ -43,7 +43,183 @@
 
     programs.rmpc = {
       enable = true;
+      config = ''
+        #![enable(implicit_some)]
+        #![enable(unwrap_newtypes)]
+        #![enable(unwrap_variant_newtypes)]
+        (
+            address: "insert your mpd address here",
+            theme: "custom",
+            cava: (
+                input: (
+                    method: Fifo,
+                    source: "/tmp/mpd.fifo",
+                    sample_rate: 44100,
+                    channels: 2,
+                    sample_bits: 16,
+                ),
+            ),
+            tabs: [
+                (
+                    name: "Queue",
+                    pane: Split(
+                        direction: Horizontal,
+                        panes: [
+                            (size: "60%", pane: Split(
+                                direction: Vertical,
+                                borders: "RIGHT",
+                                panes: [
+                                    (size: "50%", pane: Pane(Directories)),
+                                    (size: "50%", pane: Pane(Queue), borders: "TOP"),
+                                ],
+                            )),
+                            (size: "40%", pane: Split(
+                                direction: Vertical,
+                                panes: [
+                                    (size: "80%", pane: Pane(AlbumArt)),
+                                    (size: "20%", pane: Pane(Cava)),
+                                ],
+                            )),
+                        ],
+                    ),
+                ),
+                (name: "Playlists", pane: Pane(Playlists)),
+                (name: "Artists",   pane: Pane(Artists)),
+                (name: "Albums",    pane: Pane(Albums)),
+                (name: "Search",    pane: Pane(Search)),
+            ],
+        )
+      '';
     };
+
+    xdg.configFile."rmpc/themes/custom.ron".text = ''
+      #![enable(implicit_some)]
+      #![enable(unwrap_newtypes)]
+      #![enable(unwrap_variant_newtypes)]
+      (
+          background_color: "#181825",
+          text_color: "#dfe1eb",
+          tab_bar: (
+              enabled: true,
+              active_style: (fg: "#181825", bg: "#adacb2", modifiers: "Bold"),
+              inactive_style: (),
+          ),
+          highlighted_item_style: (fg: "#a6adc8", modifiers: "Bold"),
+          current_item_style: (fg: "#181825", bg: "#adacb2", modifiers: "Bold"),
+          borders_style: (fg: "#a6adc8"),
+          highlight_border_style: (fg: "#adacb2"),
+          symbols: (
+              song: "S",
+              dir: " ",
+              playlist: "󰲸 ",
+              marker: "󰘍  ",
+              ellipsis: "...",
+          ),
+          progress_bar: (
+              symbols: ["", "█", "◣", "█", "█"],
+              track_style:   (fg: "#1e2030"),
+              elapsed_style: (fg: "#a6adc8"),
+              thumb_style:   (fg: "#a6adc8", bg: "#1e2030"),
+          ),
+          cava: (
+              bar_symbols: ['▁', '▂', '▃', '▄', '▅', '▆', '▇', '█'],
+              bar_width: 2,
+              bar_spacing: 1,
+              bar_color: Gradient({
+                    0: "#45475a",
+                  50: "#6c7086",
+                  100: "#b4befe",
+              })
+          ),
+          scrollbar: (
+              symbols: ["│", "█", "⊤", "⊥"],
+              track_style: (),
+              ends_style: (),
+              thumb_style: (fg: "#a6adc8"),
+          ),
+          song_table_format: [
+              (
+                  prop: (kind: Property(Artist), default: (kind: Text("Unknown"))),
+                  width: "33%",
+              ),
+              (
+                  prop: (kind: Property(Title), default: (kind: Text("Unknown"))),
+                  width: "33%",
+                  alignment: Center,
+              ),
+              (
+                  prop: (kind: Property(Duration), default: (kind: Text("-"))),
+                  width: "33%",
+                  alignment: Right,
+              ),
+          ],
+          layout: Split(
+              direction: Vertical,
+              panes: [
+                  (size: "4",    borders: "ALL", pane: Pane(Header)),
+                  (size: "3",                    pane: Pane(Tabs)),
+                  (size: "100%", borders: "ALL", pane: Pane(TabContent)),
+                  (size: "3",    borders: "ALL", pane: Pane(ProgressBar)),
+              ],
+          ),
+          header: (
+              rows: [
+                  (
+                      left: [
+                          (kind: Text("|"),  style: (fg: "#a6adc8", modifiers: "Bold")),
+                          (kind: Property(Status(StateV2(playing_label: "Playing", paused_label: "Paused", stopped_label: "Stopped"))), style: (fg: "#a6adc8", modifiers: "Bold")),
+                          (kind: Text("|"),  style: (fg: "#a6adc8", modifiers: "Bold")),
+                      ],
+                      center: [
+                          (kind: Property(Song(Title)), style: (modifiers: "Bold"),
+                              default: (kind: Text("No Song"), style: (modifiers: "Bold")))
+                      ],
+                      right: [
+                          (kind: Property(Widget(ScanStatus)), style: (fg: "#a6adc8")),
+                          (kind: Property(Widget(Volume)),     style: (fg: "#a6adc8")),
+                      ]
+                  ),
+                  (
+                      left: [
+                          (kind: Property(Status(Elapsed))),
+                          (kind: Text(" / ")),
+                          (kind: Property(Status(Duration))),
+                      ],
+                      center: [
+                          (kind: Property(Song(Artist)), style: (fg: "#9399b2", modifiers: "Bold"),
+                              default: (kind: Text("Unknown"), style: (fg: "#9399b2", modifiers: "Bold"))),
+                          (kind: Text(" - ")),
+                          (kind: Property(Song(Album)),
+                              default: (kind: Text("Unknown Album"))),
+                      ],
+                      right: [
+                          (
+                              kind: Property(Widget(States(
+                                  active_style: (fg: "white", modifiers: "Bold"),
+                                  separator_style: (fg: "white")))),
+                              style: (fg: "#9399b2")
+                          ),
+                      ]
+                  ),
+              ],
+          ),
+          browser_song_format: [
+              (kind: Group([
+                  (kind: Property(Track)),
+                  (kind: Text(" ")),
+              ])),
+              (
+                  kind: Group([
+                      (kind: Property(Artist)),
+                      (kind: Text(" - ")),
+                      (kind: Property(Title)),
+                  ]),
+                  default: (kind: Property(Filename))
+              ),
+          ],
+          lyrics: (timestamp: false)
+      )
+    '';
 
     programs.beets = {
       enable = true;
