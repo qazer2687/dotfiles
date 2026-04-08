f138720028ee0b38fb576daf5b237423c8088c77
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-04-08 20:02:36 +0100

    fix

--- modules/base/sage/ollama/default.nix
+++ modules/base/sage/ollama/default.nix
@@ -9,6 +9,7 @@
   config = lib.mkIf config.modules.ollama.enable {
     services.ollama = {
       enable = true;
+      package = pkgs.ollama-vulkan;
       host = "0.0.0.0";
       port = 11434;
     };
