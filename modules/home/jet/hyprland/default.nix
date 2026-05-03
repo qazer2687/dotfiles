9029e0120ecba134705a673dd717cd8df40af16c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-03 12:40:33 +0100

    fix

--- modules/home/jet/hyprland/default.nix
+++ modules/home/jet/hyprland/default.nix
@@ -216,7 +216,7 @@ in
 
       extraConfig = ''
         # Add extra config here...
-        AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card2
+        env = AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card2
       '';
     };
   };
