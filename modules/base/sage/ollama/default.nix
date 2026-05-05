e9d08ec029465651faa4af1c7a53e9bb9f38fd3c
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-05 23:46:53 +0100

    fix

--- modules/base/sage/ollama/default.nix
+++ modules/base/sage/ollama/default.nix
@@ -12,6 +12,9 @@
       package = pkgs.ollama-vulkan;
       host = "0.0.0.0";
       port = 11434;
+      environmentVariables = {
+        OLLAMA_KV_CACHE_TYPE = "q4_0";
+      };
     };
   };
 }
