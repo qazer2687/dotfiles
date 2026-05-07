acfd395721b4b6c476d57c4a90d0ff4b4c0c12c8
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-07 16:21:02 +0100

    fix

--- modules/base/sage/ollama/default.nix
+++ modules/base/sage/ollama/default.nix
@@ -13,7 +13,7 @@
       host = "0.0.0.0";
       port = 11434;
       environmentVariables = {
-        OLLAMA_KV_CACHE_TYPE = "q4_0";
+        #OLLAMA_KV_CACHE_TYPE = "q4_0";
       };
     };
   };
