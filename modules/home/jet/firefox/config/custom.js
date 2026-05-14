dadd217867702d76ce3d1fe6a561d91d9004c3bd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-14 14:51:29 +0100

    betterfox update

--- modules/home/jet/firefox/config/custom.js
+++ modules/home/jet/firefox/config/custom.js
@@ -1,5 +1,9 @@
 /* 1.6x Scaling */
 user_pref("layout.css.devPixelsPerPx", 1.6);
-
 /* Don't sync theme */
 user_pref("services.sync.addons.ignoreUserEnabledChanges", true);
+/* Limit content processes to reduce RAM usage */
+user_pref("dom.ipc.processCount", 4);
+user_pref("dom.ipc.processCount.webIsolated", 1);
+/* Discard background tabs under memory pressure */
+user_pref("browser.tabs.unloadOnLowMemory", true);
