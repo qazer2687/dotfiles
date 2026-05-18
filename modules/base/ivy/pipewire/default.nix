0fe1f7a4dc3e506603084dce635978ea2433cf68
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-19 00:45:04 +0100

    sound

---
+++ modules/base/ivy/pipewire/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.modules.pipewire.enable = lib.mkEnableOption "";
+
+  config = lib.mkIf config.modules.pipewire.enable {
+    security.rtkit.enable = true;
+    environment.systemPackages = with pkgs; [
+      pulseaudio
+      pavucontrol
+      pwvucontrol
+      pamixer
+    ];
+    
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+      extraConfig.pipewire."92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate"          = 48000;
+          "default.clock.allowed-rates" = [ 48000 ];
+          "default.clock.force-rate"    = 48000;
+
+          "default.clock.quantum"       = 128;
+          "default.clock.min-quantum"   = 128;
+          "default.clock.max-quantum"   = 128;
+        };
+      };
+    };
+  };
+}
