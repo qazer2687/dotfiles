b14965a840d3a73dc0c8f960a36f139ae40171cf
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-24 00:48:45 +0100

    fix

--- modules/home/sage/niri/default.nix
+++ modules/home/sage/niri/default.nix
@@ -105,7 +105,7 @@ in {
           "Mod+Shift+k".action = move-column-to-workspace-up;
 
           # Keyboard Layout
-          "Mod+'".action.switch-layout = "next";
+          "Mod+n".action.switch-layout = "next";
         };
 
         switch-events = {
