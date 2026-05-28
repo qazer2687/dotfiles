71db482e8c33d13ec3089e39f20706a8cb6b84a4
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-05-29 00:26:51 +0100

    tweaks

--- modules/home/sage/niri/default.nix
+++ modules/home/sage/niri/default.nix
@@ -9,8 +9,9 @@
   scheme = base16 "mountain";
 in {
   options.modules.niri.enable = lib.mkEnableOption "";
-
   config = lib.mkIf config.modules.niri.enable {
+    environment.systemPackages = [ pkgs.xwayland-satellite ];
+
     programs.niri = {
       enable = true;
       package = inputs.niri.packages.${pkgs.system}.niri-unstable;
@@ -18,44 +19,37 @@ in {
         hotkey-overlay.skip-at-startup = true;
         prefer-no-csd = true;
 
+        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+
         input = {
           keyboard.xkb = {
-            layout = "gb,gb";
-            variant = "colemak,";
+            layout = "gb";
+            variant = "colemak";
           };
           touchpad = {
             tap = false;
             dwt = true;
           };
-          focus-follows-mouse = {
-            enable = false;
-          };
+          focus-follows-mouse.enable = false;
         };
 
-        gestures = {
-          hot-corners.enable = false;
-        };
+        gestures.hot-corners.enable = false;
 
-        outputs = {
-          "DP-1" = {
-            mode = {
-              width = 2560;
-              height = 1440;
-              refresh = 180.00;
-            };
-            scale = 1.0;
+        outputs."DP-1" = {
+          mode = {
+            width = 2560;
+            height = 1440;
+            refresh = 180.0;
           };
+          scale = 1.0;
         };
 
         window-rules = [
           {
             clip-to-geometry = true;
-
             open-maximized = false;
-
             min-width = 2480;
             max-width = 2480;
-
             focus-ring = {
               enable = true;
               width = 1;
@@ -73,25 +67,12 @@ in {
 
         binds = with config.lib.niri.actions; {
           "Mod+Return".action = spawn "kitty";
-
           "Mod+e".action = spawn "bash" "-c" "tofi-run | xargs niri msg action spawn --";
-
           "Mod+q".action = close-window;
           "Mod+Shift+f".action = toggle-window-floating;
           "Mod+f".action = fullscreen-window;
           "Mod+o".action = toggle-overview;
-
-          "XF86AudioRaiseVolume".action = spawn "${pkgs.pamixer}/bin/pamixer" "-i" "2";
-          "XF86AudioLowerVolume".action = spawn "${pkgs.pamixer}/bin/pamixer" "-d" "2";
-          "XF86AudioMute".action = spawn "${pkgs.pamixer}/bin/pamixer" "-t";
-          "XF86AudioMicMute".action = spawn "${pkgs.pamixer}/bin/pamixer" "--default-source" "-t";
-
-          "XF86MonBrightnessUp".action = spawn "${pkgs.brightnessctl}/bin/brightnessctl" "s" "1%+";
-          "XF86MonBrightnessDown".action = spawn "${pkgs.brightnessctl}/bin/brightnessctl" "s" "1%-";
-
-          "Mod+XF86MonBrightnessUp".action = spawn "${pkgs.brightnessctl}/bin/brightnessctl" "--class" "leds" "--device" "kbd_backlight" "set" "10%+";
-          "Mod+XF86MonBrightnessDown".action = spawn "${pkgs.brightnessctl}/bin/brightnessctl" "--class" "leds" "--device" "kbd_backlight" "set" "10%-";
-
+          
           # Navigate
           "Mod+h".action = focus-column-left;
           "Mod+l".action = focus-column-right;
@@ -103,19 +84,11 @@ in {
           "Mod+Shift+l".action = move-column-right;
           "Mod+Shift+j".action = move-column-to-workspace-down;
           "Mod+Shift+k".action = move-column-to-workspace-up;
-
-          # Keyboard Layout
-          "Mod+n".action.switch-layout = "next";
-        };
-
-        switch-events = {
-          lid-close.action.spawn = ["hyprlock" "--immediate"];
         };
 
         spawn-at-startup = [
-          {command = ["hyprlock" "-q" "||" "loginctl" "terminate-session" "$XDG_SESSION_ID"];}
-          {command = ["fish" "-c" "'waybar'"];}
-          {command = ["${pkgs.wbg}/bin/wbg" "-s" "/home/alex/.config/wallpaper/wallpaper.png"];}
+          { command = [ "${pkgs.sunsetr}/bin/sunsetr" ]; }
+          { command = [ "${pkgs.wbg}/bin/wbg" "-s" "/home/alex/.config/wallpaper/wallpaper.png" ]; }
         ];
       };
     };
