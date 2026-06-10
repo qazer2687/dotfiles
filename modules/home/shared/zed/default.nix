e8bbc95fbefd0e86eb86483bba0f6633a25088bc
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-06-11 00:55:02 +0100

    undo that

--- modules/home/shared/zed/default.nix
+++ modules/home/shared/zed/default.nix
@@ -7,14 +7,10 @@
   options.modules.zed.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.zed.enable {
-    #programs.zed-editor = {
-    #  enable = true;
-    #  package = pkgs.zed-editor-fhs;
-    #};
-
-    home.packages = [
-      (pkgs.zed-editor.fhsWithPackages (pkgs: [ pkgs.zlib ]))
-    ];
+    programs.zed-editor = {
+      enable = true;
+      package = pkgs.zed-editor-fhs;
+    };
 
     home.shellAliases = {
       "zed" = "zeditor";
