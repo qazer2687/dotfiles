9fd3a7bd194bf37dec9f8bec0c303107a54dad03
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-06-11 00:47:54 +0100

    test

--- modules/home/shared/zed/default.nix
+++ modules/home/shared/zed/default.nix
@@ -7,10 +7,14 @@
   options.modules.zed.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.modules.zed.enable {
-    programs.zed-editor = {
-      enable = true;
-      package = pkgs.zed-editor-fhs;
-    };
+    #programs.zed-editor = {
+    #  enable = true;
+    #  package = pkgs.zed-editor-fhs;
+    #};
+
+    home.systemPackages = [
+      (pkgs.zed-editor.fhsWithPackages (pkgs: [ pkgs.zlib ]))
+    ];
 
     home.shellAliases = {
       "zed" = "zeditor";
