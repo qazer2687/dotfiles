6afdb4b601fd08ce2d7ed6eef4109787403ef4c2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-06-17 01:31:37 +0100

    docker group

--- hosts/sage/default.nix
+++ hosts/sage/default.nix
@@ -13,7 +13,7 @@
   users.users = {
     alex = {
       isNormalUser = true;
-      extraGroups = ["networkmanager" "wheel" "video" "audio" "dialout"];
+      extraGroups = ["networkmanager" "wheel" "video" "audio" "dialout" "docker"];
       shell = pkgs.fish;
       hashedPassword = "$6$qRDf73LqqlnrtGKd$fwNbmyhVjAHfgjPpM.Wn8YoYVbLRq1oFWN15fjP3b.cVW8Dv3s/7q8NY4WBYY7x1Xe71S.AHpuqL1PY6IJe0x1";
     };
