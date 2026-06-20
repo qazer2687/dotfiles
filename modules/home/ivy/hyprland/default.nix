1c37f2cd673c7ad5f0390956f94dc78cf1ad50b7
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-06-20 15:59:24 +0100

    fix

--- modules/home/ivy/hyprland/default.nix
+++ modules/home/ivy/hyprland/default.nix
@@ -133,7 +133,7 @@ in {
         render = {
           # Direct scanout attempts to reduce lag when
           # there is only one fullscreen application on a screen.
-          direct_scanout = 1;
+          #direct_scanout = 1;
         };
 
         misc = {
