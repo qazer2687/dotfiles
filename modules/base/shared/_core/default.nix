89ec7a6595fa6ef4782b792d4af15a78733c043a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-08-01 19:33:01 +0100

    remove garnix because they shut down

--- modules/base/shared/_core/default.nix
+++ modules/base/shared/_core/default.nix
@@ -17,7 +17,6 @@
         "https://nix-community.cachix.org/"
         "https://nixos-apple-silicon.cachix.org"
         "https://hyprland.cachix.org"
-        "https://cache.garnix.io"
         "https://attic.xuyh0120.win/lantian"
         "https://cache.xinux.uz"
       ];
@@ -31,7 +30,6 @@
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
         ];
