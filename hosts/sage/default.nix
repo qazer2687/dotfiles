78a2e828e13ea427783957fe851a72c482d5c82a
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-08-29 04:41:26 +0100

    ydotool

--- hosts/sage/default.nix
+++ hosts/sage/default.nix
@@ -12,11 +12,13 @@
   users.users = {
     alex = {
       isNormalUser = true;
-      extraGroups = ["networkmanager" "wheel" "video" "audio" "dialout" "docker"];
+      extraGroups = ["networkmanager" "wheel" "video" "audio" "dialout" "docker" "ydotool"];
       shell = pkgs.fish;
       hashedPassword = "$6$qRDf73LqqlnrtGKd$fwNbmyhVjAHfgjPpM.Wn8YoYVbLRq1oFWN15fjP3b.cVW8Dv3s/7q8NY4WBYY7x1Xe71S.AHpuqL1PY6IJe0x1";
     };
   };
+
+  programs.ydotool.enable = true;
 
   programs.fish.enable = true;
 
